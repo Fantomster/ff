f3d85ef65ec1bc54afce61d246895a3b5ed3460d
sharaf <sharaf@El-Babuino>
2016-08-31 17:22:04 +0300

    Revert "доступы, лэйауты и дрпр"
    This reverts commit 42343846bd456490b888c69280edefb8ac8224be.

--- frontend/controllers/ClientController.php
+++ frontend/controllers/ClientController.php
@@ -17,8 +17,6 @@ use yii\filters\AccessControl;
  */
 class ClientController extends Controller {
 
-    public $layout = "main-client";
-    
     private $currentUser;
 
     /**
@@ -53,7 +51,7 @@ class ClientController extends Controller {
                     ],
                 ],
                 'denyCallback' => function($rule, $action) {
-                    throw new HttpException(404 ,'Нет здесь ничего такого, проходите, гражданин');
+                    throw new HttpException(404 ,'Не здесь ничего такого, проходите, гражданин');
                 }
             ],
         ];

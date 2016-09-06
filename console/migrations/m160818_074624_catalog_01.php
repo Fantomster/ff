a42fde3cd67329fc513f13dcf749ee0fd1ab12f9
marshal <marshal@hh.ru>
2016-09-06 13:26:19 +0300

    Миграции - обновленная таблиц для работы с каталогами (финал)

--- console/migrations/m160818_074624_catalog_01.php
+++ console/migrations/m160818_074624_catalog_01.php
@@ -12,49 +12,62 @@ class m160818_074624_catalog_01 extends Migration {
         }
         $this->createTable('{{%catalog}}', [
             'id' => Schema::TYPE_PK,
-            'name' => Schema::TYPE_STRING . ' not null',
-            'org_supp_id' => Schema::TYPE_INTEGER . ' not null',
-            'type' => Schema::TYPE_INTEGER . ' not null',
-            'create_datetime' => Schema::TYPE_TIMESTAMP . ' null',
+            'type' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+            'supp_org_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'name' => Schema::TYPE_STRING . ' NOT NULL',
+            'status' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+            'created_at' => Schema::TYPE_TIMESTAMP . ' NULL',
+            'updated_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
         ], $tableOptions);
 
         $this->createTable('{{%catalog_base_goods}}', [
             'id' => Schema::TYPE_PK,
-            'cat_id' => Schema::TYPE_INTEGER . ' not null',
-            'category_id' => Schema::TYPE_INTEGER . ' not null',
-            'article' => Schema::TYPE_STRING . ' null',
-            'product' => Schema::TYPE_STRING . ' null',
-            'units' => Schema::TYPE_STRING . ' null',
-            'price' => Schema::TYPE_STRING . ' null',
+            'cat_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'category_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'article' => Schema::TYPE_STRING . ' NULL',
+            'product' => Schema::TYPE_STRING . ' NULL',
+            'units' => Schema::TYPE_STRING . ' NULL',
+            'price' => Schema::TYPE_STRING . ' NULL',
+            'status' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+            'market_place' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+            'deleted' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+            'created_at' => Schema::TYPE_TIMESTAMP . ' NULL',
+            'updated_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
         ], $tableOptions);
 
         $this->createTable('{{%catalog_goods}}', [
             'id' => Schema::TYPE_PK,
-            'cat_id' => Schema::TYPE_INTEGER . ' not null',
-            'cat_base_goods_id' => Schema::TYPE_INTEGER . ' not null',
-            'article' => Schema::TYPE_STRING . ' null',
-            'product' => Schema::TYPE_STRING . ' null',
-            'units' => Schema::TYPE_STRING . ' null',
-            'price' => Schema::TYPE_STRING . ' null',
-            'note' => Schema::TYPE_STRING . ' null',
+            'cat_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'base_goods_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'price' => Schema::TYPE_STRING . ' NULL',
+            'note' => Schema::TYPE_STRING . ' NULL',
+            'discount' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+            'created_at' => Schema::TYPE_TIMESTAMP . ' NULL',
+            'updated_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
         ], $tableOptions);
 
         $this->createTable('{{%category}}', [
             'id' => Schema::TYPE_PK,
-            'name' => Schema::TYPE_STRING . ' not null',
+            'name' => Schema::TYPE_STRING . ' NOT NULL',
         ], $tableOptions);
 
         $this->createTable('{{%relation_category}}', [
             'id' => Schema::TYPE_PK,
-            'relation_supp_rest_id' => Schema::TYPE_INTEGER . ' not null',
-            'category' => Schema::TYPE_INTEGER . ' not null',
+            'category_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'rest_org_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'supp_org_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'created_at' => Schema::TYPE_TIMESTAMP . ' NULL',
+            'updated_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
         ], $tableOptions);
 
         $this->createTable('{{%relation_supp_rest}}', [
             'id' => Schema::TYPE_PK,
-            'rest_org_id' => Schema::TYPE_INTEGER . ' not null',
-            'sup_org_id' => Schema::TYPE_INTEGER . ' not null',
-            'cat_id' => Schema::TYPE_INTEGER . ' not null',
+            'rest_org_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'supp_org_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'cat_id' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+            'invite' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+            'created_at' => Schema::TYPE_TIMESTAMP . ' NULL',
+            'updated_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
         ], $tableOptions);
     }
 

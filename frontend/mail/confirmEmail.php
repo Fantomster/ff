d3fbe0c63315cab69b674ac169856a40accea95e
sharaf <sharaf@El-Babuino>
2016-09-23 16:52:38 +0300

    почта
    Former-commit-id: d1f164f63507a18505c3a7b3503bdf15872f42ef

--- frontend/mail/confirmEmail.php
+++ frontend/mail/confirmEmail.php
@@ -9,9 +9,18 @@ use yii\helpers\Url;
  * @var \amnah\yii2\user\models\UserToken $userToken
  */
 ?>
+<p style="font-weight: normal; font-size: 14px; line-height: 1.6; margin: 0 0 10px; padding: 0;">
+    <img src="http://f-keeper.ru/mail/img/header-bg.jpg" style="max-width: 100%; margin: 0; padding: 0;" />
+</p>
+<h3 style="font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; line-height: 1.1; color: #3f3e3e; font-weight: 500; font-size: 27px; margin: 0 0 15px; padding: 0;">
+    Здравствуйте, <small style="font-size: 60%; color: #787878; line-height: 0; text-transform: none; margin: 0; padding: 0;"><?= $profile->full_name ?></small>
+</h3>
+<p style="font-weight: normal; font-size: 14px; line-height: 1.6; margin: 0 0 10px; padding: 0;">
+    Вы зарегистрировали организацию <u style="margin: 0; padding: 0;">"<?= $user->organization->name ?>"</u> в сервисе <a href="<?= Url::toRoute(["/site/index"], true); ?>" style="color: #84bf76; margin: 0; padding: 0;">F-keeper</a>
+</p>
 
-<h3><?= $subject ?></h3>
-
-<p><?= Yii::t("user", "Please confirm your email address by clicking the link below:") ?></p>
-
-<p><?= Url::toRoute(["/user/confirm", "token" => $userToken->token], true); ?></p>
+<p style="font-weight: normal; font-size: 14px; line-height: 1.6; margin: 0 0 10px; padding: 0;">Для завершения регистрации, пожалуйста, пройдите по следующей ссылке:</p>
+<br style="margin: 0; padding: 0;" />
+<div style="text-align: center; width: 100%; margin: 0; padding: 0;" align="center">
+    <a href="<?= Url::toRoute(["/user/confirm", "token" => $userToken->token], true); ?>" style="color: #FFF; text-decoration: none; font-weight: bold; text-align: center; cursor: pointer; display: block !important; border-radius: 4px; background-image: none !important; background-color: #84bf76; margin: 0 0 10px; padding: 10px 16px;">Подтвердить</a>
+</div>
--- frontend/mail/layouts/html.php
+++ frontend/mail/layouts/html.php
@@ -8,17 +8,118 @@ use yii\helpers\Html;
  */
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
-<?= $content ?>
-<?php $this->endBody() ?>
-</body>
+    <head>
+        <meta name="viewport" content="width=device-width" />
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+
+    <body bgcolor="#FFFFFF" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; background-image: url('http://f-keeper.ru/mail/img/pattern.png'); margin: 0; padding: 0;">
+        <?php $this->beginBody() ?>
+        <div style="width: 600px; box-shadow: 0px 0px 18px 0px rgba(0, 0, 0, 0.32) !important; margin: 40px auto; padding: 0;">
+            <!-- HEADER -->
+            <table style="display: table; border-spacing: 0px; width: 100%; margin: 0; padding: 0;">
+                <tr style="margin: 0; padding: 0;">
+                    <td style="margin: 0; padding: 0;"></td>
+                    <td style="display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto; padding: 0;">
+
+                        <div style="max-width: 600px; display: block; background-color: #fff; margin: 0 auto; padding: 15px;">
+                            <table style="display: table; border-spacing: 0px; width: 100%; margin: 0; padding: 0;">
+                                <tr style="margin: 0; padding: 0;">
+                                    <td style="margin: 0; padding: 0;"><img src="http://f-keeper.ru/mail/img/logo.png" style="max-width: 100%; margin: 0; padding: 0;" /></td>
+                                    <td style="margin: 0; padding: 0;">
+                                        <ul style="font-weight: normal; font-size: 14px; line-height: 1.6; text-align: right; margin: 0 0 10px; padding: 0;">
+                                            <li style="display: inline-block; list-style-position: inside; margin: 8px 0 0 5px; padding: 0;"><a href="#d41d8cd98f00b204e9800998ecf8427e" style="color: #3f3e3e; margin: 0; padding: 0;">Вход / Регистрация</a></li>
+                                            <li style="display: inline-block; list-style-position: inside; margin: 8px 0 0 5px; padding: 0;"><a href="#d41d8cd98f00b204e9800998ecf8427e" style="color: #3f3e3e; margin: 0; padding: 0;">О компании</a></li>
+                                        </ul>
+                                    </td>
+
+                                </tr>
+                            </table>
+                        </div>
+
+                    </td>
+                    <td style="margin: 0; padding: 0;"></td>
+                </tr>
+            </table><!-- /HEADER -->
+
+
+            <!-- BODY -->
+            <table style="display: table; border-spacing: 0px; width: 100%; margin: 0; padding: 0;">
+                <tr style="margin: 0; padding: 0;">
+                    <td style="margin: 0; padding: 0;"></td>
+                    <td bgcolor="#FFFFFF" style="display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto; padding: 0;">
+
+                        <div style="max-width: 600px; display: block; background-color: #fff; margin: 0 auto; padding: 15px;">
+                            <table style="display: table; border-spacing: 0px; width: 100%; margin: 0; padding: 0;">
+                                <tr style="margin: 0; padding: 0;">
+                                    <td style="margin: 0; padding: 0;">
+                                        <?= $content ?>					
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+
+                    </td>
+                    <td style="margin: 0; padding: 0;"></td>
+                </tr>
+            </table><!-- /BODY -->
+            <!-- FOOTER -->
+            <!-- social & contact -->
+            <table width="100%" style="display: table; border-spacing: 0px; margin: 0; padding: 0;">
+                <tr style="margin: 0; padding: 0;">
+                    <td style="margin: 0; padding: 0;">
+                        <div style="margin: 0; padding: 0;">
+                            <ul style="width: 100%; text-align: center; font-weight: normal; font-size: 14px; line-height: 1.6; list-style-type: none; margin: 0 0 10px; padding: 0;">
+                                <li style="display: inline-block; text-align: center; list-style-position: inside; margin: 0 0 0 5px; padding: 0;"><img src="http://f-keeper.ru/mail/img/phone.png" alt="" style="max-width: 100%; margin: 0; padding: 0 5px 0 0;" /><a href="http://f-keeper.ru/mail/hero.html" style="color: #3f3e3e; margin: 0; padding: 0;">8-499-404-10-18</a></li>
+                                <li style="display: inline-block; text-align: center; list-style-position: inside; margin: 0 0 0 5px; padding: 0;"><img src="http://f-keeper.ru/mail/img/mail.png" alt="" style="max-width: 100%; margin: 0; padding: 0 5px 0 0;" /><a href="http://f-keeper.ru/mail/hero.html" style="color: #3f3e3e; margin: 0; padding: 0;">info@f-keeper.ru</a></li>
+                                <li style="display: inline-block; text-align: center; list-style-position: inside; margin: 0 0 0 5px; padding: 0;"><img src="http://f-keeper.ru/mail/img/web.png" alt="" style="max-width: 100%; margin: 0; padding: 0 5px 0 0;" /><a href="http://f-keeper.ru/mail/hero.html" style="color: #3f3e3e; margin: 0; padding: 0;">http://f-keeper.ru</a></li>
+                            </ul>
+
+                            <ul style="width: 100%; text-align: center; font-weight: normal; font-size: 14px; line-height: 1.6; list-style-type: none; margin: 0 0 10px; padding: 0;">
+                                <li style="display: inline-block; text-align: center; list-style-position: inside; margin: 0 0 0 5px; padding: 0;"><a href="http://f-keeper.ru/mail/hero.html" style="color: #3f3e3e; margin: 0; padding: 0;"><img src="http://f-keeper.ru/mail/img/yout.png" alt="" style="max-width: 100%; margin: 0; padding: 0 5px 0 0;" /></a></li>
+                                <li style="display: inline-block; text-align: center; list-style-position: inside; margin: 0 0 0 5px; padding: 0;"><a href="http://f-keeper.ru/mail/hero.html" style="color: #3f3e3e; margin: 0; padding: 0;"><img src="http://f-keeper.ru/mail/img/insta.png" alt="" style="max-width: 100%; margin: 0; padding: 0 5px 0 0;" /></a></li>
+                                <li style="display: inline-block; text-align: center; list-style-position: inside; margin: 0 0 0 5px; padding: 0;"><a href="http://f-keeper.ru/mail/hero.html" style="color: #3f3e3e; margin: 0; padding: 0;"><img src="http://f-keeper.ru/mail/img/face.png" alt="" style="max-width: 100%; margin: 0; padding: 0 5px 0 0;" /></a></li>
+                                <li style="display: inline-block; text-align: center; list-style-position: inside; margin: 0 0 0 5px; padding: 0;"><a href="http://f-keeper.ru/mail/hero.html" style="color: #3f3e3e; margin: 0; padding: 0;"><img src="http://f-keeper.ru/mail/img/twit.png" alt="" style="max-width: 100%; margin: 0; padding: 0 5px 0 0;" /></a></li>
+                                <li style="display: inline-block; text-align: center; list-style-position: inside; margin: 0 0 0 5px; padding: 0;"><a href="http://f-keeper.ru/mail/hero.html" style="color: #3f3e3e; margin: 0; padding: 0;"><img src="http://f-keeper.ru/mail/img/vk.png" alt="" style="max-width: 100%; margin: 0; padding: 0 5px 0 0;" /></a></li>
+                            </ul>
+                        </div>
+                        <span style="display: block; clear: both; margin: 0; padding: 0;"></span>	
+                    </td>
+                </tr>
+            </table><!-- /social & contact -->
+            <table style="display: table; border-spacing: 0px; width: 100%; clear: both !important; margin: 0; padding: 0;">
+
+                <tr style="margin: 0; padding: 0;">
+                    <td style="margin: 0; padding: 0;"></td>
+                    <td style="display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto; padding: 0;">
+
+                        <!-- content -->
+                        <div style="max-width: 600px; display: block; background-color: #fff; margin: 0 auto; padding: 15px;">
+                            <table style="display: table; border-spacing: 0px; width: 100%; margin: 0; padding: 0;">
+                                <tr style="margin: 0; padding: 0;">
+                                    <td align="center" style="margin: 0; padding: 0;">
+                                        <p style="font-size: 11px; font-weight: normal; line-height: 1.6; margin: 0 0 10px; padding: 0;">
+                                            © 2016 F-Keeper — ООО «Онлайн Маркет»
+                                        </p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div><!-- /content -->
+
+                    </td>
+                    <td style="margin: 0; padding: 0;"></td>
+                </tr>
+            </table><!-- /FOOTER -->
+
+
+
+        </div>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>

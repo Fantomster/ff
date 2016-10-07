a75819eb0559e597b5f6f71d8ba6632a43eddbbf
marshal <marshal@hh.ru>
2016-10-07 14:23:29 +0300

    update

--- frontend/views/vendor/catalogs/basecatalog.php
+++ frontend/views/vendor/catalogs/basecatalog.php
@@ -58,10 +58,10 @@ $exportColumns = [
         </div>
     <div class="tab-content">
         <div id="tabCatalog" class="tab-pane fade in active">
-                <div class="panel-body">
-    <div class="col-sm-4">
-        <?=Html::input('text', 'search', $searchString, ['class' => 'form-control pull-left','placeholder'=>'Поиск','id'=>'search']) ?>
-    </div>   
+            <div class="panel-body">
+                <div class="col-sm-4">
+                    <?=Html::input('text', 'search', $searchString, ['class' => 'form-control pull-left','placeholder'=>'Поиск','id'=>'search']) ?>
+                </div>   
                 <?=
                 Modal::widget([
                     'id' => 'add-product',
@@ -172,127 +172,7 @@ $exportColumns = [
                        ],
                    ])
                ?>
-            </div>
-            <?php 
-         /*   $gridColumnsBaseCatalog = [
-                            [
-                            'attribute' => 'article',
-                            'label'=>'Артикул',
-                            'value'=>'article',
-                            'contentOptions' => ['style' => 'vertical-align:middle;'],
-                            ],
-                            [
-                            'attribute' => 'product',
-                            'label'=>'Наименование',
-                            'value'=>'product',
-                            'contentOptions' => ['style' => 'vertical-align:middle;'],
-                            ],
-                            [
-                            'attribute' => 'units',
-                            'label'=>'Кратность',
-                            'value'=>'units',
-                            'contentOptions' => ['style' => 'vertical-align:middle;width:120px;'],    
-                            ],
-                            [
-                            'attribute' => 'category_id',
-                            'label'=>'Категория',
-                            'value'=>function ($data) {
-                            $data['category_id']==0 ? $category_name='':$category_name=Category::get_value($data['category_id'])->name;
-                            return $category_name;
-                            },
-                            'contentOptions' => ['style' => 'vertical-align:middle;'],
-                            ],
-                            [
-                            'attribute' => 'price',
-                            'label'=>'Цена',
-                            'value'=>'price',
-                            'contentOptions' => ['style' => 'vertical-align:middle;'],
-                            ],
-                            [
-                            'attribute' => 'status',
-                            'label'=>'Наличие',
-                            'format' => 'raw',
-                            'contentOptions' => ['style' => 'vertical-align:middle;width:100px;'],
-                            'value' => function ($data) {
-                                $link = CheckboxX::widget([
-                                    'name'=>'status_'.$data['id'],
-                                    'initInputType' => CheckboxX::INPUT_CHECKBOX,
-                                    'value'=>$data['status']==0 ? 0 : 1,
-                                    'autoLabel' => false,
-                                    'options'=>['id'=>'status_'.$data['id'], 'data-id'=>$data['id']],
-                                    'pluginOptions'=>[
-                                        'threeState'=>false,
-                                        'theme' => 'krajee-flatblue',
-                                        'enclosedLabel' => true,
-                                        'size'=>'lg',
-                                        ]
-                                ]);
-                                return $link;               
-                            },
-                            ],                           
-                    
-                    [
-                        'attribute' => '',
-                        'label' => '',
-                        'format' => 'raw',
-                        'contentOptions' => ['style' => 'width:50px;'],
-                        'value' => function ($data) {
-                            $link = Html::a('<i class="fa fa-pencil m-r-xs"></i>', ['/vendor/ajax-update-product', 'id' => $data['id']], [
-                                'data' => [
-                                'target' => '#add-product',
-                                'toggle' => 'modal',
-                                'backdrop' => 'static',
-                                          ],
-                                'class'=>'btn btn-sm btn-warning'
-
-                            ]);
-                            return $link;
-                        },
-
-                    ],
-                    [
-                        'attribute' => '',
-                        'label' => '',
-                        'format' => 'raw',
-                        'contentOptions' => ['style' => 'width:50px;'],
-                        'value' => function ($data) {
-                            $link = Html::button('<i class="fa fa-trash m-r-xs"></i>',[
-                                'class'=>'btn btn-sm btn-danger del-product',
-                                'data'=>['id'=>$data['id']],
-                            ]);
-                            return $link;
-                        },
-
-                    ],
-            ];
-            ?>
-           
-            
-                
-                <div class="panel-body">
-                    <div class="box-body table-responsive no-padding">               
-                    <?php Pjax::begin(['enablePushState' => false, 'id' => 'products-list',]); ?>
-                    <?=GridView::widget([
-                            'dataProvider' => $dataProvider,
-                            'filterModel' => $searchModel,
-                            'filterPosition' => false,
-                            'columns' => $gridColumnsBaseCatalog, 
-                            'tableOptions' => ['class' => 'table no-margin'],
-                            'options' => ['class' => 'table-responsive'],
-                            'bordered' => false,
-                            'striped' => true,
-                            'condensed' => false,
-                            'responsive' => false,
-                            'hover' => false,
-                            'export' => [
-                                'fontAwesome' => true,
-                            ],
-                    ]);
-                    ?>  
-                    <?php Pjax::end(); ?>
-                    </div>
-                </div> */
-            ?>
+                </div>
                 <div class="panel-body">
                     <?php Pjax::begin(['enablePushState' => false, 'id' => 'products-list',]); ?>
                         

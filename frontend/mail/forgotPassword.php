c69603b5a02548b900fe8db28c3a53cebc7c373d
sharaf <sharaf@El-Babuino>
2016-10-16 22:24:58 +0300

    mail

--- frontend/mail/forgotPassword.php
+++ frontend/mail/forgotPassword.php
@@ -1,16 +1,19 @@
 <?php
-
 use yii\helpers\Url;
-
-/**
- * @var string $subject
- * @var \amnah\yii2\user\models\User $user
- * @var \amnah\yii2\user\models\UserToken $userToken
- */
 ?>
-
-<h3><?= $subject ?></h3>
-
-<p><?= Yii::t("user", "Пройдите по ссылке для установки нового пароля:") ?></p>
-
-<p><?= Url::toRoute(["/user/reset", "token" => $userToken->token], true); ?></p>
+<p style="font-weight: normal; font-size: 14px; line-height: 1.6; margin: 0 0 10px; padding: 0;">Пройдите по ссылке для установки нового пароля:</p>
+<br style="margin: 0; padding: 0;" />
+<div style="text-align: center; width: 100%; margin: 0; padding: 0;" align="center">
+    <a href="<?= Url::toRoute(["/user/reset", "token" => $userToken->token], true); ?>" 
+       style="text-decoration: none;
+    color: #FFF;
+    background-color: #84bf76;
+    padding: 10px 16px;
+    font-weight: bold;
+    margin-right: 10px;
+    text-align: center;
+    cursor: pointer;
+    display: inline-block;
+    border-radius: 4px;
+    width: 80%;">Подтвердить</a>
+</div>

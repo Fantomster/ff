203631ee09295184c6c10c95c2a79a2a391dd56c
sharaf <sharaf@El-Babuino>
2016-10-17 12:32:42 +0300

    misc styles 2

--- frontend/views/order/_change-quantity.php
+++ frontend/views/order/_change-quantity.php
@@ -18,7 +18,7 @@ echo Html::hiddenInput('product_id', $product_id);
     <h4 class="modal-title">Изменить количество</h4>
 </div>
 <div class="modal-body form-inline" style="text-align: center;"> 
-    <?= Html::label("<b>$product_name</b> ($vendor_name)") ?>
+    <?= Html::label("<b>$product_name</b> ($vendor_name)", '', ['class' => 'padding-right-15']) ?>
                     <?=
                         TouchSpin::widget([
                             'name' => "quantity",
@@ -29,17 +29,17 @@ echo Html::hiddenInput('product_id', $product_id);
                                 'max' => PHP_INT_MAX,
                                 'step' => 1,
                                 'decimals' => 0,
-                                'buttonup_class' => 'btn btn-primary',
-                                'buttondown_class' => 'btn btn-info',
+                                'buttonup_class' => 'btn btn-default',
+                                'buttondown_class' => 'btn btn-default',
                                 'buttonup_txt' => '<i class="glyphicon glyphicon-plus-sign"></i>',
                                 'buttondown_txt' => '<i class="glyphicon glyphicon-minus-sign"></i>'
                             ],
-                           // 'options' => ['style' => 'width: 70px;'],
+                            'options' => ['style' => 'width: 100px;'],
                         ])
                         ?>
 </div>
 <div class="modal-footer">
-    <a href="#" class="btn btn-default" data-dismiss="modal">Закрыть</a>
+    <a href="#" class="btn btn-gray" data-dismiss="modal">Закрыть</a>
     <?= Html::button('Сохранить', ['class' => 'btn btn-success save', 'data' => ['dismiss' => "modal"]]) ?>
 </div>
 <?php

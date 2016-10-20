6242f0fae7601b190e8ca1b6302ab0810bbe5318
sharaf <sharaf@El-Babuino>
2016-10-20 14:41:31 +0300

    tmp

--- frontend/views/client/employees.php
+++ frontend/views/client/employees.php
@@ -1,8 +1,148 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+use yii\helpers\Html;
+use yii\grid\GridView;
+use common\models\User;
+use common\models\Role;
+use kartik\form\ActiveForm;
+use yii\bootstrap\Modal;
+use yii\helpers\Url;
+use yii\widgets\Pjax;
+use yii\widgets\Breadcrumbs;
 
+$user = new User();
+$role = new Role();
+
+$this->registerJs(
+        '$("document").ready(function(){
+            var timer = null;
+            $("#users-list").on("change keyup paste cut", "input", function() {
+                if (timer) {
+                    clearTimeout(timer);
+                }
+                timer = setTimeout(function() {
+                    $("#search-form").submit();
+                }, 300);
+            });
+            $("#users-list").on("pjax:complete", function() {
+                var searchInput = $("#search-string");
+                var strLength = searchInput.val().length * 2;
+                searchInput.focus();
+                searchInput[0].setSelectionRange(strLength, strLength);
+            });
+            $("#add-user").on("click", ".edit", function() {
+                var form = $("#user-form");
+                $.post(
+                    form.attr("action"),
+                    form.serialize()
+                )
+                .done(function(result) {
+                    form.replaceWith(result);
+                });
+                return false;
+            });
+            $("body").on("hidden.bs.modal", "#add-user", function() {
+                $(this).data("bs.modal", null);
+                $.pjax.reload({container: "#users-list"});
+            });
+        });'
+);
+?>
+<section class="content-header">
+    <h1>
+        Работники
+        <small>Список работников организации</small>
+    </h1>
+    <?=
+    Breadcrumbs::widget([
+        'options' => [
+            'class' => 'breadcrumb',
+        ],
+        'links' => [
+            'Настройки',
+            'Работники',
+        ],
+    ])
+    ?>
+</section>
+<section class="content">
+    <div class="box box-info settings">
+        <?php Pjax::begin(['enablePushState' => false, 'id' => 'users-list', 'timeout' => 3000]); ?>
+        <?=
+        Modal::widget([
+            'id' => 'add-user',
+            'clientOptions' => false,
+            'toggleButton' => [
+                'label' => ' Добавить пользователя',
+                'tag' => 'a',
+                'data-target' => '#add-user',
+                'class' => 'btn btn-primary pull-right margin-right-15',
+                'href' => Url::to(['/client/ajax-create-user']),
+            ],
+        ])
+        ?>
+        <?php
+        $form = ActiveForm::begin([
+                    'options' => [
+                        'data-pjax' => true,
+                        'id' => 'search-form',
+                        'class' => "navbar-form",
+                        'role' => 'search',
+                    ],
+                    'method' => 'get',
+        ]);
+        ?>
+        <?=
+        $form->field($searchModel, 'searchString')->textInput([
+            'id' => 'search-string',
+            'class' => 'form-control',
+            'placeholder' => 'Поиск'])->label(false)
+        ?>
+        <?php ActiveForm::end(); ?>
+        <!--?= Html::button('Добавить пользователя', ['id' => 'add-user', 'class' => 'btn btn-primary']) ?-->
+        <?=
+        GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'filterPosition' => false,
+            'options' => ['class' => 'table-responsive'],
+            'tableOptions' => ['class' => 'table table-bordered table-striped dataTable'],
+            'summary' => '',
+            'columns' => [
+                [
+                    'attribute' => 'profile.full_name',
+                    'format' => 'raw',
+                    'value' => function ($data) {
+                        $link = Html::a($data->profile->full_name, ['client/ajax-update-user', 'id' => $data->id], [
+                                    'data' => [
+                                        'target' => '#add-user',
+                                        'toggle' => 'modal',
+                                        'backdrop' => 'static',
+                                    ]
+                        ]);
+                        return $link;
+                    },
+                        ],
+                        'email',
+                        'role.name',
+                        [
+                            'attribute' => 'status',
+                            'label' => Yii::t('user', 'Status'),
+                            'filter' => $user::statusDropdown(),
+                            'value' => function($model, $index, $dataColumn) use ($user) {
+                                $statusDropdown = $user::statusDropdown();
+                                return $statusDropdown[$model->status];
+                            },
+                        ],
+                    ],
+                ]);
+                ?>
+                <?php Pjax::end(); ?>
+            </div>
+            <?php
+            Modal::begin([
+                'id' => 'user-edit',
+            ]);
+            ?>
+            <?php Modal::end(); ?>
+</section>
--- frontend/views/layouts/client/left.php
+++ frontend/views/layouts/client/left.php
@@ -51,7 +51,7 @@ $cartCount = $user->organization->getCartCount();
                             'options' => ['class' => "treeview"],
                             'items' => [
                                 ['label' => 'Общие', 'icon' => 'fa fa-circle-o', 'url' => ['client/settings']],
-                                ['label' => 'Работники', 'icon' => 'fa fa-circle-o', 'url' => ['client/settings']],
+                                ['label' => 'Работники', 'icon' => 'fa fa-circle-o', 'url' => ['client/employees']],
                             ]
                             ],
                         ['label' => 'Поддержка', 'icon' => 'fa fa-support', 'url' => ['client/support']],

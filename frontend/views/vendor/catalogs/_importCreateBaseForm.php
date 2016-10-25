02af0f3e71fdd0ef4d516e0993acdd7136167d10
marshal <marshal@hh.ru>
2016-10-25 12:19:48 +0300

    update

--- frontend/views/vendor/catalogs/_importCreateBaseForm.php
+++ frontend/views/vendor/catalogs/_importCreateBaseForm.php
@@ -13,12 +13,17 @@ use yii\web\View;
 ?>
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-    <h4 class="modal-title">Главный каталог</h4>
+    <h4 class="modal-title">ИМПОРТ ГЛАВНОГО КАТАЛОГА</h4>
 </div>
 <div class="modal-body">
-<?= $form->field($importModel, 'importFile')->fileInput() ?>
+<?= $form->field($importModel, 'importFile')->fileInput()->label('Выберите .XLSX') ?>
 </div>
 <div class="modal-footer">
+    <?= Html::a(
+       '<i class="fa fa-list-alt"></i> Скачать шаблон (XLS)',
+       Url::to('@web/upload/template.xlsx'),
+       ['class' => 'btn btn-default btn-sm pull-right','style' => ['margin-right'=>'10px;']]
+   ) ?> 
     <a href="#" class="btn btn-default" data-dismiss="modal">Отмена</a>
     <?= Html::submitButton('Импорт',['class' => 'btn btn-success import']) ?>
 </div>
--- frontend/views/vendor/catalogs/_importForm.php
+++ frontend/views/vendor/catalogs/_importForm.php
@@ -11,15 +11,18 @@ $this->registerCss('.select2-container .select2-selection--single .select2-selec
 <?php $form = ActiveForm::begin([
     'id' => 'import-form',
     'enableAjaxValidation' => false,
+    //'enableAjaxValidation' => true,
     'options' => ['enctype' => 'multipart/form-data'],
     'action' => Url::toRoute(['vendor/import-to-xls','id'=>Yii::$app->request->get('id')])])
 ?>
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-    <h4 class="modal-title">Импорт продуктов</h4>
+    <h4 class="modal-title">Импорт товаров</h4>
 </div>
 <div class="modal-body">
-<?= $form->field($importModel, 'importFile')->fileInput()->label('Загрузите свой каталог') ?>
+
+<?php //= Html::error($importModel, 'importFile', ['class' => 'help-block']); ?>
+<?= $form->field($importModel, 'importFile',['template' => "{error}\n{label}\n{hint}\n{input}"])->fileInput()->label('Выберите .XLSX') ?>
 <?php /*='<label class="control-label" for="importUnique">Уникальное поле</label>' ?>
 <?=Select2::widget([
     'name' => 'importUnique',
@@ -30,6 +33,11 @@ $this->registerCss('.select2-container .select2-selection--single .select2-selec
 ]) */?>
 </div>
 <div class="modal-footer">
+    <?= Html::a(
+                   '<i class="fa fa-list-alt"></i> Скачать шаблон (XLS)',
+                   Url::to('@web/upload/template.xlsx'),
+                   ['class' => 'btn btn-default pull-left','style' => ['margin-right'=>'10px;']]
+               ) ?> 
     <a href="#" class="btn btn-default" data-dismiss="modal">Отмена</a>
     <?= Html::submitButton('Импорт',['class' => 'btn btn-success import']) ?>
 </div>

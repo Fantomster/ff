0b13e93ddac2c919146a9712c8a2eb99af5046ad
sharaf <sharaf@El-Babuino>
2016-11-15 17:08:14 +0300

    wtf 3

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -1,15 +1,27 @@
 <?php
 
 namespace console\controllers;
+
 use Yii;
 use yii\console\Controller;
+
 //`php yii cron/count`
-class CronController extends Controller
-{
-   public function actionCount() {
-        $restourants =  rand(15, 25);
-        $suppliers =  rand(5, 10);
+class CronController extends Controller {
+
+    public function actionCount() {
+        $restourants = rand(15, 25);
+        $suppliers = rand(5, 10);
         $sql = "update main_counter set supp_count = supp_count + $suppliers, rest_count = rest_count + $restourants ";
         \Yii::$app->db->createCommand($sql)->execute();
-    } 
+    }
+
+    public function actionPlusOne() {
+        $query = "SELECT updated_at FROM main_counter LIMIT 1";
+        $latest = Yii::$app->db->createCommand($query)->queryScalar();
+        $now = new \DateTime();
+        $latest = new \DateTime($latest);
+        $randomInterval = rand(3, 15);
+        $interval = $now->diff($latest, true)->i;
+        echo "latest:".Yii::$app->formatter->asTime($latest, "php:j M Y, H:i:s").";now:".Yii::$app->formatter->asTime($now, "php:j M Y, H:i:s").";diff:".$interval."\n";
+    }
 }

d580965b91c9cadd58a2511b635d41000f3b0f0a
marshal <marshal@hh.ru>
2016-11-16 14:03:31 +0300

    update

--- frontend/controllers/ClientController.php
+++ frontend/controllers/ClientController.php
@@ -523,8 +523,8 @@ class ClientController extends DefaultController {
                     $org = $organization->name;
                     $categorys = $relationCategory['category_id'];
                     $get_supp_org_id = $check['org_id'];
-                    
-                    $sql = "insert into " . RelationSuppRest::tableName() . "(`rest_org_id`,`supp_org_id`,`created_at`) VALUES ($currentUser->organization_id,$get_supp_org_id,NOW())";
+                    $supp_base_cat_id = Catalog::find()->where(['supp_org_id'=>$get_supp_org_id, 'type'=>1])->one()->id;
+                    $sql = "insert into " . RelationSuppRest::tableName() . "(`rest_org_id`,`supp_org_id`,`created_at`,`invite`,`status`,`cat_id`) VALUES ($currentUser->organization_id,$get_supp_org_id,NOW(),1,1,$supp_base_cat_id)";
                     \Yii::$app->db->createCommand($sql)->execute();
                     if(!empty($categorys)){
                         foreach ($categorys as $arrCategorys) {

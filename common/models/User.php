db524402cc9089b8bdcfcde89c9cedb81b828b95
sharaf <sharaf@El-Babuino>
2016-11-28 18:35:17 +0300

    prod 28  nov 16 disable mail queue

--- common/models/User.php
+++ common/models/User.php
@@ -220,9 +220,9 @@ class User extends \amnah\yii2\user\models\User {
         /** @var Message $message */
 
         // modify view path to module views
-//        $mailer = Yii::$app->mailer;
-//        $oldViewPath = $mailer->viewPath;
-//        $mailer->viewPath = $this->module->emailViewPath;
+        $mailer = Yii::$app->mailer;
+        $oldViewPath = $mailer->viewPath;
+        $mailer->viewPath = $this->module->emailViewPath;
 
         // send email
         $user = $this;
@@ -230,18 +230,18 @@ class User extends \amnah\yii2\user\models\User {
         $email = $userToken->data ?: $user->email;
         $subject = Yii::$app->id . " - " . Yii::t("user", "Email Confirmation");
         
-        $result = Yii::$app->mailqueue->compose('confirmEmail', compact("subject", "user", "profile", "userToken"))
-                ->setTo($email)
-                ->setSubject($subject)
-                ->queue();
+//        $result = Yii::$app->mailqueue->compose('confirmEmail', compact("subject", "user", "profile", "userToken"))
+//                ->setTo($email)
+//                ->setSubject($subject)
+//                ->queue();
         
-//        $result = $mailer->compose('confirmEmail', compact("subject", "user", "profile", "userToken"))
-//            ->setTo($email)
-//            ->setSubject($subject)
-//            ->send();
+        $result = $mailer->compose('confirmEmail', compact("subject", "user", "profile", "userToken"))
+            ->setTo($email)
+            ->setSubject($subject)
+            ->send();
 
         // restore view path and return result
-        //$mailer->viewPath = $oldViewPath;
+        $mailer->viewPath = $oldViewPath;
         return $result;
     }
     

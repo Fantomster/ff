f29e20c6ce2cca30747eb03ec1378130f2a1c5b4
marshal <marshal@hh.ru>
2017-01-10 15:40:54 +0300

    up

--- console/migrations/m170109_142839_col_mp_show_price.php
+++ console/migrations/m170109_142839_col_mp_show_price.php
@@ -7,9 +7,8 @@ class m170109_142839_col_mp_show_price extends Migration
      // Use safeUp/safeDown to run migration code within a transaction
     public function safeUp()
     {
-        $this->addColumn('{{%catalog_base_goods}}', 'mp_show_price', $this->integer()->null());
+        $this->addColumn('{{%catalog_base_goods}}', 'mp_show_price', $this->integer()->notNull()->defaultValue(0));
     }
-
     public function safeDown()
     {
         $this->dropColumn('{{%catalog_base_goods}}', 'mp_show_price');

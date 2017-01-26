b7341a798d65a08cf4cbabc672cbbb221794dcf2
marshal <marshal@hh.ru>
2017-01-26 12:18:15 +0300

    up

--- market/views/layouts/left.php
+++ market/views/layouts/left.php
@@ -4,6 +4,41 @@ $count_products_from_mp = \common\models\CatalogBaseGoods::find()->where(['marke
 $left_menu_categorys = \common\models\MpCategory::find()->select('id,name,parent')->where(['parent'=>NULL])->asArray()->all();
 $left_menu_categorys_sub = \common\models\MpCategory::find()->select('id,name,parent,')->where('parent is not null')->asArray()->all();
 ?>
+<style>
+.panel-group {margin-bottom: 0px;overflow: hidden;}  
+.panel-group .panel{border-radius:0;border:0;border-bottom:1px solid #ddd}
+.panel-body { padding:0px; }
+.panel-body table tr td { padding-left: 25px;padding-top:11px;}
+.panel-body .table {margin-bottom: 0px;}
+.panel-group .panel+.panel {margin-top: 0px;}
+.panel-default>.panel-heading {
+    color: #333;
+    background-color: #fff;
+}
+#accordion {box-shadow: 0px 1px 3px rgba(9, 12, 17, 0.2);border-radius:3px;}
+.panel-default>.panel-heading {
+    padding-top:0;
+    padding-bottom:0;
+}
+.panel-collapse{
+        background: #f6f3f3;
+}
+.panel-default > .panel-heading + .panel-collapse > .panel-body {
+    border-top: none; 
+}
+.panel-default>.panel-heading h4{padding-top:15px;padding-bottom:10px;}
+.caret{margin-top: -21px;}
+.panel-default>.panel-heading a{
+    text-decoration: none;
+    color:#3f3e3e;
+}
+.panel-default>.panel-heading a:hover{
+    text-decoration: none;
+    color:#84bf76;
+}
+.panel-body table tr td a{ text-decoration: none;color:#3f3e3e;}
+.panel-body table tr td a:hover{ text-decoration: none;color:#84bf76;}
+</style>
 <div class="row">
     <div class="col-md-12">
         <h3>Каталог <span class="badge pull-right"><?=$count_products_from_mp;?> товаров</span></h3>  
@@ -11,29 +46,45 @@ $left_menu_categorys_sub = \common\models\MpCategory::find()->select('id,name,pa
 </div>
 <div class="row">
     <div class="col-md-12">
-      <div class="category">
-        <ul class="list-unstyled">
-<?php
-foreach($left_menu_categorys as $row){
-    echo '<div class="dropdown">
-              <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown">'.$row['name'].' 
+      <div class="panel-group" id="accordion">
+        <?php 
+        $i = 0;
+        foreach($left_menu_categorys as $row){
+        $i++;
+        ?>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+              <a data-toggle="collapse" data-parent="#accordion" href="#coll<?= $i ?>">
+                <h4 class="panel-title">
+                    <?=$row['name']?>
+                </h4>
                 <span class="caret pull-right"></span>
-              </button>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                <div class="col-sm-12 no-padding">
-                  <div class="sub-cat">
-                    <ul class="list-unstyled">';
-    foreach($left_menu_categorys_sub as $row2){
-        if($row['id'] == $row2['parent']){
-            echo     '<li><a href="?r=site/category&id=' . $row2['id'] . '" title="">'.$row2['name'].'</a></li>';
-        }
-    }
-            echo   '</ul>
-                  </div>
+              </a>
+            </div>
+            <div id="coll<?= $i ?>" class="panel-collapse collapse">
+                <?php
+                foreach($left_menu_categorys_sub as $row2){
+                    if($row['id'] == $row2['parent']){
+                ?>
+                <div class="panel-body">
+                    <table class="table">
+                        <tr>
+                            <td>
+                                <?='<a href="?r=site/category&id=' . $row2['id'] . '" title="">'.$row2['name'].'</a>'?>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
-              </div>
-          </div>';}?>
-        </ul>
+                <?php 
+                    }
+                }
+                ?>
+            </div>
+        </div>
+        <?php
+        } 
+        ?>
       </div>
+      
     </div>
 </div>
---
+++ market/views/layouts/left_bak.php
@@ -0,0 +1,39 @@
+<?php
+use yii\helpers\Html;
+$count_products_from_mp = \common\models\CatalogBaseGoods::find()->where(['market_place'=>1])->count();
+$left_menu_categorys = \common\models\MpCategory::find()->select('id,name,parent')->where(['parent'=>NULL])->asArray()->all();
+$left_menu_categorys_sub = \common\models\MpCategory::find()->select('id,name,parent,')->where('parent is not null')->asArray()->all();
+?>
+<div class="row">
+    <div class="col-md-12">
+        <h3>Каталог <span class="badge pull-right"><?=$count_products_from_mp;?> товаров</span></h3>  
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+      <div class="category">
+        <ul class="list-unstyled">
+<?php
+foreach($left_menu_categorys as $row){
+    echo '<div class="dropdown">
+              <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown">'.$row['name'].' 
+                <span class="caret pull-right"></span>
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <div class="col-sm-12 no-padding">
+                  <div class="sub-cat">
+                    <ul class="list-unstyled">';
+    foreach($left_menu_categorys_sub as $row2){
+        if($row['id'] == $row2['parent']){
+            echo     '<li><a href="?r=site/category&id=' . $row2['id'] . '" title="">'.$row2['name'].'</a></li>';
+        }
+    }
+            echo   '</ul>
+                  </div>
+                </div>
+              </div>
+          </div>';}?>
+        </ul>
+      </div>
+    </div>
+</div>

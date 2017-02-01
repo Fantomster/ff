aec90b949e8fa798b6233e4720bf7519992fc6fa
sharaf <sharaf@El-Babuino>
2017-02-01 15:38:45 +0300

    organizations white list (for f-market)

---
+++ common/models/WhiteList.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "white_list".
+ *
+ * @property integer $id
+ * @property integer $organization_id
+ * @property string $info
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property Organization $organization
+ */
+class WhiteList extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'white_list';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['organization_id'], 'required'],
+            [['organization_id'], 'integer'],
+            [['info'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['organization_id'], 'exist', 'skipOnError' => true, 'targetClass' => Organization::className(), 'targetAttribute' => ['organization_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'organization_id' => 'Organization ID',
+            'info' => 'Info',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrganization()
+    {
+        return $this->hasOne(Organization::className(), ['id' => 'organization_id']);
+    }
+}

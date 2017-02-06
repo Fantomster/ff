cb22d0ac3bc9393757f487cde10a2edea841bd43
sharaf <sharaf@El-Babuino>
2017-02-06 10:43:06 +0300

    white list show type

--- backend/views/white-list/index.php
+++ backend/views/white-list/index.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\WhiteListSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -13,25 +14,32 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="white-list-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]);  ?>
 
-<?php Pjax::begin(); ?>    <?= GridView::widget([
+    <?php Pjax::begin(); ?>    <?=
+    GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
             'organization_id',
+            [
+                'attribute' => 'org_type_id',
+                'value' => 'organization.type.name',
+                'label' => 'Тип',
+                'filter' => common\models\OrganizationType::getList(),
+            ],
             [
                 'format' => 'raw',
                 'attribute' => 'org_name',
                 'value' => function ($data) {
                     return Html::a($data['organization']['name'], ['organization/view', 'id' => $data['organization_id']]);
                 },
-                'label' => 'Название организации',
-            ],
-            'created_at',
-            'updated_at',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-<?php Pjax::end(); ?></div>
+                        'label' => 'Название организации',
+                    ],
+                    'created_at',
+                    'updated_at',
+                    ['class' => 'yii\grid\ActionColumn'],
+                ],
+            ]);
+            ?>
+            <?php Pjax::end(); ?></div>

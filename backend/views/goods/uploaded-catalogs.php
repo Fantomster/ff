7d9cfd18bf44db92f5f8227a4845c56a92a7a6e6
sharaf <sharaf@El-Babuino>
2017-02-07 18:53:02 +0300

    upload omgwtf

--- backend/views/goods/uploaded-catalogs.php
+++ backend/views/goods/uploaded-catalogs.php
@@ -18,12 +18,15 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'format' => 'raw',
                 'value' => function ($data) {
-                    return Html::a('Каталог', $data->getUploadUrl('uploaded_catalog'));
+                    return Html::a($data->client->name, ['organization/view', 'id' => $data->rest_org_id]);
                 },
                 'label' => 'Ресторан',
             ],
             [
-                'value' => 'vendor.name',
+                'format' => 'raw',
+                'value' => function ($data) {
+                    return Html::a($data->vendor->name, ['organization/view', 'id' => $data->supp_org_id]);
+                },
                 'label' => 'Поставщик',
             ],
             [

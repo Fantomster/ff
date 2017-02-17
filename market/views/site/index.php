95d6ac7759a359b8cfe91dc1f3c1b5100183992c
marshal <marshal@hh.ru>
2017-02-17 14:34:04 +0300

    up

--- market/views/site/index.php
+++ market/views/site/index.php
@@ -79,23 +79,23 @@ $this->title = 'F-MARKET главная';
         ?>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 min-padding">
         <div class="mp-suppiler-block animated fadeIn">
-          <a href="<?=Url::to(['/site/supplier', 'id' => $row->vendor->id]);?>">
-            <img class="supplier-image  animated fadeInUp" src="<?= empty($row->vendor->picture) ? \common\models\Organization::DEFAULT_VENDOR_AVATAR : $row->vendor->pictureUrl ?>">
+          <a href="<?=Url::to(['/site/supplier', 'id' => $row->id]);?>">
+            <img class="supplier-image  animated fadeInUp" src="<?= empty($row->picture) ? \common\models\Organization::DEFAULT_VENDOR_AVATAR : $row->pictureUrl ?>">
           </a>
           <div class="row">
             <div class="col-md-12">
               <div class="supplier-title">
-                <a href="<?=Url::to(['/site/supplier', 'id' => $row->vendor->id]);?>">
-                <h3><?=$row->vendor->name;?></h3>
+                <a href="<?=Url::to(['/site/supplier', 'id' => $row->id]);?>">
+                <h3><?=$row->name;?></h3>
                 </a>
               </div>
               <div class="supplier-category">
-                <h5><?=!empty($row->vendor->city) ? $row->vendor->city : '&nbsp;';?></h5>
+                <h5><?=!empty($row->city) ? $row->city : '&nbsp;';?></h5>
               </div>
             </div>
             <div class="col-md-12">
               <div class="supplier-button">
-                <a href="#" class="btn btn-100 btn-success invite-vendor" data-vendor-id="<?= $row->vendor->id ?>" style="width: 100%">ДОБАВИТЬ</a>
+                <a href="#" class="btn btn-100 btn-success invite-vendor" data-vendor-id="<?= $row->id ?>" style="width: 100%">ДОБАВИТЬ</a>
               </div>
             </div>
           </div>

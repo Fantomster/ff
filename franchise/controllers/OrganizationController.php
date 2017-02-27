33da7861a16d98cf0329c0df51f5f9be4ce60edd
sharaf <sharaf@El-Babuino>
2017-02-27 14:30:12 +0300

    fr access rules

--- franchise/controllers/OrganizationController.php
+++ franchise/controllers/OrganizationController.php
@@ -22,6 +22,43 @@ use common\models\FranchiseeAssociate;
 class OrganizationController extends DefaultController {
 
     /**
+     * @inheritdoc
+     */
+    public function behaviors() {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                // We will override the default rule config with the new AccessRule class
+                'ruleConfig' => [
+                    'class' => AccessRule::className(),
+                ],
+                'only' => ['index', 'clients', 'vendors', 'ajax-show-client', 'ajax-show-vendor', 'create-client', 'create-vendor'],
+                'rules' => [
+                    [
+                        'actions' => ['index', 'clients', 'vendors', 'ajax-show-client', 'ajax-show-vendor', 'create-client', 'create-vendor'],
+                        'allow' => true,
+                        'roles' => [
+                            Role::ROLE_FRANCHISEE_OWNER,
+                            Role::ROLE_FRANCHISEE_OPERATOR,
+                            Role::ROLE_FRANCHISEE_ACCOUNTANT,
+                            Role::ROLE_ADMIN,
+                        ],
+                    ],
+                ],
+            /* 'denyCallback' => function($rule, $action) {
+              throw new HttpException(404 ,'Нет здесь ничего такого, проходите, гражданин');
+              } */
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
      * Displays clients list
      * 
      * @return mixed

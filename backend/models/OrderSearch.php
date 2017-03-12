04c98d1a8167544615e2458f2d053e35230d1536
sharaf <sharaf@El-Babuino>
2017-03-12 15:54:32 +0300

    admin order filter

--- backend/models/OrderSearch.php
+++ backend/models/OrderSearch.php
@@ -18,6 +18,7 @@ class OrderSearch extends Order {
     public $vendor_name;
     public $client_manager;
     public $vendor_manager;
+    private $blacklist = '(1,2,5,16,63,88,99,100,106,108,111,114,116,272,284,333,440,449,526,673,784,824,1037)'; 
 
     /**
      * @inheritdoc
@@ -48,6 +49,7 @@ class OrderSearch extends Order {
     public function search($params) {
         $profileTable = Profile::tableName();
         $organizationTable = Organization::tableName();
+        $orderTable = Order::tableName();
 
         $query = Order::find();
 
@@ -75,6 +77,8 @@ class OrderSearch extends Order {
                 ], true);
 
         // add conditions that should always apply here
+        $query->where("`$orderTable`.client_id not in $this->blacklist");
+        $query->andWhere("`$orderTable`.status <> " . Order::STATUS_FORMING);
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,

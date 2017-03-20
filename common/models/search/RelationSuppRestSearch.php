4ebf7105fcc13026dcd01662b90281c080feefd6
sharaf <sharaf@El-Babuino>
2017-03-20 15:17:38 +0300

    oy vey

---
+++ common/models/search/RelationSuppRestSearch.php
@@ -0,0 +1,14 @@
+<?php
+
+use common\models\RelationSuppRest;
+
+namespace common\models\search;
+
+/**
+ * Description of RelationSuppRest
+ *
+ * @author sharaf
+ */
+class RelationSuppRestSearch extends RelationSuppRest {
+    //put your code here
+}

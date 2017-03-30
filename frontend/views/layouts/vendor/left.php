e4768f27e4cd166135baf1d6ce625cc2593b7462
sharaf <sharaf@El-Babuino>
2017-03-30 19:13:20 +0300

    menu & fontawesome batshit

--- frontend/views/layouts/vendor/left.php
+++ frontend/views/layouts/vendor/left.php
@@ -11,38 +11,38 @@ $cartCount = $user->organization->getCartCount();
 
 $menuItems = [
     ['label' => 'НАВИГАЦИЯ', 'options' => ['class' => 'header']],
-    ['label' => 'Рабочий стол', 'icon' => 'fa fa-home', 'url' => ['vendor/index']],
+    ['label' => 'Рабочий стол', 'icon' => 'home', 'url' => ['vendor/index']],
     [
         'label' => 'Заказы',
-        'icon' => 'fa fa-history',
+        'icon' => 'history',
         'url' => ['order/index'],
         'template' => '<a href="{url}">{icon}{label}<span class="pull-right-container"><span class="label bg-yellow pull-right new-orders-count">' . ($newOrdersCount ? $newOrdersCount : '') . '</span></span></a>',
     ],
-    ['label' => 'Мои каталоги', 'icon' => 'fa fa-list-alt', 'url' => ['vendor/catalogs'], 'options' => ['class' => 'hidden-xs']],
-//                        ['label' => 'Сообщения' . Html::tag('span', 4, ['class' => 'label label-danger pull-right']), 'icon' => 'fa fa-envelope', 'url' => ['vendor/messages']],
-    ['label' => 'F-MARKET<sub class="sub-new">NEW</sub>', 'icon' => 'fa fa-shopping-cart', 'url' => 'http://market.f-keeper.ru', 'options' => ['class' => 'l-fmarket']],
+    ['label' => 'Мои каталоги', 'icon' => 'list-alt', 'url' => ['vendor/catalogs'], 'options' => ['class' => 'hidden-xs']],
+//                        ['label' => 'Сообщения' . Html::tag('span', 4, ['class' => 'label label-danger pull-right']), 'icon' => 'envelope', 'url' => ['vendor/messages']],
+    ['label' => 'F-MARKET<sub class="sub-new">NEW</sub>', 'icon' => 'shopping-cart', 'url' => 'http://market.f-keeper.ru', 'options' => ['class' => 'l-fmarket']],
     [
         'label' => 'Мои клиенты',
-        'icon' => 'fa fa-users',
+        'icon' => 'users',
         'url' => ['vendor/clients'],
         'template' => '<a href="{url}">{icon}{label}<span class="pull-right-container"><span class="label bg-yellow pull-right">' . ($newClientCount ? $newClientCount : '') . '</span></span></a>',
     ],
-    ['label' => 'Аналитика', 'icon' => 'fa fa-signal', 'url' => ['vendor/analytics'], 'options' => ['class' => 'hidden-xs']],
-    ['label' => 'Обучающие видео', 'icon' => 'fa fa-play-circle-o', 'url' => ['vendor/tutorial', 'video' => 'video']],
-    //['label' => 'Мои акции', 'icon' => 'fa fa-ticket', 'url' => ['vendor/events']],
-    ['label' => 'Новости', 'icon' => 'fa fa-newspaper-o', 'url' => 'http://blog.f-keeper.ru?news', 'options' => ['class' => 'hidden-xs']],
-    //['label' => 'Поддержка', 'icon' => 'fa fa-support', 'url' => ['vendor/support']],
+    ['label' => 'Аналитика', 'icon' => 'signal', 'url' => ['vendor/analytics'], 'options' => ['class' => 'hidden-xs']],
+    ['label' => 'Обучающие видео', 'icon' => 'play-circle-o', 'url' => ['vendor/tutorial', 'video' => 'video']],
+    //['label' => 'Мои акции', 'icon' => 'ticket', 'url' => ['vendor/events']],
+    ['label' => 'Новости', 'icon' => 'newspaper-o', 'url' => 'http://blog.f-keeper.ru?news', 'options' => ['class' => 'hidden-xs']],
+    //['label' => 'Поддержка', 'icon' => 'support', 'url' => ['vendor/support']],
 ];
 if (Yii::$app->user->can('manage')) {
     $menuItems[] = [
         'label' => 'Настройки',
-        'icon' => 'fa fa-gears',
+        'icon' => 'gears',
         'url' => '#',
         'options' => ['class' => "treeview hidden-xs"],
         'items' => [
-            ['label' => 'Общие', 'icon' => 'fa fa-circle-o', 'url' => ['vendor/settings']],
-            ['label' => 'Сотрудники', 'icon' => 'fa fa-circle-o', 'url' => ['vendor/employees']],
-            ['label' => 'Доставка', 'icon' => 'fa fa-circle-o', 'url' => ['vendor/delivery']],
+            ['label' => 'Общие', 'icon' => 'circle-o', 'url' => ['vendor/settings']],
+            ['label' => 'Сотрудники', 'icon' => 'circle-o', 'url' => ['vendor/employees']],
+            ['label' => 'Доставка', 'icon' => 'circle-o', 'url' => ['vendor/delivery']],
         ]
     ];
 }

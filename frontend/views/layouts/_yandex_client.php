30fbaa8a5cabc757ba0d263315d10dd0752c884c
sharaf <sharaf@El-Babuino>
2017-05-15 16:31:10 +0300

    new metrics

--- frontend/views/layouts/_yandex_client.php
+++ frontend/views/layouts/_yandex_client.php
@@ -1,30 +1,29 @@
-﻿<!-- Yandex.Metrika counter -->
+<!-- Yandex.Metrika counter -->
 <script type="text/javascript">
     (function (d, w, c) {
         (w[c] = w[c] || []).push(function() {
             try {
-                w.yaCounter42554574 = new Ya.Metrika({
-                    id:42554574,
+                w.yaCounter44637517 = new Ya.Metrika({
+                    id:44637517,
                     clickmap:true,
                     trackLinks:true,
                     accurateTrackBounce:true,
-                    webvisor:true,
-                    trackHash:true
+                    webvisor:true
                 });
             } catch(e) { }
         });
-
+ 
         var n = d.getElementsByTagName("script")[0],
             s = d.createElement("script"),
             f = function () { n.parentNode.insertBefore(s, n); };
         s.type = "text/javascript";
         s.async = true;
         s.src = "https://mc.yandex.ru/metrika/watch.js";
-
+ 
         if (w.opera == "[object Opera]") {
             d.addEventListener("DOMContentLoaded", f, false);
         } else { f(); }
     })(document, window, "yandex_metrika_callbacks");
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/42554574" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/44637517" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
--- frontend/views/layouts/_yandex_vendor.php
+++ frontend/views/layouts/_yandex_vendor.php
@@ -1,30 +1,29 @@
-﻿<!-- Yandex.Metrika counter -->
+<!-- Yandex.Metrika counter -->
 <script type="text/javascript">
     (function (d, w, c) {
         (w[c] = w[c] || []).push(function() {
             try {
-                w.yaCounter42569594 = new Ya.Metrika({
-                    id:42569594,
+                w.yaCounter44637583 = new Ya.Metrika({
+                    id:44637583,
                     clickmap:true,
                     trackLinks:true,
                     accurateTrackBounce:true,
-                    webvisor:true,
-                    trackHash:true
+                    webvisor:true
                 });
             } catch(e) { }
         });
-
+ 
         var n = d.getElementsByTagName("script")[0],
             s = d.createElement("script"),
             f = function () { n.parentNode.insertBefore(s, n); };
         s.type = "text/javascript";
         s.async = true;
         s.src = "https://mc.yandex.ru/metrika/watch.js";
-
+ 
         if (w.opera == "[object Opera]") {
             d.addEventListener("DOMContentLoaded", f, false);
         } else { f(); }
     })(document, window, "yandex_metrika_callbacks");
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/42569594" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/44637583" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->

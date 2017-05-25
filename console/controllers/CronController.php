df831ef036a5f8b5476753caa3e4da6c925f9dbc
marshal <marshal@hh.ru>
2017-05-25 18:37:27 +0300

    fix

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -224,52 +224,51 @@ class CronController extends Controller {
         }
     }
     public function actionMappingOrganizationFromGoogleApiMaps() {
-        $model = Organization::find()->where('lng is not null and lat is not null')->limit(100)->all();
+        $model = Organization::find()->where('lng is not null and lat is not null and administrative_area_level_1 is null')->limit(100)->all();
         foreach($model as $s){
             $address_url = 'https://maps.googleapis.com/maps/api/geocode/json?key='.Yii::$app->params['google-api']['key-id'].'&latlng=' . $s->lat . ',' . $s->lng . '&language=ru&sensor=false';
             $address_json = json_decode(file_get_contents($address_url));
-            //$address_data = $address_json->results[0]->address_components;
-            if(empty($address_json->results)){
-            var_dump($address_json);
+            if(!empty($address_json->results)){
+            $address_data = $address_json->results[0]->address_components;
+            $location = array();
+            foreach ($address_data as $component) {
+              switch ($component->types) {
+                case in_array('street_number', $component->types):
+                  $location['street_number'] = $component->long_name;
+                  break;
+                case in_array('route', $component->types):
+                  $location['street'] = $component->long_name;
+                  break;
+                case in_array('sublocality', $component->types):
+                  $location['sublocality'] = $component->long_name;
+                  break;
+                case in_array('locality', $component->types):
+                  $location['locality'] = $component->long_name;
+                  break;
+                case in_array('administrative_area_level_2', $component->types):
+                  $location['admin_2'] = $component->long_name;
+                  break;
+                case in_array('administrative_area_level_1', $component->types):
+                  $location['admin_1'] = $component->long_name;
+                  break;
+                case in_array('postal_code', $component->types):
+                  $location['postal_code'] = $component->long_name;
+                  break;
+                case in_array('country', $component->types):
+                  $location['country'] = $component->long_name;
+                  break;
+              }
+
             }
-//            $location = array();
-//            foreach ($address_data as $component) {
-//              switch ($component->types) {
-//                case in_array('street_number', $component->types):
-//                  $location['street_number'] = $component->long_name;
-//                  break;
-//                case in_array('route', $component->types):
-//                  $location['street'] = $component->long_name;
-//                  break;
-//                case in_array('sublocality', $component->types):
-//                  $location['sublocality'] = $component->long_name;
-//                  break;
-//                case in_array('locality', $component->types):
-//                  $location['locality'] = $component->long_name;
-//                  break;
-//                case in_array('administrative_area_level_2', $component->types):
-//                  $location['admin_2'] = $component->long_name;
-//                  break;
-//                case in_array('administrative_area_level_1', $component->types):
-//                  $location['admin_1'] = $component->long_name;
-//                  break;
-//                case in_array('postal_code', $component->types):
-//                  $location['postal_code'] = $component->long_name;
-//                  break;
-//                case in_array('country', $component->types):
-//                  $location['country'] = $component->long_name;
-//                  break;
-//              }
-//
-//            }
         
-//        $country = $location['country'];
-//        $locality = $location['locality'];
-//        $administrative_area_level_1 = $location['admin_1'];
-//        
-//        $organization = Organization::findOne($s->id);
-//        $organization->administrative_area_level_1 = $administrative_area_level_1;
-//        $organization->save();
+        $country = $location['country'];
+        $locality = $location['locality'];
+        $administrative_area_level_1 = $location['admin_1'];
+        
+        $organization = Organization::findOne($s->id);
+        $organization->administrative_area_level_1 = $administrative_area_level_1;
+        $organization->save();
+            }
         }
     }
     public function actionSendMailNewRequests() {

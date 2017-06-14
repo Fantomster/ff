6f67bddf6a8cea6df7e673cb938f4bf54fa477c2
xsupervisor <xsupervisor@f-keeper.ru>
2017-06-14 22:54:56 +0300

    ge

--- api/modules/v1/modules/restor/views/tasks/index.php
+++ api/modules/v1/modules/restor/views/tasks/index.php
@@ -8,7 +8,9 @@ echo $this->render('/default/_menu.php');
 // echo "<br><br>";
 // print_r($xml);
 
-VarDumper::dump($array);
+echo '<pre>'; 
+print_r($array); 
+echo '</pre>';
 
 // print_r($array);
 // echo "<h2>".$res."</h2>";

d709c1e060ebacb7859181750b9d7b33937f4cc9
Sergey <sims2planet@mail.ru>
2017-06-21 18:31:06 +0300

    фикс

--- api/modules/v1/Module.php
+++ api/modules/v1/Module.php
@@ -19,7 +19,7 @@ class Module extends \yii\base\Module
                 'class' => 'api\modules\v1\modules\supp\Module',
             ],
             
-            'supp' => [
+            'mobile' => [
                 // здесь имеет смысл использовать более лаконичное пространство имен
                 'class' => 'api\modules\v1\modules\mobile\Module',
             ],

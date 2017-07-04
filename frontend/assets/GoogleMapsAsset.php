d14540bc78003d1e63608198ce7e8e2c87c6bfd9
elbabuino <elbabuino@el-babuino>
2017-07-04 18:55:12 +0300

    omg

--- frontend/assets/GoogleMapsAsset.php
+++ frontend/assets/GoogleMapsAsset.php
@@ -16,11 +16,12 @@ class GoogleMapsAsset extends AssetBundle {
     public static function register($view) {
         parent::register($view);
         $gpJsLink = 'https://maps.googleapis.com/maps/api/js?' . http_build_query(array(
+                    'v' => '3.20',
                     'libraries' => 'places',
                     'key' => Yii::$app->params['google-api']['key-id'],
                     'language' => Yii::$app->params['google-api']['language'],
                     'callback' => 'initMap'
         ));
-        Yii::$app->view->registerJsFile($gpJsLink, ['depends' => [yii\web\JqueryAsset::className()], 'async' => true, 'defer' => true]);
+        Yii::$app->view->registerJsFile($gpJsLink, ['depends' => [yii\web\JqueryAsset::className()], 'async' => true, 'defer' => true, 'position' => \yii\web\View::POS_END]);
     }
 }

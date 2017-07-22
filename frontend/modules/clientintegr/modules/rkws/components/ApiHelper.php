e9cd73354556bb8dffe355026cb28a92ca4a291f
xsupervisor <xsupervisor@f-keeper.ru>
2017-07-23 00:46:08 +0300

    check attr

--- frontend/modules/clientintegr/modules/rkws/components/ApiHelper.php
+++ frontend/modules/clientintegr/modules/rkws/components/ApiHelper.php
@@ -286,7 +286,7 @@ class ApiHelper  {
           
                 $atts_object = $r->attributes();
                 $atts_array = (array) $atts_object;
-                $arr[$r->getName()][]=$atts_array;
+                $arr[$r->getName()]=$atts_array;
                 
                 $arr[$r->getName()][] = self::xml2array($r);
             }//Endif

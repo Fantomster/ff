fb7606cf5da56e7f991b62995b83d069e98d15d5
marshal <marshal@hh.ru>
2017-07-22 15:05:34 +0300

    fix ilia

--- market/controllers/SiteController.php
+++ market/controllers/SiteController.php
@@ -301,15 +301,31 @@ class SiteController extends Controller {
         $params = [
             'filtered' => [
                 'query' => [
-                    'match' => [
+                    /*'match' => [
                         'supplier_name' => [
                            'query' => $search,
-                           'fuzziness' => 'auto',
-                           /*'type' =>'text_phrase',
-                           'max_expansions' =>20,
-                           'prefix_length' => 0,
-                           'fuzziness' => 0.8,*/
+                           'fuzziness' => 10,
                         ]
+                    ]*/
+                    'fuzzy' => [
+                        'supplier_name' => [
+                            'value' => '$search',
+                            'fuzziness' => 10, // этот параметр для вашей задачи нужно выставлять повыше
+                            'prefix_length' => 2, // будет равняться кол-ву уже введенных символов
+                            "max_expansions"=> 100 // также выставляется повыше
+                        ]
+                    ]
+                ],
+                'highlight'=>[
+                    'fields'=>[
+                      'supplier_name'=>[
+                        'post_tags'=>[
+                          "</span>"
+                        ],
+                        'pre_tags'=>[
+                          '<span class=\"vulners-highlight\">'
+                        ]
+                      ]
                     ]
                 ],
                 'filter' => [
@@ -897,21 +913,29 @@ class SiteController extends Controller {
             $params_suppliers = [
                 'filtered' => [
                     'query' => [
-                        'more_like_this' => [
-                                'fields'=>['supplier_name'],
-                                'like' => $search,
-                                'min_term_freq' => 1,
-                                'min_doc_freq' => 1,
-                                'max_query_terms' => 12,
-                        ],
-                        /*'match' => [
+                        'match' => [
                             'supplier_name' => [
-                                'query' => $search,
-                                'fuzziness' => 'AUTO',
-                            ]   
+                               'query' => $search,
+                               'fuzziness' => 'auto',
+                               'operator' =>  'and'
+                            ]
+                        ]
+                        /*'fuzzy' => [
+                            'supplier_name' => [
+                                'value' => $search,
+                                'fuzziness' => 20, // этот параметр для вашей задачи нужно выставлять повыше
+                                'prefix_length' => 2, // будет равняться кол-ву уже введенных символов
+                                "max_expansions"=> 100 // также выставляется повыше
+                            ]
                         ]*/
                     ],
-                    
+                    'highlight' => [
+                        'pre_tags' => ['<em>'],
+                        'post_tags' => ['</em>'],
+                        'fields' => [
+                            'supplier_name'
+                        ]
+                    ],
                     'filter' => [
                         'bool' => [
                             'must_not' => [

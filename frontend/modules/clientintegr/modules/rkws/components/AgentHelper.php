9bd9d89f942b8a6707e08ce8c6dd339babfa7562
xsupervisor <xsupervisor@f-keeper.ru>
2017-07-23 03:55:24 +0300

    check2

--- frontend/modules/clientintegr/modules/rkws/components/AgentHelper.php
+++ frontend/modules/clientintegr/modules/rkws/components/AgentHelper.php
@@ -109,7 +109,7 @@ class AgentHelper extends AuthHelper {
             
             $amodel = new RkAgent();
             
-            $amodel->acc = parent::org;
+            $amodel->acc = User::findOne(Yii::$app->user->id)->organization_id;
             $amodel->rid = $a['rid'];
             $amodel->denom = $a['name'];
             $amodel->agent_type = $a['type'];

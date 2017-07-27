673e1e7d308c6ce64681ad812553445f58baaa42
elbabuino <elbabuino@el-babuino>
2017-07-27 13:11:13 +0300

    wtf

--- backend/controllers/SnsEndpointController.php
+++ backend/controllers/SnsEndpointController.php
@@ -3,6 +3,9 @@ namespace backend\controllers;
 
 use Yii;
 use yii\web\Controller;
+use Aws\Sns\MessageValidator\Message;
+use Aws\Sns\MessageValidator\MessageValidator;
+use Aws\Sns\Exception\SnsException;
 
 /**
  * Description of SnsEndpointController
@@ -13,5 +16,7 @@ class SnsEndpointController extends Controller {
     //put your code here
     public function actionBounce() {
         //
+        $message = MessageValidator\Message::fromRawPostData();
+        $validator 
     }
 }

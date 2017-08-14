5e81ee5aa20738192b54e4d176cc89687480aa1c
xsupervisor <xsupervisor@f-keeper.ru>
2017-08-14 18:09:35 +0300

    smfix55

--- frontend/modules/clientintegr/modules/rkws/components/EdismHelper.php
+++ frontend/modules/clientintegr/modules/rkws/components/EdismHelper.php
@@ -39,7 +39,7 @@ class EdismHelper extends AuthHelper {
      
      $tmodel = new RkTasks();
      
-     $tmodel->tasktype_id = 32;
+     $tmodel->tasktype_id = 34;
      $tmodel->acc = $this->org;
      $tmodel->fid = 1;
      $tmodel->guid = $res['respcode']['taskguid'];

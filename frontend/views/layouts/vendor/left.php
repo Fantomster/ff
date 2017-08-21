405fee27af8bbd31be2000ebc604c4557890b588
elbabuino <elbabuino@el-babuino>
2017-08-21 15:19:14 +0300

    wtf

--- frontend/views/layouts/vendor/left.php
+++ frontend/views/layouts/vendor/left.php
@@ -58,7 +58,7 @@ if (Yii::$app->user->can('manage')) {
             ['label' => 'Общие', 'icon' => 'circle-o', 'url' => ['/vendor/settings']],
             //   ['label' => 'Интеграции', 'icon' => 'circle-o', 'url' => ['/vendorintegr/default']],
             ['label' => 'Сотрудники', 'icon' => 'circle-o', 'url' => ['/vendor/employees']],
-//            ['label' => 'Уведомления', 'icon' => 'circle-o', 'url' => ['/settings/notifications']],
+            ['label' => 'Уведомления', 'icon' => 'circle-o', 'url' => ['/settings/notifications']],
             ['label' => 'Доставка', 'icon' => 'circle-o', 'url' => ['/vendor/delivery']],
         ]
     ];

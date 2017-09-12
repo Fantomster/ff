3c8361fbd2e4efec02ad752fa9fd99538984c8a6
elbabuino <elbabuino@el-babuino>
2017-09-12 18:09:30 +0300

    wtf

--- console/controllers/UtilsController.php
+++ console/controllers/UtilsController.php
@@ -84,7 +84,7 @@ class UtilsController extends Controller {
     }
 
     public function actionCheckOrganizationPictures() {
-        $organizations = \common\models\Organization::find()->all();
+        $organizations = \common\models\Organization::find()->limit(100)->all();
         foreach ($organizations as $organization) {
             if ($organization->picture) {
                 $headers = get_headers($organization->pictureUrl);

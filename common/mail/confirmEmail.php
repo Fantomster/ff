65a5e3faeda99e730ef81b3507824ff3a7bc6c64
elbabuino <elbabuino@el-babuino>
2017-10-02 11:49:31 +0300

    wtf

--- common/mail/confirmEmail.php
+++ common/mail/confirmEmail.php
@@ -1,5 +1,8 @@
 <?php
 use yii\helpers\Url;
+if (empty($toFrontEnd)) {
+    $toFrontEnd = false;
+}
 ?>
 <p style="line-height: 1.6; margin: 0 0 10px; padding: 0;"><img src="https://mixcart.ru/img/immotion1.jpg" style="max-width: 100%; margin: 0; padding: 0;" alt="" /></p>
 <h3 style="line-height: 1.1; color: #3f3e3e; font-weight: 500; font-size: 27px; margin: 0 0 15px; padding: 0;">

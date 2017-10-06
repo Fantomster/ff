12683eae569a55b186045ca4ab5ff1c4080970de
elbabuino <elbabuino@el-babuino>
2017-10-06 17:23:16 +0300

    wtf

--- common/config/params.php
+++ common/config/params.php
@@ -12,5 +12,6 @@ return [
     ],
     'password_generation' => 'Создание пароля для входа в систему MixCart',
     'protocol' => 'http',
-    'franchiseeHost' => '//partner.f-keeper.ru'
+    'franchiseeHost' => '//partner.f-keeper.ru',
+    'integratAdminID' => [],
 ];

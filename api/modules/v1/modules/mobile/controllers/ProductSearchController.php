576403609e7a35c1d4654f528d7415d355106367
Sergey <sims2planet@mail.ru>
2017-10-12 15:46:04 +0300

    fix

--- api/modules/v1/modules/mobile/controllers/ProductSearchController.php
+++ api/modules/v1/modules/mobile/controllers/ProductSearchController.php
@@ -13,12 +13,14 @@ use common\models\OrderContent;
 use common\models\GoodsNotes;
 use common\models\Organization;
 use yii\helpers\Json;
+use common\models\guides\GuideProduct;
+use yii\db\Query;
 
 
 /**
  * @author Eugene Terentev <eugene@terentev.net>
  */
-class FavoritesController extends ActiveController {
+class ProductSearchController extends ActiveController {
 
     /**
      * @var string
@@ -67,49 +69,71 @@ class FavoritesController extends ActiveController {
      */
     public function prepareDataProvider()
     {
+        $params = new \api\modules\v1\modules\mobile\resources\GuideProductSearch();
         $user = Yii::$app->user->getIdentity();
         $client = $user->organization;
-        $params = new \api\modules\v1\modules\mobile\resources\FavoriteSearch();
-
+        
         $cbgTable = CatalogBaseGoods::tableName();
-        $orderTable = Order::tableName();
-        $ordContentTable = OrderContent::tableName();
         $goodsNotesTable = GoodsNotes::tableName();
         $organizationTable = Organization::tableName();
-
+        
         $query = CatalogBaseGoods::find();
         $query->select("$cbgTable.*, $organizationTable.name as organization_name, $goodsNotesTable.note as comment");
-        $query->leftJoin($ordContentTable, "$cbgTable.id=$ordContentTable.product_id");
-        $query->leftJoin($orderTable, "$ordContentTable.order_id=$orderTable.id");
+        $query->from("guide_product");
+        $query->leftJoin($cbgTable,"$cbgTable.id = guide_product.cbg_id");
         $query->leftJoin($organizationTable, "$organizationTable.id = $cbgTable.supp_org_id");
         $query->leftJoin($goodsNotesTable, "$goodsNotesTable.catalog_base_goods_id = $cbgTable.id and $goodsNotesTable.rest_org_id = $organizationTable.id");
+        // add conditions that should always apply here
+        
+        $orderTable = Order::tableName();
+        $ordContentTable = OrderContent::tableName();
+
+        $query2 = CatalogBaseGoods::find();
+        $query2->select("$cbgTable.*, $organizationTable.name as organization_name, $goodsNotesTable.note as comment");
+        $query2->leftJoin($ordContentTable, "$cbgTable.id=$ordContentTable.product_id");
+        $query2->leftJoin($orderTable, "$ordContentTable.order_id=$orderTable.id");
+        $query2->leftJoin($organizationTable, "$organizationTable.id = $cbgTable.supp_org_id");
+        $query2->leftJoin($goodsNotesTable, "$goodsNotesTable.catalog_base_goods_id = $cbgTable.id and $goodsNotesTable.rest_org_id = $organizationTable.id");
 
         // add conditions that should always apply here
         //where ord.client_id = 1 and cbg.status=1 and cbg.deleted = 0
-        $query->where(["$orderTable.client_id1" => $client->id, "$cbgTable.status" => CatalogBaseGoods::STATUS_ON, "$cbgTable.deleted" => CatalogBaseGoods::DELETED_OFF]);
-        $query->groupBy(["$cbgTable.id"]);
+        $query2->where(["$orderTable.client_id" => $client->id, "$cbgTable.status" => CatalogBaseGoods::STATUS_ON, "$cbgTable.deleted" => CatalogBaseGoods::DELETED_OFF]);
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
-             'pagination' => false,
+            'sort' => ['defaultOrder' => ['id' => SORT_DESC]],
+            'pagination' => false,
         ]);
 
         if (!($params->load(Yii::$app->request->queryParams) && $params->validate())) {
             // uncomment the following line if you do not want to return any records when validation fails
             // $query->where('0=1');
+                $query->leftJoin ("guide","guide.id = guide_product.guide_id")->where ("guide.client_id = ".$client->id);
+                $query->union($query2);
+                
+                $query_r = new Query();
+                $query_r->select('*')->from(['u' => $query]);//->orderBy(['id' => SORT_DESC]);
+                $dataProvider->query = $query_r;
             return $dataProvider;
         }
+
+        $query->leftJoin ("guide","guide.id = guide_product.guide_id")->where ("guide.client_id = ".$client->id);
+        
+        $query->union($query2);
+        $query_r = new Query();
+        $query_r->select('*')->from(['u' => $query]);
+        $dataProvider->query = $query_r;
         
         if (isset($params->count)) {
-            $query->limit($params->count);
+            $query_r->limit($params->count);
             if (isset($params->page)) {
                 $offset = ($params->page * $params->count) - $params->count;
-                $query->offset($offset);
+                $query_r->offset($offset);
             }
         }
-
+       
         // grid filtering conditions
-        $query->andFilterWhere(['like', "$cbgTable.product", $params->searchString]);
+        $query_r->andFilterWhere(['like', 'catalog_base_goods.product', $params->searchString]);
 
         return $dataProvider;
     }

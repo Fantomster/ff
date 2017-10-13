e31c2278f04927ca3c81f688f2e3412452d02a1f
xsupervisor <xsupervisor@f-keeper.ru>
2017-10-13 14:01:00 +0300

    xfixc

--- frontend/modules/clientintegr/modules/rkws/components/AgentHelper.php
+++ frontend/modules/clientintegr/modules/rkws/components/AgentHelper.php
@@ -230,6 +230,7 @@ class AgentHelper extends AuthHelper {
     
     if (empty($er)) $er = 'пусто';     
     if (empty($er3)) $er3 = 'пусто'; 
+    if (empty($er2)) $er2 = 'пусто'; 
         
     file_put_contents('runtime/logs/callback.log',PHP_EOL.'=======AGENT==EVENT==START================='.PHP_EOL,FILE_APPEND);  
     file_put_contents('runtime/logs/callback.log', PHP_EOL.date("Y-m-d H:i:s").':REQUEST:'.PHP_EOL, FILE_APPEND);   

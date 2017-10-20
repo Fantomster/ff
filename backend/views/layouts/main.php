1425a0deaec5a8a128a5b11e66cb7f9febef68cc
Нарзяев М <m@cn13.ru>
2017-10-20 17:50:48 +0300

    fix

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -144,7 +144,7 @@ $this->registerJs($customJs, yii\web\View::POS_READY);
                     ]);
 
                   //  if ((Yii::$app->user->id === 467) || (Yii::$app->user->id === 3529) || (Yii::$app->user->id === 4435) || (Yii::$app->user->id === 7761)) {
-                      if (inArray(Yii::$app->user->id,Yii::$app->params['integratAdminID'])) {
+                      if (in_array(Yii::$app->user->id,Yii::$app->params['integratAdminID'])) {
                         $menuItems = array_merge($menuItems, [
                             [
                                 'label' => 'Интеграция',

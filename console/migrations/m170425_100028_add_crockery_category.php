b6b52a495d7531d00cb260c3d8968b5bf243f3f8
elbabuino <elbabuino@el-babuino>
2017-10-26 17:31:03 +0300

    wtf

--- console/migrations/m170425_100028_add_crockery_category.php
+++ console/migrations/m170425_100028_add_crockery_category.php
@@ -5,10 +5,11 @@ use yii\db\Migration;
 class m170425_100028_add_crockery_category extends Migration {
 
     public function safeUp() {
-        $masterCategory = common\models\MpCategory::findOne(['name' => 'Сопутствующие товары']);
+        $query = "SELECT id FROM mp_category WHERE name='Сопутствующие товары' AND parent IS NULL";
+        $newMasterCategoryId = Yii::$app->db->createCommand($query)->queryScalar();
         if ($masterCategory) {
             $this->batchInsert('{{%mp_category}}', ['name', 'parent'], [
-                ['Посуда', $masterCategory->id],
+                ['Посуда', $newMasterCategoryId],
             ]);
         }
     }

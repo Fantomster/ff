bce932340dd4488965410226a32c8c2731ee7315
elbabuino <elbabuino@el-babuino>
2017-11-10 17:22:35 +0300

    market

--- market/views/site/main/_search_form.php
+++ market/views/site/main/_search_form.php
@@ -95,7 +95,7 @@ if($i > 0){
                             <?php if (empty($arr['_source']['product_show_price'])){ ?>
                             <h5 class="media-price" style="color: #dfdfdf">договорная цена</h5>
                             <?php } else {?>
-                            <h5 class="media-price"><?=floatval($arr['_source']['product_price']); ?> <small><?= $arr['_source']['product_price'] ?></small></h5>
+                            <h5 class="media-price"><?=floatval($arr['_source']['product_price']); ?> <small><?= $arr['_source']['product_currency'] ?></small></h5>
                             <?php } ?>                 
                         </div>
                       </div>

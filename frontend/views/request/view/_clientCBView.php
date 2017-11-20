b970d9ab2d853c1802f3aa88542c3fa81ecd9b48
Нарзяев М <m@cn13.ru>
2017-11-20 16:40:19 +0300

    #DEV-131 Мобильная вёрстка в заявках

--- frontend/views/request/view/_clientCBView.php
+++ frontend/views/request/view/_clientCBView.php
@@ -1,59 +1,94 @@
 <?php
+
 use yii\helpers\Html;
 use yii\helpers\Url;
+
 ?>
-<div class="col-md-12" style="padding:15px;border-radius:3px;background:#f7f7f7;margin-bottom:15px;">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="media">
-        <div class="media-left">
-          <img src="<?=$model->organization->pictureUrl?>" class="media-object" style="width:160px">
-        </div>
-        <div class="media-body" style="line-height: 1.6;">
-          <div class="row">
-            <div class="col-md-12" style="padding-right: 5px;">
-              <h4 class="text-success"><?=$model->organization->name?> 
-                  <?php $model->request->responsible_supp_org_id == $model->supp_org_id? 
-            $n = ['value'=>'Убрать исполнителя',
-                'class'=>'btn btn-danger',
-                'event'=>'exclude']: 
-            $n =['value'=>'Назначить исполнителем',
-                'class'=>'btn btn-success',
-                'event'=>'appoint'];?>
-            <?=Html::button($n['value'], ['class' => 'change pull-right ' . $n['class'],
-                'style'=>'font-size:16px;margin-top:-10px;margin-right:10px',
-                'data-supp-id'=>$model->supp_org_id,
-                'data-req-id'=>$model->request_id,
-                'data-event'=>$n['event']]) ?>
-                 <?php 
-                 if($model->request->responsible_supp_org_id == $model->supp_org_id){
-                   if(!common\models\RelationSuppRest::find()->where([
-                       'supp_org_id'=>$model->supp_org_id,
-                       'rest_org_id'=>$model->request->rest_org_id,
-                       'deleted'=>false])->exists()){
-                       $n = ['value'=>'Добавить поставщика',
-                        'class'=>'btn btn-success pull-right add-supplier',
-                        'event'=>'add-supplier'];
-                       }else{
-                       $n = ['value'=>'Поставщик добавлен',
-                        'class'=>'btn btn-gray pull-right disabled',
-                        'event'=>''];    
-                       }
-                       echo Html::button($n['value'], ['class' => $n['class'],
-                'style'=>'font-size:16px;margin-top:-10px;margin-right:10px',
-                'data-supp-id'=>$model->supp_org_id,
-                'data-req-id'=>$model->request_id,
-                'data-event'=>$n['event']]);
-                 }
-                 ?>
-                  <!--a href="#" class="btn btn-gray pull-right disabled" style="font-size:16px;margin-top:-10px;margin-right:10px"><i class="fa fa-comment"></i></a-->
-              </h4>
-              <h5>Стоимость услуги: <span class="text-bold"><?=$model->price?> руб.</span></h5>
-              <p><b>Комментарий поставщика:</b> <?=$model->comment?></p>
+
+<?php
+
+if ($model->request->responsible_supp_org_id == $model->supp_org_id) {
+    $n = [
+        'value' => 'Убрать исполнителя',
+        'class' => 'btn btn-danger',
+        'event' => 'exclude'
+    ];
+
+    $where = [
+        'supp_org_id' => $model->supp_org_id,
+        'rest_org_id' => $model->request->rest_org_id,
+        'deleted' => false
+    ];
+
+    if (!common\models\RelationSuppRest::find()->where($where)->exists()) {
+        $n_1 = [
+            'value' => 'Добавить поставщика',
+            'class' => 'btn btn-success add-supplier',
+            'event' => 'add-supplier'
+        ];
+    } else {
+        $n_1 = [
+            'value' => 'Поставщик добавлен',
+            'class' => 'btn btn-gray disabled',
+            'event' => ''
+        ];
+    }
+} else {
+    $n = [
+        'value' => 'Назначить исполнителем',
+        'class' => 'btn btn-success',
+        'event' => 'appoint'
+    ];
+}
+
+?>
+
+<div class="row" style="padding:15px;border-radius:3px;background:#f7f7f7;margin-bottom:15px;" >
+    <div class="col-md-12 col-xs-12">
+        <div class="media">
+            <div class="media-left">
+                <img src="<?= $model->organization->pictureUrl ?>" class="media-object" style="width:160px">
             </div>
-          </div>
+            <div class="media-body">
+
+                <div class="col-md-4 col-sm-4 col-xs-4">
+                    <h4 class="text-success">
+                        <?= $model->organization->name ?>
+                    </h4>
+                </div>
+
+                <div class="col-md-8 col-sm-8 col-xs-8 text-right">
+                        <?php
+                        echo Html::button($n['value'], [
+                            'class' => 'change ' . $n['class'],
+                            'style' => 'font-size:16px;margin-right:3px',
+                            'data-supp-id' => $model->supp_org_id,
+                            'data-req-id' => $model->request_id,
+                            'data-event' => $n['event']
+                        ]);
+                        ?>
+                        <?php
+
+                        if (isset($n_1)) {
+                            echo Html::button($n_1['value'], [
+                                'class' => $n_1['class'],
+                                'style' => 'font-size:16px;margin-right:3px',
+                                'data-supp-id' => $model->supp_org_id,
+                                'data-req-id' => $model->request_id,
+                                'data-event' => $n_1['event']
+                            ]);
+                        }
+
+                        ?>
+                </div>
+
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <!--a href="#" class="btn btn-gray pull-right disabled" style="font-size:16px;margin-top:-10px;margin-right:10px"><i class="fa fa-comment"></i></a-->
+
+                    <h5>Стоимость услуги: <span class="text-bold"><?= $model->price ?> руб.</span></h5>
+                    <p><b>Комментарий поставщика:</b> <?= $model->comment ?></p>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </div>

45028c8c052491826b474acaf420a62bbc7828ab
Нарзяев М <m@cn13.ru>
2017-11-22 10:56:04 +0300

    #DEV-131 Мобильная вёрстка в заявках

--- frontend/views/request/view/_clientCBView.php
+++ frontend/views/request/view/_clientCBView.php
@@ -10,7 +10,7 @@ use yii\helpers\Url;
 if ($model->request->responsible_supp_org_id == $model->supp_org_id) {
     $n = [
         'value' => 'Убрать исполнителя',
-        'class' => 'btn btn-danger',
+        'class' => 'btn btn-danger btn-md',
         'event' => 'exclude'
     ];
 
@@ -23,71 +23,64 @@ if ($model->request->responsible_supp_org_id == $model->supp_org_id) {
     if (!common\models\RelationSuppRest::find()->where($where)->exists()) {
         $n_1 = [
             'value' => 'Добавить поставщика',
-            'class' => 'btn btn-success add-supplier',
+            'class' => 'btn btn-success  btn-md add-supplier',
             'event' => 'add-supplier'
         ];
     } else {
         $n_1 = [
             'value' => 'Поставщик добавлен',
-            'class' => 'btn btn-gray disabled',
+            'class' => 'btn btn-gray  btn-md disabled',
             'event' => ''
         ];
     }
 } else {
     $n = [
         'value' => 'Назначить исполнителем',
-        'class' => 'btn btn-success',
+        'class' => 'btn btn-success  btn-md ',
         'event' => 'appoint'
     ];
 }
 
 ?>
 
-<div class="row" style="padding:15px;border-radius:3px;background:#f7f7f7;margin-bottom:15px;" >
-    <div class="col-md-12 col-xs-12">
-        <div class="media">
-            <div class="media-left">
-                <img src="<?= $model->organization->pictureUrl ?>" class="media-object" style="width:160px">
+<div class="row" style="padding:5px;border-radius:3px;background:#f7f7f7;margin-bottom:15px;" >
+    <div class="media">
+
+        <div class="media-left visible-sm visible-md visible-lg col-sm-12 col-md-2 col-lg-2">
+            <img src="<?= $model->organization->pictureUrl ?>" class="media-object" style="max-width: 160px;">
+        </div>
+
+        <div class="visible-xs text-center">
+            <img src="<?= $model->organization->pictureUrl ?>" style="width: 160px;">
+        </div>
+
+        <div class="media-body">
+            <div class="media-heading">
+                <h4 class="text-success" ><?= $model->organization->name ?></h4>
             </div>
-            <div class="media-body">
-
-                <div class="col-md-4 col-sm-4 col-xs-4">
-                    <h4 class="text-success">
-                        <?= $model->organization->name ?>
-                    </h4>
-                </div>
-
-                <div class="col-md-8 col-sm-8 col-xs-8 text-right">
-                        <?php
-                        echo Html::button($n['value'], [
-                            'class' => 'change ' . $n['class'],
-                            'style' => 'font-size:16px;margin-right:3px',
-                            'data-supp-id' => $model->supp_org_id,
-                            'data-req-id' => $model->request_id,
-                            'data-event' => $n['event']
-                        ]);
-                        ?>
-                        <?php
-
-                        if (isset($n_1)) {
-                            echo Html::button($n_1['value'], [
-                                'class' => $n_1['class'],
-                                'style' => 'font-size:16px;margin-right:3px',
-                                'data-supp-id' => $model->supp_org_id,
-                                'data-req-id' => $model->request_id,
-                                'data-event' => $n_1['event']
-                            ]);
-                        }
-
-                        ?>
-                </div>
-
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <!--a href="#" class="btn btn-gray pull-right disabled" style="font-size:16px;margin-top:-10px;margin-right:10px"><i class="fa fa-comment"></i></a-->
-
-                    <h5>Стоимость услуги: <span class="text-bold"><?= $model->price ?> руб.</span></h5>
-                    <p><b>Комментарий поставщика:</b> <?= $model->comment ?></p>
-                </div>
+            <p>Стоимость услуги: <span class="text-bold"><?= $model->price ?> руб.</span></p>
+            <p><b>Комментарий поставщика:</b> <?= $model->comment ?></p>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12" >
+            <div class="pull-right text-center" >
+                <div class="clearfix visible-xs"><br></div>
+                <?=Html::button($n['value'], [
+                    'class' => 'change ' . $n['class'],
+                    'data-supp-id' => $model->supp_org_id,
+                    'data-req-id' => $model->request_id,
+                    'data-event' => $n['event']
+                ]);?>
+                <?php
+                if (isset($n_1)) {
+                    echo '<div class="clearfix visible-xs"><br></div>';
+                    echo Html::button($n_1['value'], [
+                        'class' => $n_1['class'],
+                        'data-supp-id' => $model->supp_org_id,
+                        'data-req-id' => $model->request_id,
+                        'data-event' => $n_1['event']
+                    ]);
+                }
+                ?>
             </div>
         </div>
     </div>

cf01e840ae3b9593bd4f41658273f6d25e497828
alexey.sergeev <sergeev@mixcart.ru>
2017-12-22 11:10:05 +0300

    83 lang 29

---
+++ backend/views/translations/create.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Franchisee */
+
+$this->title = 'Создать трансляцию';
+$this->params['breadcrumbs'][] = ['label' => 'Трансляции', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="translation-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'sourceMessage' => $sourceMessage,
+        'message' => $message,
+    ]) ?>
+
+</div>

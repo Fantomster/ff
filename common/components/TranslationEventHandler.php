69774901e00d593fbad360d57be87e0ff18ee93c
alexey.sergeev <sergeev@mixcart.ru>
2017-12-26 12:46:55 +0300

    dev-314 Доп. обработчик

--- common/components/TranslationEventHandler.php
+++ common/components/TranslationEventHandler.php
@@ -13,17 +13,20 @@ use yii\i18n\MissingTranslationEvent;
  */
 class TranslationEventHandler {
 
-    public function handleMissingTranslation(MissingTranslationEvent $event){
-        $sourceMessage = new SourceMessage();
-        $sourceMessage->category = $event->category;
-        $sourceMessage->message = $event->message;
-        $sourceMessage->save();
-        $id = $sourceMessage->id;
-        foreach (SourceMessage::LANGUAGES as $language){
-            $message = new Message();
-            $message->id = $id;
-            $message->language = $language;
-            $message->save();
+    public function handleMissingTranslation(MissingTranslationEvent $event)
+    {
+        if (!SourceMessage::findOne(['message' => $event->message])) {
+            $sourceMessage = new SourceMessage();
+            $sourceMessage->category = $event->category;
+            $sourceMessage->message = $event->message;
+            $sourceMessage->save();
+            $id = $sourceMessage->id;
+            foreach (SourceMessage::LANGUAGES as $language) {
+                $message = new Message();
+                $message->id = $id;
+                $message->language = $language;
+                $message->save();
+            }
         }
     }
 }

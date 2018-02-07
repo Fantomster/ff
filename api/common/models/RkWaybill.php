6f7a4fa060687f463c9aab4f7f329a6602828031
xsupervisor <xsupervisor@mixcart.ru>
2018-02-07 22:44:56 +0300

    Раздельная привязка ресторанов к одному SH (#DEV-92) тесты при переносе на RC

--- api/common/models/RkWaybill.php
+++ api/common/models/RkWaybill.php
@@ -48,7 +48,7 @@ class RkWaybill extends \yii\db\ActiveRecord {
     public function rules() {
         return [
             [['order_id', 'doc_date', 'corr_rid'], 'required'],
-            [['corr_rid', 'store_rid', 'status_id'], 'integer'],
+            [['corr_rid', 'store_rid', 'status_id','num_code'], 'integer'],
                 //     [['comment'], 'string', 'max' => 255],
             [['store_rid', 'org','vat_included','text_code','num_code','note'],'safe']
         ];

c4a81309d8bb41ff50454c439ee2642da6777289
alexey.sergeev <sergeev@mixcart.ru>
2018-02-13 17:26:01 +0300

    (HotFix) Создание сотрудника за Владельца
     (HotFix) Создание сотрудника за Владельца #DEV-485 To Verify ${revision}

--- frontend/controllers/VendorController.php
+++ frontend/controllers/VendorController.php
@@ -252,7 +252,6 @@ class VendorController extends DefaultController {
                 $profile->load($post);
 
                 if ($user->validate() && $profile->validate()) {
-
                     if (!in_array($user->role_id, User::getAllowedRoles($this->currentUser->role_id)) && $this->currentUser->role_id != Role::ROLE_FRANCHISEE_OWNER) {
                         $user->role_id = $this->currentUser->role_id;
                     }

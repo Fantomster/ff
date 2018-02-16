25cf67ef553c941902192e8e9167761e61414e2e
El Babuino <sharapudin.omarov@gmail.com>
2018-02-16 13:19:54 +0300

    фикс ссылок

--- common/mail/_bill.php
+++ common/mail/_bill.php
@@ -32,7 +32,7 @@
         <?php if (!empty($order->comment)) { ?>
             <div style="width: 100%;height: auto;border: 1px solid #DDDDDD;float: left;margin: 30px 0%; border-radius: 100px;">
                 <div style="border-right: 1px solid #DDDDDD;width: 55px;float: left;height: 50px;">
-                    <img src="https://mixcart.ru/img/c.png" style="margin-left: 18px;margin-top: 13px;" alt="">
+                    <img src="https://app.mixcart.ru/img/c.png" style="margin-left: 18px;margin-top: 13px;" alt="">
                 </div>
                 <p class = "pl" style="margin-left: 10px;padding-left: 60px;padding-top: 13px;"><?= $order->comment ?></p>
             </div>
@@ -42,7 +42,7 @@
             ?>
             <div style="width: 100%;height: auto;border: 1px solid #DDDDDD;float: left;margin: 30px 0%; border-radius: 100px;">
                 <div style="border-right: 1px solid #DDDDDD;width: 55px;float: left;height: 50px;">
-                    <img src="https://mixcart.ru/img/c.png" style="margin-left: 18px;margin-top: 13px;" alt="">
+                    <img src="https://app.mixcart.ru/img/c.png" style="margin-left: 18px;margin-top: 13px;" alt="">
                 </div>
                 <div style="padding: 20px 60px 20px 60px;text-align:left;">
                     <?php foreach ($order->orderChat as $message) { ?>

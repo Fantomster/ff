220d22cac11e90cc4a807c09dde2f7019c203a0a
Alien <sims2planet@mail.ru>
2018-03-16 15:21:42 +0300

    fix mobile_api

--- api/modules/v1/modules/mobile/controllers/UserController.php
+++ api/modules/v1/modules/mobile/controllers/UserController.php
@@ -333,6 +333,9 @@ class UserController extends ActiveController {
         }
         throw new BadRequestException;
     */
-        return (new UserWebApi())->setOrganization(['organization_id' => $id]);
+        $user_api = new UserWebApi();
+        if (($user_api)->setOrganization(['organization_id' => $id]))
+            return $user_api->user->organization->attributes;
+        throw new BadRequestException;
     }
 }

412326f8db50f63baa3766e42b50b7ee50d05211
Нарзяев М <m@cn13.ru>
2018-03-20 13:04:07 +0300

    Список заявок для поставщика #DEV-629 To Verify ${revision}

--- api_web/classes/RequestWebApi.php
+++ api_web/classes/RequestWebApi.php
@@ -2,12 +2,13 @@
 
 namespace api_web\classes;
 
+use yii\data\Pagination;
+use common\models\Request;
+use yii\helpers\ArrayHelper;
 use api_web\components\WebApi;
 use common\models\Organization;
-use common\models\Request;
 use yii\data\ArrayDataProvider;
-use yii\data\Pagination;
-use yii\helpers\ArrayHelper;
+use yii\data\ActiveDataProvider;
 use yii\web\BadRequestHttpException;
 
 class RequestWebApi extends WebApi
@@ -31,9 +32,14 @@ class RequestWebApi extends WebApi
      * Список заявок для ресторана
      * @param array $post
      * @return array
+     * @throws BadRequestHttpException
      */
     public function getListClient(array $post)
     {
+        if ($this->user->organization->type_id !== Organization::TYPE_RESTAURANT) {
+            throw new BadRequestHttpException('Раздел доступен только для ресторанов...');
+        }
+
         $page = (isset($post['pagination']['page']) ? $post['pagination']['page'] : 1);
         $pageSize = (isset($post['pagination']['page_size']) ? $post['pagination']['page_size'] : 12);
 
@@ -81,26 +87,82 @@ class RequestWebApi extends WebApi
      * Список заявок для поставщика
      * @param array $post
      * @return array
+     * @throws BadRequestHttpException
      */
     public function getListVendor(array $post)
     {
+        if ($this->user->organization->type_id !== Organization::TYPE_SUPPLIER) {
+            throw new BadRequestHttpException('Раздел доступен только для поставщиков...');
+        }
+
+        $organization = $this->user->organization;
         $page = (isset($post['pagination']['page']) ? $post['pagination']['page'] : 1);
         $pageSize = (isset($post['pagination']['page_size']) ? $post['pagination']['page_size'] : 12);
 
-        $query = Request::find();
-        $query->where(['rest_org_id' => $this->user->organization->id]);
-
-        if (isset($post['search'])) {
-            /**
-             * Фильтр по статусу
-             */
-            if (isset($post['search']['status'])) {
-                $query->andWhere(['active_status' => (int)$post['search']['status']]);
+        $query = Request::find()->joinWith('client')->orderBy('id DESC');
+        //Массив в доставками
+        $deliveryRegions = $organization->deliveryRegionAsArray;
+        //Доступные для доставки регионы
+        if (!empty($deliveryRegions['allow'])) {
+            foreach ($deliveryRegions['allow'] as $row) {
+                if (!empty($row['administrative_area_level_1']) && !empty($row['locality'])) {
+                    $p = $row['administrative_area_level_1'] . $row['locality'];
+                    $query->orWhere('CONCAT(`administrative_area_level_1`, `locality`) = :p', [':p' => $p]);
+                } elseif ((empty($row['administrative_area_level_1']) || $row['administrative_area_level_1'] == 'undefined') && !empty($row['locality'])) {
+                    $query->orWhere(['=', 'locality', $row['locality']]);
+                } elseif (!empty($row['administrative_area_level_1']) && empty($row['locality'])) {
+                    $query->orWhere(['=', 'administrative_area_level_1', $row['administrative_area_level_1']]);
+                }
+            }
+        }
+        //Условия для исключения доставки с регионов
+        if (!empty($deliveryRegions['exclude'])) {
+            if (!empty($deliveryRegions['exclude'])) {
+                foreach ($deliveryRegions['exclude'] as $row) {
+                    if (!empty($row['administrative_area_level_1']) && !empty($row['locality'])) {
+                        $p = $row['administrative_area_level_1'] . $row['locality'];
+                        $query->andWhere('CONCAT(`administrative_area_level_1`, `locality`) <> :s', [':s' => $p]);
+                    } elseif ((empty($row['administrative_area_level_1']) || $row['administrative_area_level_1'] == 'undefined') && !empty($row['locality'])) {
+                        $query->andWhere(['!=', 'locality', $row['locality']]);
+                    } elseif (!empty($row['administrative_area_level_1']) && empty($row['locality'])) {
+                        $query->andWhere(['!=', 'administrative_area_level_1', $row['administrative_area_level_1']]);
+                    }
+                }
             }
         }
 
-        $dataProvider = new ArrayDataProvider([
-            'allModels' => $query->orderBy(['created_at' => SORT_DESC])->all()
+        $query->andWhere(['>=', 'end', new \yii\db\Expression('NOW()')])->andWhere(['active_status' => Request::ACTIVE]);
+
+        /**
+         * только мои заявки, на которые откликнулся
+         */
+        if (isset($post['my_only']) && $post['my_only'] == true) {
+            $query->andWhere(['responsible_supp_org_id' => (int)$organization->id]);
+        }
+
+        if (isset($post['search'])) {
+            /**
+             * Фильтр по Категории
+             */
+            if (isset($post['search']['category'])) {
+                $query->andWhere(['category' => (int)$post['search']['category']]);
+            }
+            /**
+             * поиск по продукту
+             */
+            if (isset($post['search']['product'])) {
+                $query->andWhere(['like', 'product', $post['search']['product']]);
+            }
+            /**
+             * только срочные заявки
+             */
+            if (isset($post['search']['urgent']) && $post['search']['urgent'] == true) {
+                $query->andWhere(['rush_order' => 1]);
+            }
+        }
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query
         ]);
 
         $pagination = new Pagination();
@@ -108,10 +170,8 @@ class RequestWebApi extends WebApi
         $pagination->setPageSize($pageSize);
         $dataProvider->setPagination($pagination);
 
-        $models = $dataProvider->models;
-
         $result = [];
-        foreach ($models as $model) {
+        foreach ($dataProvider->models as $model) {
             $result[] = $this->prepareRequest($model);
         }
 

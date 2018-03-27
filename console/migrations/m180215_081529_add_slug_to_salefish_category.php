49920549d90328578e770e82731bb733bfb3abb2
El Babuino <sharapudin.omarov@gmail.com>
2018-03-27 15:37:30 +0300

    old migration fix

--- console/migrations/m180215_081529_add_slug_to_salefish_category.php
+++ console/migrations/m180215_081529_add_slug_to_salefish_category.php
@@ -12,7 +12,10 @@ class m180215_081529_add_slug_to_salefish_category extends Migration
      */
     public function safeUp()
     {
-        $this->update('{{%mp_category}}', ['slug' => 'mollyuski'], ['id' => 240]);
+        $query = "SELECT id FROM mp_category WHERE name = :name";
+        $id = Yii::$app->db->createCommand($query)->bindParam(':name', 'Моллюски')->queryScalar();
+        
+        $this->update('{{%mp_category}}', ['slug' => 'mollyuski'], ['id' => $id]);
     }
 
     /**

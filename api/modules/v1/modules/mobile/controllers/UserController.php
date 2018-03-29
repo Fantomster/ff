55a25eae9b0f504d3da94749c74ee02f19e68cab
Alien <sims2planet@mail.ru>
2018-03-29 14:17:42 +0300

    API Mobile — Быстрая регистрация #DEV-678

--- api/modules/v1/modules/mobile/controllers/UserController.php
+++ api/modules/v1/modules/mobile/controllers/UserController.php
@@ -20,7 +20,8 @@ use api_web\classes\UserWebApi;
 /**
  * @author Eugene Terentev <eugene@terentev.net>
  */
-class UserController extends ActiveController {
+class UserController extends ActiveController
+{
 
     /**
      * @var string
@@ -30,15 +31,16 @@ class UserController extends ActiveController {
     /**
      * @return array
      */
-    public function behaviors() {
+    public function behaviors()
+    {
         $behaviors = parent::behaviors();
 
         $behaviors = array_merge($behaviors, $this->module->controllerBehaviors);
 
         return $behaviors;
     }
-    
-     /**
+
+    /**
      * @inheritdoc
      */
     public function actions()
@@ -47,8 +49,9 @@ class UserController extends ActiveController {
         ];
     }
 
-    public function actionAuth() {
-        
+    public function actionAuth()
+    {
+
         $user = User::findOne(Yii::$app->user->id);
         $profile = $user->profile;
         $organization = $user->organization;
@@ -56,19 +59,21 @@ class UserController extends ActiveController {
         $role = Role::findOne(['id' => $user['role_id']]);
         $user['role_name'] = ($role != null) ? $role->name : "none";
         //$organization->picture = $organization->pictureUrl;
-        return compact("user", "profile","organization");
+        return compact("user", "profile", "organization");
     }
-    
-     public function actionAvatar($name) {
-         $organization = new Organization();  
-         $organization->picture = $name;
-         header('Content-type: image/jpeg');
-         echo file_get_contents($organization->pictureUrl);
-  
+
+    public function actionAvatar($name)
+    {
+        $organization = new Organization();
+        $organization->picture = $name;
+        header('Content-type: image/jpeg');
+        echo file_get_contents($organization->pictureUrl);
+
     }
-    
-    public function actionRegistration() {
-        
+
+    public function actionRegistration()
+    {
+
         $user = new User(["scenario" => "register"]);
         $profile = new Profile (["scenario" => "register"]);
         $organization = new Organization (["scenario" => "register"]);
@@ -78,11 +83,11 @@ class UserController extends ActiveController {
         $post = Yii::$app->request->post();
         if ($user->load($post, 'user') && $user->validate()) {
             // ensure profile data gets loaded
-            
+
             $profile->load($post, 'profile');
 
             // load organization data
-            $organization->load($post,'organization');
+            $organization->load($post, 'organization');
 
             // validate for normal request
             if ($profile->validate() && $organization->validate()) {
@@ -110,19 +115,17 @@ class UserController extends ActiveController {
 
                 $user = User::findOne($user->id);
                 $this->afterRegister($user);
-                return ['success' => 1];  
-            }
-            elseif(!$profile->validate())
-                    $profile_errors = $profile->getErrors();
-                else
-                    $organization_errors = $organization->getErrors();
-        }else
-        {
+                return ['success' => 1];
+            } elseif (!$profile->validate())
+                $profile_errors = $profile->getErrors();
+            else
+                $organization_errors = $organization->getErrors();
+        } else {
             $user_errors = $user->getErrors();
         }
-        return compact("user", "profile", "organization", "user_errors", "profile_errors","organization_errors");
+        return compact("user", "profile", "organization", "user_errors", "profile_errors", "organization_errors");
     }
-    
+
     /**
      * Process data after registration
      * @param \amnah\yii2\user\models\User $user
@@ -131,7 +134,7 @@ class UserController extends ActiveController {
     {
         /** @var \amnah\yii2\user\models\UserToken $userToken */
         $userToken = new UserToken();
-        
+
         // determine userToken type to see if we need to send email
         $userTokenType = null;
         if ($user->status == $user::STATUS_INACTIVE) {
@@ -144,13 +147,14 @@ class UserController extends ActiveController {
         if ($userTokenType) {
             $userToken = $userToken::generate($user->id, $userTokenType);
             $user->sendEmailConfirmation($userToken);
-        } 
+        }
     }
-    
-     /**
+
+    /**
      * Confirm email
      */
-    public function actionConfirm($pin) {
+    public function actionConfirm($pin)
+    {
         /** @var \amnah\yii2\user\models\UserToken $userToken */
         /** @var \amnah\yii2\user\models\User $user */
         // search for userToken
@@ -173,17 +177,18 @@ class UserController extends ActiveController {
             if ($userToken->type == $userToken::TYPE_EMAIL_ACTIVATE) {
                 //send welcome
                 $user->sendWelcome();
-                 \api\modules\v1\modules\mobile\components\notifications\NotificationUser::actionConfirm($user);
+                \api\modules\v1\modules\mobile\components\notifications\NotificationUser::actionConfirm($user);
             }
             // set email and delete token
-            $email = $newEmail ? : $user->email;
+            $email = $newEmail ?: $user->email;
             $userToken->delete();
         }
 
-        return ($success) ? compact("user","profile","organization") : ['error' => Yii::t('app', 'api.modules.v1.modules.mobile.controllers.wrong_code', ['ru'=>'Неверный код'])/*Yii::t('user','Invalid PIN')*/];
+        return ($success) ? compact("user", "profile", "organization") : ['error' => Yii::t('app', 'api.modules.v1.modules.mobile.controllers.wrong_code', ['ru' => 'Неверный код'])/*Yii::t('user','Invalid PIN')*/];
     }
 
-    public function actionCompleteRegistration() {
+    public function actionCompleteRegistration()
+    {
         $user = Yii::$app->user->identity;
         $profile = new Profile();
         $profile = $user->profile;
@@ -194,63 +199,63 @@ class UserController extends ActiveController {
         $post = Yii::$app->request->post();
         $profile->load($post, 'profile');
         $organization->load($post, 'organization');
-        
-        if ($profile->validate() && $organization->validate()) {
-                $profile->save();
-                $organization->save();
-                return ['success' => 1];
-            }
-             elseif(!$profile->validate())
-                    $profile_errors = $profile->getErrors();
-                else
-                    $organization_errors = $organization->getErrors();
 
-        return compact("profile", "organization", "profile_errors","organization_errors");
+        if ($profile->validate() && $organization->validate()) {
+            $profile->save();
+            $organization->save();
+            return ['success' => 1];
+        } elseif (!$profile->validate())
+            $profile_errors = $profile->getErrors();
+        else
+            $organization_errors = $organization->getErrors();
+
+        return compact("profile", "organization", "profile_errors", "organization_errors");
     }
-    
+
     /**
      * Forgot password
      */
     public function actionForgot()
     {
         // load post data and send email
-        $model =  new \api\modules\v1\modules\mobile\models\ForgotForm();
+        $model = new \api\modules\v1\modules\mobile\models\ForgotForm();
         $model->email = Yii::$app->request->post('email');
 
         if ($model->sendForgotEmail()) {
             return ['success' => 1];
         }
-        $email_errors =  $model->getErrors();   
+        $email_errors = $model->getErrors();
         return compact('email_errors');
     }
-    
+
     public function actionSend()
     {
         /*$user = Yii::$app->user->identity;
         \api\modules\v1\modules\mobile\components\NotificationHelper::actionConfirm($user->email, $user->id);*/
 
     }
-    
-    public function actionRefreshFcmToken() {
+
+    public function actionRefreshFcmToken()
+    {
         $device_id = Yii::$app->request->headers->get("Device_id");
         $token = Yii::$app->request->post('token');
-        if($device_id === null)
+        if ($device_id === null)
             return "Fail";
-        
+
         $fcm = UserFcmToken::find()->where('user_id = :user_id and device_id = :device_id', [':user_id' => Yii::$app->user->id, ':device_id' => $device_id])->one();
-        
-        if($fcm === null)
-        {
+
+        if ($fcm === null) {
             $fcm = new UserFcmToken();
             $fcm->device_id = $device_id;
         }
-        
+
         $fcm->token = $token;
 
         return ($fcm->save()) ? "success" : print_r($fcm->getErrors());
     }
 
-    public function actionBuisinessList(){
+    public function actionBuisinessList()
+    {
         $params = Yii::$app->request->queryParams;
         $pageSize = isset($params['per-page']) ? intval($params['per-page']) : 4;
         $dataProvider = new \yii\data\ArrayDataProvider([
@@ -269,4 +274,51 @@ class UserController extends ActiveController {
             return $user_api->user->organization->attributes;
         throw new BadRequestException;
     }
+
+    public function actionEmptyRegistration()
+    {
+
+        $user = new User();
+        $user->emptyInit();
+        $profile = new Profile ();
+        $profile->full_name = "Тестовый Пользователь";
+
+        $testVendor = TestVendors::find()->where('1')->one();
+        $organizationTemp = Organization::get_value($testVendor->vendor_id);
+        unset($organizationTemp->id);
+        unset($organizationTemp->phone);
+        unset($organizationTemp->website);
+        unset($organizationTemp->created_at);
+        unset($organizationTemp->updated_at);
+        unset($organizationTemp->picture);
+        unset($organizationTemp->parent_id);
+        unset($organizationTemp->manager_id);
+        $organization = new Organization();
+        $organization->setAttributes($organizationTemp->attributes);
+        $organization->type_id = Organization::TYPE_RESTAURANT;
+        $organization->name = "Тестовый ресторан";
+
+        $transaction = Yii::$app->db->beginTransaction();
+        try {
+            $user->setRegisterAttributes(Role::getManagerRole($organization->type_id))->save();
+            $profile->setUser($user->id)->save();
+            //var_dump($organization->attributes);
+            $organization->save();
+            $user->setOrganization($organization, true)->save();
+            $profile->full_name = $profile->full_name."-".$user->id;
+            $organization->name = $organization->name."-".$organization->id;
+            $organization->save();
+            $transaction->commit();
+        } catch (Exception $ex) {
+            $transaction->rollBack();
+        }
+
+        if ($organization->type_id == Organization::TYPE_RESTAURANT) {
+            TestVendors::setGuides($organization);
+        }
+
+        $user = User::findOne($user->id);
+        return compact("user", "profile", "organization");
+    }
+
 }

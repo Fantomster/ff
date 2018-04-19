9e96cbb8bbc5cfd65446422e3adbaeb396d91a64
Alien <sims2planet@mail.ru>
2018-04-19 18:36:28 +0300

    #DEV-832 To Verify ${revision}

--- frontend/views/order/_checkout-total.php
+++ frontend/views/order/_checkout-total.php
@@ -9,7 +9,7 @@ use yii\helpers\Html;
 <div class="block_right_wrap_1">
     <?php if ($cart['for_min_order_price']) { ?>
         <button type="button" class="btn btn-default alButton" disabled="disabled"><?= Yii::t('message', 'frontend.views.order.make_order_two', ['ru' => 'Оформить заказ']) ?></button>
-        <p class="alP"><?= Yii::t('message', 'frontend.views.order.until_min', ['ru' => 'до минимального заказа']) ?></p><p><?= $cart['for_min_order_price'] ?> <?= $cart['currency'] ?></p>
+        <p class="alP"><?= Yii::t('message', 'frontend.views.order.until_min', ['ru' => 'до минимального заказа']) ?></p><p><?= $cart['for_min_cart_price'] ?> <?= $cart['currency'] ?></p>
         <?php } elseif ($cart['for_free_delivery'] > 0) { ?>
         <p><?= Yii::t('message', 'frontend.views.order.until_free', ['ru' => 'до бесплатной доставки']) ?> </p><p> <?= $cart['currency'] ?></p>
     <?php } elseif ($cart['for_free_delivery'] == 0) { ?>

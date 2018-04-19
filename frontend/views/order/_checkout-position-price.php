05d4e2dcc6299a4f41a0bc87e84bdf67f61bc837
Alien <sims2planet@mail.ru>
2018-04-19 18:25:55 +0300

    #DEV-832 To Verify ${revision}

--- frontend/views/order/_checkout-position-price.php
+++ frontend/views/order/_checkout-position-price.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 ?>
-<p class = "block_cena_p"><span id="total<?= $position->id ?>"><?= number_format($position['price'] * $position['in_basket'], 2) ?></span> <?= $position['currency'] ?></p>
+<p class = "block_cena_p"><span id="total<?= $position['id']?>"><?= number_format($position['price'] * $position['in_basket'], 2) ?></span> <?= $position['currency'] ?></p>
 <p class = "block_cena_p1"><?= $position['in_basket'] ?> x <span> <?= $position['price'] ?> <?= $position['currency'] ?></span></p>
 <?=
 Html::a('<img class= "delete_tovar1" src="/img/tovar_delete.png" alt="">', '#', [

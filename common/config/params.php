284fee8bef6461c5221fa2110f9d68a2ba8fdd0a
Unknown <sharapudin.omarov@gmail.com>
2018-04-20 15:31:14 +0300

    s3 images fix

--- common/config/params.php
+++ common/config/params.php
@@ -6,10 +6,10 @@ return [
         'language' => 'ru-RU'
     ],
     'pictures' => [
-        'org-noavatar' => 'https://s3-eu-west-1.amazonaws.com/static.f-keeper.ru/rest-noavatar.gif',
-        'client-noavatar' => 'https://s3-eu-west-1.amazonaws.com/static.f-keeper.ru/restaurant-noavatar.gif',
-        'vendor-noavatar' => 'https://s3-eu-west-1.amazonaws.com/static.f-keeper.ru/vendor-noavatar.gif',
-        'bill-logo' => 'https://s3-eu-west-1.amazonaws.com/static.f-keeper.ru/logo-mix.png',
+        'org-noavatar' => 'https://static.mixcart.ru/rest-noavatar.gif',
+        'client-noavatar' => 'https://static.mixcart.ru/restaurant-noavatar.gif',
+        'vendor-noavatar' => 'https://static.mixcart.ru/vendor-noavatar.gif',
+        'bill-logo' => 'https://static.mixcart.ru/logo-mix.png',
     ],
     'password_generation' => Yii::t('app', 'common.config.params.pass', ['ru' => 'Создание пароля для входа в систему MixCart']),
     'protocol' => 'http',

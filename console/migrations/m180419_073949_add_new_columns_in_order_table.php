4948326d8aaa8fc8a5eb912d3cc36824b6ceb81f
xsupervisor <xsupervisor@mixcart.ru>
2018-04-23 15:21:32 +0300

    fix  ** applying m180419_073949_add_new_columns_in_order_table     > drop column order_code from table order ...Exception: SQLSTATE[42000]: Syntax error or access violation: 1091 Can't DROP 'order_code'; check that column/key exists

--- console/migrations/m180419_073949_add_new_columns_in_order_table.php
+++ console/migrations/m180419_073949_add_new_columns_in_order_table.php
@@ -12,7 +12,7 @@ class m180419_073949_add_new_columns_in_order_table extends Migration
      */
     public function safeUp(): void
     {
-        $this->dropColumn(\common\models\Order::tableName(), 'order_code');
+       // $this->dropColumn(\common\models\Order::tableName(), 'order_code');
         $this->addColumn(\common\models\OrderContent::tableName(), 'plan_price', $this->decimal(20, 2)->defaultValue(0.00));
         $this->addColumn(\common\models\OrderContent::tableName(), 'plan_quantity', $this->decimal(20, 3)->defaultValue(0.000));
         $this->addColumn(\common\models\OrderContent::tableName(), 'updated_at', $this->timestamp()->defaultValue(new \yii\db\Expression('NOW()')));

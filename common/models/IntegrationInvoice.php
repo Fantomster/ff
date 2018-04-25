74250c0405a9d1fc5c3560d77686c95df8032e96
xsupervisor <xsupervisor@mixcart.ru>
2018-04-25 12:52:55 +0300

    #DEV-875 При формировании заказа из ТОРГ-12 цена позиции должна указываться с содержанием НДС

--- common/models/IntegrationInvoice.php
+++ common/models/IntegrationInvoice.php
@@ -174,7 +174,8 @@ class IntegrationInvoice extends \yii\db\ActiveRecord
                     'percent_nds' => ceil($row['tax_rate']),
                     'price_nds' => round($row['price_with_tax'], 2),
                     'price_without_nds' => round($row['price_without_tax'], 2),
-                    'quantity' => ceil($row['cnt'])
+                    'quantity' => $row['cnt']
+                    // 'quantity' => ceil($row['cnt']) Hotfix 1.5.12
                 ]);
                 if (!$content->save()) {
                     throw new Exception(implode(' ', $content->getFirstErrors()));
@@ -213,7 +214,8 @@ class IntegrationInvoice extends \yii\db\ActiveRecord
                 $model->supp_org_id = $vendor->id;
                 $model->ed = $row->ed;
                 $model->units = 1;
-                $model->price = $row->price_without_nds;
+                $model->price = $row->price_nds;  // Hotfix 1.5.12
+                // $model->price = $row->price_without_nds;
                 if ($model->validate()) {
                     $model->save();
                 } else {
@@ -223,7 +225,8 @@ class IntegrationInvoice extends \yii\db\ActiveRecord
             $models[] = [
                 'id' => $model->id,
                 'quantity' => $row->quantity,
-                'price' => $row->price_without_nds,
+                'price' => $row->price_nds, // Hotfix 1.5.12
+                // 'price' => $row->price_without_nds,
                 'units' => 1,
                 'product_name' => $model->product,
                 'article' => $model->article

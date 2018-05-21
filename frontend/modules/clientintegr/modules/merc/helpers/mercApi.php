02a2e291bf783cd4d3090ef1cc0caf35e5b8491f
xsupervisor <xsupervisor@mixcart.ru>
2018-05-21 21:18:57 +0300

    Merc tests

--- frontend/modules/clientintegr/modules/merc/helpers/mercApi.php
+++ frontend/modules/clientintegr/modules/merc/helpers/mercApi.php
@@ -363,8 +363,13 @@ class mercApi
             $application->addData($vetDoc);
             $request->setApplication($application);
 
+            var_dump(htmlentities($request->getXML()));
+            die();
+
             //Делаем запрос
             $response = $client->__doRequest($request->getXML(), $this->wsdls['mercury']['Endpoint_URL'], 'submitApplicationRequest', SOAP_1_1);
+
+
 
             $result = $this->parseResponse($response);
 

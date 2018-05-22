ed4ab1c7e3306aaf3ab0032c2f5c0eb862e9d13f
alexey.sergeev <sergeev@mixcart.ru>
2018-05-22 10:39:19 +0300

    Признак работы с EDI для ПОСТАВЩИКА #dev-1017

--- common/components/EComIntegration.php
+++ common/components/EComIntegration.php
@@ -159,7 +159,9 @@ class EComIntegration extends Component {
             $relationCatalogID = $rel->cat_id;
         }
 
+        $i=0;
         foreach ($goodsArray as $barcode => $good){
+            if($i>24)break;
             $catalogBaseGood = CatalogBaseGoods::findOne(['cat_id' => $baseCatalog->id, 'barcode' => $barcode]);
             if (!$catalogBaseGood) {
                 $res = Yii::$app->db->createCommand()->insert('catalog_base_goods', [
@@ -191,6 +193,7 @@ class EComIntegration extends Component {
                 }
             }
             Yii::$app->db->createCommand()->update('catalog_base_goods', ['updated_at' => new Expression('NOW()'), 'status' => CatalogBaseGoods::STATUS_ON], 'id='.$catalogBaseGood->id)->execute();
+            $i++;
         }
         return true;
     }

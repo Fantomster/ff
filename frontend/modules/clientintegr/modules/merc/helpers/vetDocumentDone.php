ebd974944a8ec867dfc0c452aaf4f393f3765968
Alien <sims2planet@mail.ru>
2018-05-29 12:36:08 +0300

    fix mercury

--- frontend/modules/clientintegr/modules/merc/helpers/vetDocumentDone.php
+++ frontend/modules/clientintegr/modules/merc/helpers/vetDocumentDone.php
@@ -250,7 +250,7 @@ class vetDocumentDone extends Component
                         <vet:productItem>
                            <prod:name>'.$doc->ns2batch->ns2productItem->prodname->__toString().'</prod:name>
                         </vet:productItem>
-                        <vet:volume>'.(($this->type = self::RETURN_ALL) ? 0 : abs($doc->ns2batch->ns2volume - $this->rejected_data['volume'])).'</vet:volume>
+                        <vet:volume>'.(($this->type == self::RETURN_ALL) ? 0 : abs($doc->ns2batch->ns2volume - $this->rejected_data['volume'])).'</vet:volume>
                         <vet:unit>
                            <base:uuid>'.$doc->ns2batch->ns2unit->bsuuid.'</base:uuid>
                         </vet:unit>';

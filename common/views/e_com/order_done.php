d5a981669775302bf7532afdc703d90ebbbacf40
alexey.sergeev <sergeev@mixcart.ru>
2018-06-05 10:42:49 +0300

    Работа с Уведомлением о приемки товара (RECADV) #dev-1154

--- common/views/e_com/order_done.php
+++ common/views/e_com/order_done.php
@@ -15,30 +15,32 @@
         <DELIVERYPLACE><?= $client->ediOrganization->gln_code ?></DELIVERYPLACE>
         <SENDER><?= $client->ediOrganization->gln_code ?></SENDER>
         <RECIPIENT><?= $vendor->ediOrganization->gln_code ?></RECIPIENT>
-        <HIERARCHICALID><?= $order->id ?></HIERARCHICALID>
-        <?php
-        $i = 1;
-        foreach ($orderContent as $position): ?>
-            <?php $product = \common\models\CatalogBaseGoods::findOne(['id' => $position['product_id']]);
-            $barcode = $product->barcode;
-            $edi_supplier_article = $product->edi_supplier_article ?? $position['id'];
-            if (!$barcode)continue;
-            ?>
-            <POSITION>
-                <POSITIONNUMBER><?= $i++ ?></POSITIONNUMBER>
-                <PRODUCT><?= $barcode ?></PRODUCT>
-                <PRODUCTIDBUYER><?= $position['id'] ?></PRODUCTIDBUYER>
-                <PRODUCTIDSUPPLIER><?= $edi_supplier_article ?></PRODUCTIDSUPPLIER>
-                <DELIVEREDQUANTITY><?= $position['quantity'] ?></DELIVEREDQUANTITY>
-                <ORDEREDQUANTITY><?= $position['quantity'] ?></ORDEREDQUANTITY>
-                <DELIVEREDUNIT><?= $position['units'] ?></DELIVEREDUNIT>
-                <ORDERUNIT><?= $position['units'] ?></ORDERUNIT>
-                <EGAISCODE><?= $position['id'] ?></EGAISCODE>
-                <EGAISQUANTITY><?= $position['quantity'] ?></EGAISQUANTITY>
-                <PRICE><?= $position['price'] ?></PRICE>
-                <PRICEWITHVAT><?= $position['price'] ?></PRICEWITHVAT>
-                <TAXRATE>0</TAXRATE>
-            </POSITION>
-        <?php endforeach; ?>
+        <PACKINGSEQUENCE>
+            <HIERARCHICALID><?= $order->id ?></HIERARCHICALID>
+            <?php
+            $i = 1;
+            foreach ($orderContent as $position): ?>
+                <?php $product = \common\models\CatalogBaseGoods::findOne(['id' => $position['product_id']]);
+                $barcode = $product->barcode;
+                $edi_supplier_article = $product->edi_supplier_article ?? $position['id'];
+                if (!$barcode) continue;
+                ?>
+                <POSITION>
+                    <POSITIONNUMBER><?= $i++ ?></POSITIONNUMBER>
+                    <PRODUCT><?= $barcode ?></PRODUCT>
+                    <PRODUCTIDBUYER><?= $position['id'] ?></PRODUCTIDBUYER>
+                    <PRODUCTIDSUPPLIER><?= $edi_supplier_article ?></PRODUCTIDSUPPLIER>
+                    <DELIVEREDQUANTITY><?= $position['quantity'] ?></DELIVEREDQUANTITY>
+                    <ORDEREDQUANTITY><?= $position['quantity'] ?></ORDEREDQUANTITY>
+                    <DELIVEREDUNIT><?= $position['units'] ?></DELIVEREDUNIT>
+                    <ORDERUNIT><?= $position['units'] ?></ORDERUNIT>
+                    <EGAISCODE><?= $position['id'] ?></EGAISCODE>
+                    <EGAISQUANTITY><?= $position['quantity'] ?></EGAISQUANTITY>
+                    <PRICE><?= $position['price'] ?></PRICE>
+                    <PRICEWITHVAT><?= $position['price'] ?></PRICEWITHVAT>
+                    <TAXRATE>0</TAXRATE>
+                </POSITION>
+            <?php endforeach; ?>
+        </PACKINGSEQUENCE>
     </HEAD>
 </RECADV>

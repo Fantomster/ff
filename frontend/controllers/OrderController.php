2c9e145be55592046ffa5a3dea8d8a3714d3d742
Alien <sims2planet@mail.ru>
2018-06-15 16:41:35 +0300

    fix #dev-688

--- frontend/controllers/OrderController.php
+++ frontend/controllers/OrderController.php
@@ -2244,11 +2244,7 @@ class OrderController extends DefaultController
 
             foreach ($orgs as $org)
             {
-<<<<<<< HEAD
-                $sql .= "IF(SUM(IF (`order`.client_id = ".$org['id'].", oc.quantity, 0)) = 0, '', CAST(SUM(IF (`order`.client_id = ".$org['id'].", oc.quantity, 0))as CHAR(10))) as '".$org['client_name']."'";
-=======
                 $sql .= "IF(SUM(IF (`order`.client_id = ".$org['id'].", oc.quantity, 0)) = 0, '', CAST(SUM(IF (`order`.client_id = ".$org['id'].", oc.quantity, 0))as CHAR(10))) as '".$org['client_name']."',";
->>>>>>> b412e1f416448b6334f2534047b943cefb925d4a
             }
 
             $sql = substr($sql, 0, -1);

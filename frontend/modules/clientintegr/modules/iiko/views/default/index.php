3bffa0c47b85221ffa96ceaabd14dc6927f4af0c
kosyanov <kosyanov@mixcart.ru>
2018-06-18 18:04:24 +0300

    #DEV-572 Показ состояния неактивной лицензии в Настройки->Интеграции->IIKO->внутренние страницы

--- frontend/modules/clientintegr/modules/iiko/views/default/index.php
+++ frontend/modules/clientintegr/modules/iiko/views/default/index.php
@@ -29,18 +29,30 @@ use kartik\grid\GridView;
 </section>
 
 <section class="content-header">
-    <div class="box box-info">
-        <div class="box-header with-border">
-            <div class="panel-body">
-                <div class="box-body table-responsive no-padding">
-                    <p>
-                        Состояние лицензии:
-                        <?php echo '<strong>Активна</strong> ID: ' . $lic->code . ' (с ' . date("d-m-Y H:i:s", strtotime($lic->fd)) . ' по ' . date("d-m-Y H:i:s", strtotime($lic->td)) . ') '; ?>
-                    </p>
+    <?php
+    //echo '<strong>Активна</strong> ID: ' . $lic->code . ' (с ' . date("d-m-Y H:i:s", strtotime($lic->fd)) . ' по ' . date("d-m-Y H:i:s", strtotime($lic->td)) . ') ';
+    $timestamp_now=time();
+    ($lic->status_id==1) && ($timestamp_now<=(time($lic->td))) ? $lic_iiko=1 : $lic_iiko=0;
+    if ($lic_iiko==0) {
+        ?>
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <div class="panel-body">
+                        <div class="box-body table-responsive no-padding">
+                            <p>
+                                Состояние лицензии:
+                                <?php
+                                print " Лицензия IIKO: ID ".$lic->id." <strong><span style=\"color:#dd4b39\">Не активна. </span></strong>";
+                                print "Пожалуйста, обратитесь к вашему менеджеру MixCart.";
+                                ?>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+        <?php
+    }
+    ?>
     СПРАВОЧНИКИ:
 </section>
 <section class="content-header">

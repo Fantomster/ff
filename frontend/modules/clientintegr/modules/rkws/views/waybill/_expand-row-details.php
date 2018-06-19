1926164da0cd4c972c8e470966dc650c94c0a9df
kosyanov <kosyanov@mixcart.ru>
2018-06-19 18:24:42 +0300

    #DEV-1122 Исправление ошибки

--- frontend/modules/clientintegr/modules/rkws/views/waybill/_expand-row-details.php
+++ frontend/modules/clientintegr/modules/rkws/views/waybill/_expand-row-details.php
@@ -106,10 +106,13 @@
 
             ]
         );
+        print "<pre>";
+        print_r($columns);
+        print "</pre>";
         $timestamp_now=time();
         ($licucs->status_id==1) && ($timestamp_now<=(time($licucs->td))) ? $lic_rkws_ucs=1 : $lic_rkws_ucs=0;
         (($lic->status_id==1) && ($timestamp_now<=(time($lic->td)))) ? $lic_rkws=1 : $lic_rkws=0;
-        if (($lic_rkws_ucs==0) or ($lic_rkws==0)) {unset($columns[9]['buttons']['export']);}
+        if (($lic_rkws_ucs==0) or ($lic_rkws==0)) {unset($columns[10]['buttons']['export']);}
 
 ?>
 

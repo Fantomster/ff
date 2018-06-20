86a792bd89f3c327af04fb39f0c7d64bba28c71e
Нарзяев М <m@cn13.ru>
2018-06-20 13:29:46 +0300

    refactor: Создание накладной #dev-1150

--- api_web/classes/IikoWebApi.php
+++ api_web/classes/IikoWebApi.php
@@ -5,42 +5,32 @@ namespace api_web\classes;
 use api_web\components\WebApi;
 use api_web\modules\integration\modules\iiko\models\iikoOrder;
 
-
 class IikoWebApi extends WebApi
 {
-
     /**
      * iiko: Список Накладных к заказу
      * @param array $post
      * @return array
-     * @throws BadRequestHttpException
-     * @throws \Exception
      */
     public function getOrderWaybillsList(array $post): array
     {
         return (new iikoOrder())->getOrderWaybillsList($post);
     }
 
-
     /**
      * iiko: Завершенные заказы
      * @param array $post
      * @return array
-     * @throws BadRequestHttpException
-     * @throws \Exception
      */
     public function getCompletedOrdersList(array $post): array
     {
         return (new iikoOrder())->getCompletedOrdersList($post);
     }
 
-
     /**
      * iiko: Создание накладной к заказу
      * @param array $post
      * @return array
-     * @throws BadRequestHttpException
-     * @throws \Exception
      */
     public function createWaybill(array $post): array
     {
--- api_web/modules/integration/modules/iiko/models/iikoOrder.php
+++ api_web/modules/integration/modules/iiko/models/iikoOrder.php
@@ -58,7 +58,7 @@ class iikoOrder extends WebApi
      */
     public function createWaybill(array $post): array
     {
-        $order_id = $post['order_id'];
+        $order_id = (int)$post['order_id'];
         $ord = \common\models\Order::findOne(['id' => $order_id]);
 
         if (!$ord) {
@@ -75,12 +75,10 @@ class iikoOrder extends WebApi
         $model->store_id = $post['store_id'] ?? null;
         $model->doc_date = $post['doc_date'] ?? '';
         $model->note = $post['note'] ?? '';
-        try{
-            $model->validate();
-        }catch (ValidationException $e){
-            throw new BadRequestHttpException($e->getMessage());
+
+        if (!$model->validate() || !$model->save()) {
+            throw new ValidationException($model->getFirstErrors());
         }
-        $model->save();
 
         return [
             'success' => true,

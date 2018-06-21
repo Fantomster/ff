a896bfb48142c9edbf78e71c159d91c6fe4f0fa0
Alien <sims2planet@mail.ru>
2018-06-21 13:58:32 +0300

    complete #dev-1198

--- api_web/classes/RkeeperWebApi.php
+++ api_web/classes/RkeeperWebApi.php
@@ -4,11 +4,12 @@ namespace api_web\classes;
 
 use api_web\components\WebApi;
 use api_web\modules\integration\modules\rkeeper\models\rkeeperOrder;
+use api_web\modules\integration\modules\rkeeper\models\rkeeperStore;
 
 class RkeeperWebApi extends WebApi
 {
     /**
-     * iiko: Список Накладных к заказу
+     * rkeeper: Список Накладных к заказу
      * @param array $post
      * @return array
      */
@@ -28,7 +29,7 @@ class RkeeperWebApi extends WebApi
     }
 
     /**
-     * iiko: Создание накладной к заказу
+     * rkeeper: Создание накладной к заказу
      * @param array $post
      * @return array
      */
@@ -36,4 +37,14 @@ class RkeeperWebApi extends WebApi
     {
         return (new iikoOrder())->createWaybill($post);
     }*/
+
+    /**
+     * rkeeper: Справочник складов
+     * @param array $post
+     * @return array
+     */
+     public function getStoreList(array $post): array
+     {
+         return (new rkeeperStore())->getStoreList($post);
+     }
 }
---
+++ api_web/modules/integration/modules/rkeeper/controllers/StoreController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace api_web\modules\integration\modules\rkeeper\controllers;
+
+use api_web\components\WebApiController;
+
+class StoreController extends WebApiController
+{
+    /**
+     * @SWG\Post(path="/integration/rkeeper/order/list",
+     *     tags={"Integration/rkeeper/order"},
+     *     summary="Справочник складов",
+     *     description="Справочник складов",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *         name="post",
+     *         in="body",
+     *         required=true,
+     *         @SWG\Schema (
+     *              @SWG\Property(property="user", ref="#/definitions/User"),
+     *              @SWG\Property(
+     *                  property="request",
+     *                  default={
+     *                                  "view_type": 1,
+     *                    }
+     *              )
+     *         )
+     *     ),
+     *    @SWG\Response(
+     *         response = 200,
+     *         description = "success",
+     *            @SWG\Schema(
+     *              default={
+     *                       {
+     *                          "id": 33251,
+     *                          "rid": "10",
+     *                          "name":"Склад 1",
+     *                          "type": 0,
+     *                          "level": 0,
+     *                          "items" :
+     *                          {
+     *                              "id": 33252,
+     *                              "rid": 11,
+     *                              "name":"Длительного хранения",
+     *                              "type": 2,
+     *                              "level": 1,
+     *                          },
+     *                      },
+     *              }
+     *          )
+     *     ),
+     *     @SWG\Response(
+     *         response = 400,
+     *         description = "BadRequestHttpException"
+     *     ),
+     *     @SWG\Response(
+     *         response = 401,
+     *         description = "error"
+     *     )
+     * )
+     */
+    public function actionList()
+    {
+        $this->response = $this->container->get('RkeeperWebApi')->geStoreList($this->request);
+    }
+}

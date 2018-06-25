f1cdd989b27a6227268fadcda4308979f9969538
kosyanov <kosyanov@mixcart.ru>
2018-06-25 11:19:30 +0300

    Без задачи. Добавление комментариев к таблице jobs и её столбцам.

--- console/migrations/m180625_080435_add_comments_table_jobs.php
+++ console/migrations/m180625_080435_add_comments_table_jobs.php
@@ -7,9 +7,10 @@ class m180625_080435_add_comments_table_jobs extends Migration
     public function safeUp()
     {
         $this->execute('alter table `jobs` comment "Таблица сведений о должностях сотрудников поставщиков и ресторанов";');
+        $this->renameColumn('{{%jobs}}', 'organizarion_type_id','organization_type_id');
         $this->addCommentOnColumn('{{%jobs}}', 'id', 'Идентификатор записи в таблице');
         $this->addCommentOnColumn('{{%jobs}}', 'name_job', 'Наименование должности сотрудников поставщиков и ресторанов');
-        $this->addCommentOnColumn('{{%jobs}}', 'organization_type_id', 'Идентификатор типа организации, к которой относится должность сотрудника');
+        $this->addCommentOnColumn('{{%jobs}}', 'organization_type_id', 'Идентификатор типа организации, к которой относится должность сотрудников');
     }
 
     public function safeDown()

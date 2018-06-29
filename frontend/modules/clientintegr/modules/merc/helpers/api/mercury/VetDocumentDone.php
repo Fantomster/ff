a5278486545ca1444d30526693c9b6aee710c6d4
Alien <sims2planet@mail.ru>
2018-06-29 18:18:56 +0300

    refactoring mercury

--- frontend/modules/clientintegr/modules/merc/helpers/api/mercury/VetDocumentDone.php
+++ frontend/modules/clientintegr/modules/merc/helpers/api/mercury/VetDocumentDone.php
@@ -91,100 +91,28 @@ class VetDocumentDone extends Component
         $data->delivery->transportInfo = $this->doc->certifiedConsignment->transportInfo;
         $data->delivery->transportStorageType = $this->doc->certifiedConsignment->transportStorageType;
 
-        $doc = $this->doc;
+        $data->delivery->accompanyingForms = $this->doc->certifiedConsignment->accompanyingForms;
 
+        $user = new User();
+        $user->login = $this->login;
 
-                        if(isset($doc->ns2batch->ns2expiryDate))
-                        $xml .='<vet:expiryDate>'.
-                            $this->getDate($doc->ns2batch->ns2expiryDate)
-                        .'</vet:expiryDate>';
-                        $xml .='<vet:perishable>'.$doc->ns2batch->ns2perishable->__toString().'</vet:perishable>
-                        <vet:countryOfOrigin>
-                           <base:uuid>'.$doc->ns2batch->ns2countryOfOrigin->bsuuid->__toString().'</base:uuid>
-                        </vet:countryOfOrigin>';
+        $facts = new DeliveryFactList();
+        $facts->vetCertificatePresence = 'ELECTRONIC';
+        $facts->decision = $this->type;
+        $facts->docInspection = new DeliveryInspection();
+        $facts->docInspection->responsible = $user;
+        $facts->docInspection->result = 'CORRESPONDS';
+        $facts->vetInspection = new DeliveryInspection();
+        $facts->vetInspection->responsible = $user;
+        $facts->vetInspection->result = 'CORRESPONDS';
 
-                        if(isset($doc->ns2batch->ns2producerList))
-                        $xml .= '<vet:producerList>
-                           <ent:producer>
-                              <ent:enterprise>
-                                 <base:guid>'.$doc->ns2batch->ns2producerList->entproducer->ententerprise->bsguid.'</base:guid>
-                              </ent:enterprise>
-                              <ent:role>'.$doc->ns2batch->ns2producerList->entproducer->entrole.'</ent:role>
-                           </ent:producer>
-                        </vet:producerList>';
+        $data->deliveryFacts = $facts;
 
-                        if (isset($doc->ns2batch->ns2productMarkingList))
-                        $xml .= '<vet:productMarkingList>
-                           <vet:productMarking>'.$doc->ns2batch->ns2productMarkingList->ns2productMarking->__toString().'</vet:productMarking>
-                        </vet:productMarkingList>';
-
-                        $xml .= '<vet:lowGradeCargo>'.$doc->ns2batch->ns2lowGradeCargo->__toString().'</vet:lowGradeCargo>
-                     </vet:consignment>
-                     <vet:accompanyingForms>
-                        <vet:waybill>';
-                        $xml .= isset($doc->ns2waybillSeries) ? '<shp:issueSeries>'.$doc->ns2waybillSeries->__toString().'</shp:issueSeries>' : '';
-                        $xml .= isset($doc->ns2waybillNumber) ? '<shp:issueNumber>'.$doc->ns2waybillNumber->__toString().'</shp:issueNumber>' : '';
-                        $xml .= isset($doc->ns2waybillDate) ? '<shp:issueDate>'.$doc->ns2waybillDate->__toString().'</shp:issueDate>' : '';
-                        $xml .= isset($doc->ns2waybillType) ? '<shp:type>'.$doc->ns2waybillType->__toString().'</shp:type>' : '';
-
-                        if(isset($doc->ns2broker))
-                            $xml .='<shp:broker>
-                              <base:guid>'.$doc->ns2broker->bsguid->__toString().'</base:guid>
-                           </shp:broker>';
-
-                           $xml .= '<shp:transportInfo>
-                              <shp:transportType>'.$doc->ns2transportInfo->shptransportType->__toString().'</shp:transportType>
-                              <shp:transportNumber>';
-
-                                if(isset($doc->ns2transportInfo->shptransportNumber->shpcontainerNumber))
-                                    $xml.= '<shp:containerNumber>'.$doc->ns2transportInfo->shptransportNumber->shpcontainerNumber->__toString().'</shp:containerNumber>';
-
-                                if(isset($doc->ns2transportInfo->shptransportNumber->shpwagonNumber))
-                                    $xml.= '<shp:wagonNumber>'.$doc->ns2transportInfo->shptransportNumber->shpwagonNumber->__toString().'</shp:wagonNumber>';
-
-                                if(isset($doc->ns2transportInfo->shptransportNumber->shpvehicleNumber))
-                                    $xml.= '<shp:vehicleNumber>'.$doc->ns2transportInfo->shptransportNumber->shpvehicleNumber->__toString().'</shp:vehicleNumber>';
-
-                                if(isset($doc->ns2transportInfo->shptransportNumber->shptrailerNumber))
-                                    $xml.= '<shp:trailerNumber>'.$doc->ns2transportInfo->shptransportNumber->shptrailerNumber->__toString().'</shp:trailerNumber>';
-
-                                if(isset($doc->ns2transportInfo->shptransportNumber->shpshipName))
-                                    $xml.= '<shp:shipName>'.$doc->ns2transportInfo->shptransportNumber->shpshipName->__toString().'</shp:shipName>';
-
-                                if(isset($doc->ns2transportInfo->shptransportNumber->shpflightNumber))
-                                    $xml.= '<shp:flightNumber>'.$doc->ns2transportInfo->shptransportNumber->shpflightNumber->__toString().'</shp:flightNumber>';
-
-                              $xml .= '</shp:transportNumber>
-                           </shp:transportInfo>
-                           <shp:transportStorageType>'.$doc->ns2transportStorageType->__toString().'</shp:transportStorageType>
-                        </vet:waybill>
-                        <vet:vetCertificate>
-                            <base:uuid>'.$this->UUID.'</base:uuid>';
-
-                  $xml .= '</vet:vetCertificate>
-                  </vet:accompanyingForms>
-                  </merc:delivery>
-                  <merc:deliveryFacts>
-                     <vet:vetCertificatePresence>ELECTRONIC</vet:vetCertificatePresence>
-                     <vet:docInspection>
-                        <vet:responsible>
-                           <com:login>'.$this->login.'</com:login>
-                        </vet:responsible>
-                        <vet:result>CORRESPONDS</vet:result>
-                     </vet:docInspection>
-                     <vet:vetInspection>
-                        <vet:responsible>
-                           <com:login>'.$this->login.'</com:login>
-                        </vet:responsible>
-                        <vet:result>'.(($this->type == self::ACCEPT_ALL) ? 'CORRESPONDS' : 'MISMATCH').'</vet:result>
-                     </vet:vetInspection>
-                     <vet:decision>'.$this->type.'</vet:decision>
-                  </merc:deliveryFacts>';
-
-                 if($this->type != self::ACCEPT_ALL)
+/*
+        if($this->type != self::ACCEPT_ALL)
                      $xml .= $this->getDiscrepancyReport($doc, $date);
-               $xml .= '</merc:processIncomingConsignmentRequest>';
-              return $xml;
+         $xml .= '</merc:processIncomingConsignmentRequest>';*/
+        return $data;
     }
 
     public function getDate($date_raw)

0fb23d2d2f955a9f1ce67dffd6349536fa526307
xsupervisor <xsupervisor@mixcart.ru>
2018-07-04 21:32:26 +0300

    #DEV-1278 Организация создания каталогов Поставщика для массового сопоставления через RabbitMQ.

---
+++ console/controllers/WatcherDaemonController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 27.02.2018
+ * Time: 18:30
+ */
+
+namespace console\controllers;
+
+
+class WatcherDaemonController extends \vyants\daemon\controllers\WatcherDaemonController
+{
+
+   public $daemons= [];
+    /**
+     * @return array
+     */
+    protected function defineJobs()
+    {
+        sleep($this->sleep);
+        //TODO: modify list, or get it from config, it does not matter
+        /*$daemons = [
+            ['className' => 'OneDaemonController', 'enabled' => true],
+            ['className' => 'AnotherDaemonController', 'enabled' => false]
+        ];*/
+        return $this->daemons;
+    }
+}

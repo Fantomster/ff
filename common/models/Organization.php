2f1dbb0658beea8d6054c9cd6c6982658b82780d
k.shevchenko94 <k.shevchenko94@gmail.com>
2018-07-05 18:31:16 +0300

    исправил ошибку

--- common/models/Organization.php
+++ common/models/Organization.php
@@ -1471,7 +1471,7 @@ class Organization extends \yii\db\ActiveRecord {
         $lic = \api\common\models\RkServicedata::find()->andWhere('org = :org', ['org' => $this->id])->one();
         $t = strtotime(date('Y-m-d H:i:s', time()));
         if ($lic) {
-            if ($t >= strtotime($lic->fd) && $t <= strtotime($lic->td) && $lic->status_id === 2) {
+            if ($t >= strtotime($lic->fd) && $t <= strtotime($lic->td) && $lic->status_id === 1) {
                 $return['rk'] = true;
             }
         }

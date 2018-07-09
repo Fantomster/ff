d896fa916cb3b103cfd2196197a0d060dcc35261
Alien <sims2planet@mail.ru>
2018-07-09 19:34:05 +0300

    fix #dev-1297

--- frontend/modules/clientintegr/modules/merc/controllers/DefaultController.php
+++ frontend/modules/clientintegr/modules/merc/controllers/DefaultController.php
@@ -135,6 +135,7 @@ class DefaultController extends \frontend\modules\clientintegr\controllers\Defau
                 if(!$api->getVetDocumentDone($uuid, $model->attributes))
                     throw new \Exception('Done error');
 
+                Yii::$app->session->setFlash('success', 'ВСД успешно погашен!');
                 if (Yii::$app->request->isAjax)
                     return true;
                 return $this->redirect(['view', 'uuid' => $uuid]);
@@ -164,7 +165,6 @@ class DefaultController extends \frontend\modules\clientintegr\controllers\Defau
             return $this->goBack((!empty(Yii::$app->request->referrer) ? Yii::$app->request->referrer : ['index']));
         }
 
-        Yii::$app->session->setFlash('success', 'ВСД успешно погашен!');
         if (Yii::$app->request->isAjax)
             return $this->renderAjax('rejected/_ajaxForm', [
                 'model' => $model,

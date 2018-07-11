a400e45dd1c4bc30ae221208601de5530b111de6
Alien <sims2planet@mail.ru>
2018-07-11 12:56:06 +0300

    edit settings mercury

--- frontend/modules/clientintegr/modules/merc/views/settings/_form.php
+++ frontend/modules/clientintegr/modules/merc/views/settings/_form.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use common\models\User;
+use yii\jui\AutoComplete;
 
 /* @var $this yii\web\View */
 /* @var $model \api\common\models\iiko\iikoPconst */
@@ -18,17 +19,26 @@ use common\models\User;
     switch ($dicConst->type) {
         case \api\common\models\merc\mercDicconst::TYPE_DROP :
             if ($dicConst->denom === 'enterprise_guid') {
-                echo $form->field($model, 'value')->dropDownList($org_list);
+                echo $form->field($model, 'value')->widget(
+                    AutoComplete::className(), [
+                    'clientOptions' => [
+                        'source' => $org_list,
+                    ],
+                    'options'=>[
+                        'class'=>'form-control'
+                    ]
+                ])->label(false);
+                //echo $form->field($model, 'value')->dropDownList($org_list);
             }
         break;
         case \api\common\models\merc\mercDicconst::TYPE_PASSWORD:
-            echo $form->field($model, 'value')->passwordInput(['maxlength' => true]);
+            echo $form->field($model, 'value')->passwordInput(['maxlength' => true])->label(false);
         break;
         case \api\common\models\merc\mercDicconst::TYPE_CHECKBOX:
-            echo $form->field($model, 'value')->checkbox(['label' => 'Только ручная загрузка ВСД']);
+            echo $form->field($model, 'value')->checkbox(['label' => 'Только ручная загрузка ВСД'])->label(false);
         break;
         default:
-            echo $form->field($model, 'value')->textInput(['maxlength' => true]);
+            echo $form->field($model, 'value')->textInput(['maxlength' => true])->label(false);
     }
     ?>
     <div class="form-group">

e7b17a60f7b041c16cf30352d13677f80ca80e47
El Babuino <sharapudin.omarov@gmail.com>
2018-07-19 18:27:26 +0300

    #DEV-1308

--- deploy/scripts/init_qas.sh
+++ deploy/scripts/init_qas.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e
 cd /var/www/html/qas.mixcart.ru/
 composer update
@@ -16,7 +17,7 @@ fi
 
 php yii migrate --interactive=0
 
-echo "clear old dump.."
-[ -e /var/www/html/qas.mixcart.ru/deploy/db_dump/qas-dbs.sql ] && rm /var/www/html/qas.mixcart.ru/deploy/db_dump/qas-dbs.sql
-echo "export..."
-mysqldump -h qas-db.cj1dgfxs1e0h.eu-west-1.rds.amazonaws.com -u fkeep -phateyouf4simba -B qas-main qas-int --add-drop-database > /var/www/html/qas.mixcart.ru/deploy/db_dump/qas-dbs.sql
+#echo "clear old dump.."
+#[ -e /var/www/html/qas.mixcart.ru/deploy/db_dump/qas-dbs.sql ] && rm /var/www/html/qas.mixcart.ru/deploy/db_dump/qas-dbs.sql
+#echo "export..."
+#mysqldump -h qas-db.cj1dgfxs1e0h.eu-west-1.rds.amazonaws.com -u fkeep -phateyouf4simba -B qas-main qas-int --add-drop-database > /var/www/html/qas.mixcart.ru/deploy/db_dump/qas-dbs.sql

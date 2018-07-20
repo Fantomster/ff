60c14bd56c11f1ac5d89ff1bcd19991341d82522
Alien <sims2planet@mail.ru>
2018-07-20 15:49:10 +0300

    fix

--- frontend/modules/clientintegr/modules/rkws/components/RabbitHelper.php
+++ frontend/modules/clientintegr/modules/rkws/components/RabbitHelper.php
@@ -8,9 +8,6 @@
 
 namespace frontend\modules\clientintegr\modules\rkws\components;
 
-use api\common\models\AllMaps;
-use api\common\models\RabbitJournal;
-use common\models\Organization;
 use yii\helpers\Json;
 
 
@@ -30,7 +27,7 @@ class RabbitHelper
 
         $sel = "SELECT total_count, success_count, fail_count from rabbit_journal where id = ".$mess['id'];
 
-        $curr =  Yii::$app->db_api->createCommand($sel)->asArray()->queryAll();
+        $curr =  \Yii::$app->db_api->createCommand($sel)->asArray()->queryAll();
 
         // 'UPDATE account SET forum=:newValue WHERE forum=:oldValue', [':newValue' => 300, ':oldValue' => 200])->execute();
 
@@ -65,7 +62,7 @@ class RabbitHelper
 
                 $sel2 = "SELECT id from user where organization_id = ".$mess['body']['org_id'];
 
-                $clientUsers =  Yii::$app->api->createCommand($sel2)->asArray()->queryAll();
+                $clientUsers =  \Yii::$app->api->createCommand($sel2)->asArray()->queryAll();
 
 
             $cache->set('clientUsers_'.$mess['id'], $clientUsers, 60*10);

030c800ae67ab628e4bab9b93ced5a4812c21cd1
El Babuino <sharapudin.omarov@gmail.com>
2018-07-23 10:16:00 +0300

    #DEV-634

--- frontend/controllers/ClientController.php
+++ frontend/controllers/ClientController.php
@@ -576,7 +576,11 @@ class ClientController extends DefaultController {
                             } else {
                                 $price = str_replace('.', '', $price);
                             }
-                            $newProduct = new CatalogBaseGoods();
+                            
+                            $newProduct = CatalogBaseGoods::findOne(['product' => $product]);
+                            if (empty($newProduct)) {
+                                $newProduct = new CatalogBaseGoods();
+                            }
                             $newProduct->scenario = "import";
                             $newProduct->cat_id = $lastInsert_base_cat_id;
                             $newProduct->supp_org_id = $get_supp_org_id;

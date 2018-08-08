4eca36d5e83dfff7d2d3e0677c4c019b7b1d2a85
kosyanov <kosyanov@mixcart.ru>
2018-08-08 19:23:48 +0300

    #DEV-1513 Устранение проблем с пагинацией внутри приходных накладных ТОРГ-12.

--- frontend/modules/clientintegr/modules/email/views/invoice/_content.php
+++ frontend/modules/clientintegr/modules/email/views/invoice/_content.php
@@ -1,9 +1,16 @@
 <?php
+use kartik\grid\GridView;
 echo \yii\helpers\Html::input('hidden', 'invoice_id', $model->id);
 ?>
 
-<?= \kartik\grid\GridView::widget([
-    'dataProvider' => new \yii\data\ArrayDataProvider(['allModels' => $model->content]),
+<?= GridView::widget([
+    'dataProvider' => new \yii\data\ArrayDataProvider(['allModels' => $model->content,
+        'pagination' => ['pageParam' => 'page_inner'],
+        'sort' => false,
+    ]),
+    'layout' => '{items}{pager}',
+    'pjax' => true, // pjax is set to always true for this demo
+    'filterPosition' => false,
     'striped' => true,
     'condensed' => true,
     'summary' => false,

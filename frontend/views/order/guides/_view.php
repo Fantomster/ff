5ba89a204cc294fde48a8d137da99956f8632f95
Alexey Sergeev <otpixto@yandex.ru>
2018-08-16 21:06:50 +0300

    Исправить поиск по шаблону в Chrome #dev-1651

--- frontend/views/order/guides/_view.php
+++ frontend/views/order/guides/_view.php
@@ -16,7 +16,7 @@ SCRIPT;
 // Register tooltip/popover initialization javascript
 $this->registerJs($js);
 
-$guideUrl = Url::to(['order/ajax-show-guide', 'id'=>$guide->id]);
+$guideUrl = Url::to(['order/ajax-show-guide', 'id' => $guide->id]);
 $guideSessionUrl = Url::to(['order/ajax-add-good-quantity-to-session']);
 
 $this->registerJs('
@@ -69,37 +69,38 @@ $this->registerJs('
     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
         <span aria-hidden="true">×</span></button>
     <h4 class="modal-title"><?= $guide->name ?></h4>
-    <span><?= Yii::t('message', 'frontend.views.order.guides.can_manage', ['ru'=>'Вы можете управлять шаблоном в данном заказе']) ?></span>
+    <span><?= Yii::t('message', 'frontend.views.order.guides.can_manage', ['ru' => 'Вы можете управлять шаблоном в данном заказе']) ?></span>
     <div class="guid-header">
         <?php
         $form = ActiveForm::begin([
-                    'options' => [
-                        'id' => 'searchProductForm',
-                        'role' => 'search',
-                        'action' => ['order/ajax-show-guide', 'id' => $guide->id]
-                    ],
+            'options' => [
+                'id' => 'searchProductForm',
+                'role' => 'search',
+                'action' => ['order/ajax-show-guide', 'id' => $guide->id]
+            ],
         ]);
         ?>
 
         <?=
-                $form->field($guideSearchModel, 'searchString', [
-                    'addon' => [
-                        'append' => [
-                            'content' => '<a class="btn-xs btnSubmit" data-target-form="#searchProductForm"><i class="fa fa-search"></i></a>',
-                            'options' => [
-                                'class' => 'append',
-                            ],
-                        ],
-                    ],
+        $form->field($guideSearchModel, 'searchString', [
+            'addon' => [
+                'append' => [
+                    'content' => '<a class="btn-xs btnSubmit" data-target-form="#searchProductForm"><i class="fa fa-search"></i></a>',
                     'options' => [
-                        'class' => "form-group",
+                        'class' => 'append',
                     ],
-                ])
-                ->textInput([
-                    'id' => 'searchString',
-                    'class' => 'form-control',
-                    'placeholder' => Yii::t('message', 'frontend.views.order.guides.search', ['ru'=>'Поиск по названию'])])
-                ->label(false)
+                ],
+            ],
+            'options' => [
+                'class' => "form-group",
+            ],
+        ])
+            ->textInput([
+                'id' => 'searchString',
+                'class' => 'form-control',
+                'autocomplete' => 'off',
+                'placeholder' => Yii::t('message', 'frontend.views.order.guides.search', ['ru' => 'Поиск по названию'])])
+            ->label(false)
         ?>
 
         <?=
@@ -125,15 +126,15 @@ $this->registerJs('
 </div>
 <div class="modal-body">
     <div class="row">
-        <div class="col-md-12">   
+        <div class="col-md-12">
             <?php
             Pjax::begin(['formSelector' => '#searchProductForm', 'enablePushState' => false, 'id' => 'guideProductList', 'timeout' => 30000, 'clientOptions' => ['url' => Url::to(['/order/ajax-show-guide', 'id' => $guide->id])]]);
             ?>
             <?php
             $form = ActiveForm::begin([
-                        'options' => [
-                            'id' => 'gridForm',
-                        ],
+                'options' => [
+                    'id' => 'gridForm',
+                ],
             ]);
             ?>
             <?=
@@ -148,45 +149,45 @@ $this->registerJs('
                     [
                         'format' => 'raw',
                         'attribute' => 'baseProduct.product',
-                        'value' => function($data) {
+                        'value' => function ($data) {
                             return "<div class='guid_block_create_title'><p>" . $data["product"] . "</p></div>"
-                                    . "<div class='guid_block_create_counts'><p>" . $data["name"] . "</p></div>";
+                                . "<div class='guid_block_create_counts'><p>" . $data["name"] . "</p></div>";
                         },
                         'contentOptions' => ['style' => 'width: 40%;'],
                     ],
                     ['format' => 'raw',
                         'attribute' => 'price',
-                        'value' => function($data) {
-                            return '<span data-toggle="tooltip" data-placement="bottom" title="'.Yii::t('message', 'frontend.views.order.price_update', ['ru'=>'Обновлена:']).' '.Yii::$app->formatter->asDatetime($data['price_updated_at'], "dd-MM-YY").'">'.
-                                $data["price"] . ' ' . $data["symbol"] . '/' . $data["ed"].'</span>';
+                        'value' => function ($data) {
+                            return '<span data-toggle="tooltip" data-placement="bottom" title="' . Yii::t('message', 'frontend.views.order.price_update', ['ru' => 'Обновлена:']) . ' ' . Yii::$app->formatter->asDatetime($data['price_updated_at'], "dd-MM-YY") . '">' .
+                                $data["price"] . ' ' . $data["symbol"] . '/' . $data["ed"] . '</span>';
                         },
                         'contentOptions' => ['style' => 'width: 20%;'],
                     ],
                     [
                         'attribute' => 'quantity',
-                        'content' => function($data) use ($cart){
+                        'content' => function ($data) use ($cart) {
                             $initVal = $_SESSION['GuideProductCount.' . $data["id"]] ?? 0;
-                            foreach($cart as $one){
-                                if($data['cbg_id'] == $one['product_id']){
+                            foreach ($cart as $one) {
+                                if ($data['cbg_id'] == $one['product_id']) {
                                     $initVal = $one['quantity'];
                                 }
                             }
                             $units = $data['units'] ?? 0.100;
                             return TouchSpin::widget([
-                                        'name' => 'GuideProduct[' . $data["id"] . ']',
-                                        'pluginOptions' => [
-                                            'initval' => $initVal,
-                                            'min' => 0,
-                                            'max' => PHP_INT_MAX,
-                                            'step' => (isset($units) && ($units)) ? $units : 1,
-                                            'decimals' => (empty($units) || (fmod($units, 1) > 0)) ? 3 : 0,
-                                            'forcestepdivisibility' => (isset($units) && $units && (floor($units) == $units)) ? 'floor' : 'none',
-                                            'buttonup_class' => 'btn btn-default',
-                                            'buttondown_class' => 'btn btn-default',
-                                            'buttonup_txt' => '<i class="glyphicon glyphicon-plus-sign"></i>',
-                                            'buttondown_txt' => '<i class="glyphicon glyphicon-minus-sign"></i>'
-                                        ],
-                                        'options' => ['class' => 'quantity form-control '],
+                                'name' => 'GuideProduct[' . $data["id"] . ']',
+                                'pluginOptions' => [
+                                    'initval' => $initVal,
+                                    'min' => 0,
+                                    'max' => PHP_INT_MAX,
+                                    'step' => (isset($units) && ($units)) ? $units : 1,
+                                    'decimals' => (empty($units) || (fmod($units, 1) > 0)) ? 3 : 0,
+                                    'forcestepdivisibility' => (isset($units) && $units && (floor($units) == $units)) ? 'floor' : 'none',
+                                    'buttonup_class' => 'btn btn-default',
+                                    'buttondown_class' => 'btn btn-default',
+                                    'buttonup_txt' => '<i class="glyphicon glyphicon-plus-sign"></i>',
+                                    'buttondown_txt' => '<i class="glyphicon glyphicon-minus-sign"></i>'
+                                ],
+                                'options' => ['class' => 'quantity form-control '],
                             ]);
                         },
                         'contentOptions' => ['style' => 'width: 20%;'],
@@ -229,7 +230,11 @@ $this->registerJs('
             ?>
             <?php ActiveForm::end(); ?>
             <?php Pjax::end(); ?>
-            <button style="margin-top: 20px;margin-right: 7px;" class="btn btn-md btn-success pull-right add-guide-to-cart" data-url="<?= Url::to(['order/ajax-add-guide-to-cart', 'id' => $guide->id]) ?>"><i class="fa fa-paper-plane-o"></i> <?= Yii::t('message', 'frontend.views.order.guides.add_all', ['ru'=>'Добавить все']) ?></button>
+            <button style="margin-top: 20px;margin-right: 7px;"
+                    class="btn btn-md btn-success pull-right add-guide-to-cart"
+                    data-url="<?= Url::to(['order/ajax-add-guide-to-cart', 'id' => $guide->id]) ?>"><i
+                        class="fa fa-paper-plane-o"></i> <?= Yii::t('message', 'frontend.views.order.guides.add_all', ['ru' => 'Добавить все']) ?>
+            </button>
         </div>
     </div>
 </div>

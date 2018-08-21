44f8bf18248b3ef45f0a7bd2f4843f01597b69df
Михаил Нарзяев <narzyaev@mixcart.ru>
2018-08-21 14:59:28 +0300

    Разработать метод выполняющий выборку дублей в указанном прайс-листе #DEV-1619 Тестирование DEV Разработать метод выполняющий выборку дублей в указанном прайс-листе

--- api_web/classes/CatalogWebApi.php
+++ api_web/classes/CatalogWebApi.php
@@ -2,9 +2,12 @@
 
 namespace api_web\classes;
 
+use common\models\CatalogTemp;
+use common\models\CatalogTempContent;
 use Yii;
 use common\models\Catalog;
 use common\models\CatalogBaseGoods;
+use yii\db\Query;
 use yii\web\BadRequestHttpException;
 use api_web\components\WebApi;
 
@@ -35,7 +38,7 @@ class CatalogWebApi extends WebApi
             throw new BadRequestHttpException('Catalog not empty');
         }
     }
- 
+
     /**
      * Удаление главного каталога
      * @param Catalog $catalog
@@ -69,6 +72,86 @@ class CatalogWebApi extends WebApi
             'product' => Yii::t('api_web', 'api_web.catalog.key.product', ['ru' => 'Нименование товара']),
             'article' => Yii::t('api_web', 'api_web.catalog.key.article', ['ru' => 'Артикул']),
             'other' => Yii::t('api_web', 'api_web.catalog.key.other', ['ru' => 'Другое']),
+        ];
+    }
+
+    /**
+     * Поиск дублей в темповом каталоге
+     * @param array $request
+     * @return array
+     * @throws BadRequestHttpException
+     */
+    public function getTempDuplicatePosition(array $request)
+    {
+        if (empty($request['cat_id'])) {
+            throw new BadRequestHttpException('empty_param|cat_id');
+        }
+
+        $catalogTemp = CatalogTemp::findOne(['cat_id' => $request['cat_id'], 'user_id' => $this->user->id]);
+        if (empty($catalogTemp)) {
+            throw new BadRequestHttpException('catalog_temp_not_found');
+        }
+
+        if (empty($catalogTemp->index_column)) {
+            throw new BadRequestHttpException('Для каталога не назначен ключ');
+        }
+
+        //Ключ, по которому ищем дубли
+        $index = $catalogTemp->index_column;
+
+        //Готовим джойн, по которому будем выцеплять дубли
+        $innerJoin = (new Query())
+            ->select([$index, 'COUNT(*) AS CountOf'])
+            ->from(CatalogTempContent::tableName())
+            ->where(['temp_id' => $catalogTemp->id])
+            ->groupBy($index)
+            ->having("CountOf > 1")
+            ->createCommand()
+            ->getRawSql();
+
+        /**
+         * Сам запрос на поиск дублей
+         * можно вывести запрос, если вместо ->all() сделать
+         * ->createCommand()->getRawSql();
+         * и распечатать результат в $content
+         */
+        $content = (new Query())
+            ->select('*')
+            ->from(CatalogTempContent::tableName() . " as T1")
+            ->where(["T1.temp_id" => $catalogTemp->id])
+            ->innerJoin("({$innerJoin}) as T2", "T1.{$index} = T2.{$index}")
+            ->all();
+
+        $result = [];
+
+        //Если не нашли дублей
+        if (empty($content)) {
+            return $result;
+        }
+
+        foreach ($content as $row) {
+            $result[$row[$catalogTemp->index_column]][] = $this->prepareTempPosition($row);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $row
+     * @return array
+     */
+    private function prepareTempPosition(array $row)
+    {
+        return [
+            "id" => (int)$row['id'],
+            "temp_id" => (int)$row['temp_id'],
+            "article" => $row['article'],
+            "product" => $row['product'],
+            "price" => round($row['price'], 2),
+            "units" => round($row['units'], 3),
+            "note" => $row['note'],
+            "ed" => $row['ed'],
+            "CountOf" => (int)$row['CountOf']
         ];
     }
 }

f91063f343eb3b5887af6f84ce2a58d29ccf49fa
xsupervisor <xsupervisor@mixcart.ru>
2018-08-22 15:58:35 +0300

    #DEV-1737 Задвоение записей номенклатуры при массовом сопоставлении

--- api/common/models/rkws/OrderCatalogSearchMap.php
+++ api/common/models/rkws/OrderCatalogSearchMap.php
@@ -127,15 +127,13 @@ class OrderCatalogSearchMap extends \common\models\search\OrderCatalogSearch
              LEFT JOIN `organization` `org` ON cbg.supp_org_id = org.id
              LEFT JOIN `catalog` `cat` ON cbg.cat_id = cat.id
              LEFT JOIN `currency` `curr` ON cat.currency_id = curr.id
-             LEFT JOIN `$dbName`.`all_map` fmap ON cbg.id = fmap.product_id
+             LEFT JOIN `$dbName`.`all_map` fmap ON cbg.id = fmap.product_id AND fmap.org_id = ".$this->client->id."
              LEFT JOIN `$dbName`.`rk_product` fprod ON fmap.serviceproduct_id = fprod.id
-             LEFT JOIN `$dbName`.`rk_storetree` fstore ON fmap.store_rid = fstore.rid        
+             LEFT JOIN `$dbName`.`rk_storetree` fstore ON fmap.store_rid = fstore.rid AND fmap.org_id = fstore.acc  AND fstore.type = 2        
            WHERE          
            cbg.cat_id IN (" . $this->catalogs . ")
            ".$where."
            AND (cbg.status = 1 AND cbg.deleted = 0 ) 
-           AND fstore.type = 2
-           AND fmap.org_id = ".$this->client->id." 
         UNION ALL
           SELECT 
           " . implode(',', $fieldsCG) . "
@@ -144,15 +142,13 @@ class OrderCatalogSearchMap extends \common\models\search\OrderCatalogSearch
            LEFT JOIN `organization` `org` ON cbg.supp_org_id = org.id
            LEFT JOIN `catalog` `cat` ON cg.cat_id = cat.id
            LEFT JOIN `currency` `curr` ON cat.currency_id = curr.id
-           LEFT JOIN `$dbName`.`all_map` fmap ON cbg.id = fmap.product_id
+           LEFT JOIN `$dbName`.`all_map` fmap ON cbg.id = fmap.product_id AND fmap.org_id = ".$this->client->id."
            LEFT JOIN `$dbName`.`rk_product` fprod ON fmap.serviceproduct_id = fprod.id
-           LEFT JOIN `$dbName`.`rk_storetree` fstore ON fmap.store_rid = fstore.rid  
+           LEFT JOIN `$dbName`.`rk_storetree` fstore ON fmap.store_rid = fstore.rid  AND fmap.org_id = fstore.acc  AND fstore.type = 2
           WHERE         
           cg.cat_id IN (" . $this->catalogs . ")
           ".$where."
-          AND (cbg.status = 1 AND cbg.deleted = 0) 
-          AND fstore.type = 2
-          AND fmap.org_id = ".$this->client->id."
+          AND (cbg.status = 1 AND cbg.deleted = 0)     
         ) as c WHERE id != 0 ".$where_all;
 
         $query = \Yii::$app->db->createCommand($sql);

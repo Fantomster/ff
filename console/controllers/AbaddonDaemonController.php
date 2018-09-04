a8a6285eb688581bf73c6d43859de7dc5a7947f4
Konstantin Silukov <silukov@mixcart.ru>
2018-09-04 12:59:40 +0300

    fx errors with 'NOT_FOUND queue' #DEV-1485

--- console/controllers/AbaddonDaemonController.php
+++ console/controllers/AbaddonDaemonController.php
@@ -86,15 +86,21 @@ class AbaddonDaemonController extends \console\modules\daemons\components\Watche
         $res = \Yii::$app->db_api->createCommand('SELECT * FROM rabbit_queues')->queryAll();
 
         foreach ($res as $row) {
-            $kill = $this->checkForKill($row);
-            $this->daemons[$row['consumer_class_name'] . $row['organization_id']] = [
-                'className'     => 'ConsumerDaemonController',
-                'enabled'       => !$kill,
-                'consumerClass' => $row['consumer_class_name'],
-                'orgId'         => $row['organization_id'],
-                'demonize'      => 0,
-                'hardKill'      => $kill,
-            ];
+            try {
+                $queue = \Yii::$app->get('rabbit')->setQueue($this->getQueueName($row))->checkQueueCount();
+                $kill = $this->checkForKill($row, $queue);
+                $this->daemons[$row['consumer_class_name'] . $row['organization_id']] = [
+                    'className'     => 'ConsumerDaemonController',
+                    'enabled'       => !$kill,
+                    'consumerClass' => $row['consumer_class_name'],
+                    'orgId'         => $row['organization_id'],
+                    'demonize'      => 0,
+                    'hardKill'      => $kill,
+                ];
+            } catch(\Throwable $t){
+                $log = \Yii::getLogger();
+                $log->log($t->getMessage(), $log::LEVEL_ERROR, 'abaddon');
+            }
         }
 
 //			Testing string
@@ -115,9 +121,8 @@ class AbaddonDaemonController extends \console\modules\daemons\components\Watche
      * @param array $row sql array from rabbit_queues table row
      * @return boolean
      * */
-    protected function checkForKill($row)
+    protected function checkForKill($row, $queue)
     {
-        $queue = \Yii::$app->get('rabbit')->setQueue($this->getQueueName($row))->checkQueueCount();
         $consumerClass = $this->getConsumerClassName($row['consumer_class_name']);
 
         if (!is_null($row['last_executed'])) {

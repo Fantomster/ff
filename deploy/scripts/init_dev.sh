2525f077aa2c21b63a0d84667dd5ae0a319718a2
El Babuino <sharapudin.omarov@gmail.com>
2018-09-04 16:57:04 +0300

    erase assets on deploy, minor fix

--- deploy/scripts/init_dev.sh
+++ deploy/scripts/init_dev.sh
@@ -3,3 +3,9 @@ cd /var/www/html/dev.mixcart.ru/
 composer update
 php yii migrate --interactive=0
 
+sudo rm -rf /var/www/html/dev.mixcart.ru/frontend/web/assets/*
+sudo rm -rf /var/www/html/dev.mixcart.ru/backend/web/assets/*
+sudo rm -rf /var/www/html/dev.mixcart.ru/market/web/assets/*
+sudo rm -rf /var/www/html/dev.mixcart.ru/franchise/web/assets/*
+
+echo "assets erased"
--- deploy/scripts/init_qas.sh
+++ deploy/scripts/init_qas.sh
@@ -17,7 +17,9 @@ fi
 
 php yii migrate --interactive=0
 
-#echo "clear old dump.."
-#[ -e /var/www/html/qas.mixcart.ru/deploy/db_dump/qas-dbs.sql ] && rm /var/www/html/qas.mixcart.ru/deploy/db_dump/qas-dbs.sql
-#echo "export..."
-#mysqldump -h qas-db.cj1dgfxs1e0h.eu-west-1.rds.amazonaws.com -u fkeep -phateyouf4simba -B qas-main qas-int --add-drop-database > /var/www/html/qas.mixcart.ru/deploy/db_dump/qas-dbs.sql
+sudo rm -rf /var/www/html/dev.mixcart.ru/frontend/web/assets/*
+sudo rm -rf /var/www/html/dev.mixcart.ru/backend/web/assets/*
+sudo rm -rf /var/www/html/dev.mixcart.ru/market/web/assets/*
+sudo rm -rf /var/www/html/dev.mixcart.ru/franchise/web/assets/*
+
+echo "assets erased"

d0b98adde0ac20e8e1cef08df17295e7f2eaa17d
alexey.sergeev <sergeev@mixcart.ru>
2018-09-04 12:09:18 +0300

    Создать интерфейс для функционала Переработки 3 #DEV-1451

--- frontend/modules/clientintegr/modules/merc/views/transport-vsd/conversion-step-2.php
+++ frontend/modules/clientintegr/modules/merc/views/transport-vsd/conversion-step-2.php
@@ -126,14 +126,6 @@ $this->title = Yii::t('app', 'frontend.client.integration.store_entry.conversion
                 </div>
                 <h4>Сведения о происхождении продукции: </h4>
                 <?php
-                $model->country = isset($model->country) ? $model->country : '72a84b51-5c5e-11e1-b9b7-001966f192f1';
-                echo $form->field($model, 'country')
-                    ->dropDownList(createStoreEntryForm::getCountryList(), ['prompt' => 'не указано',
-                        /* 'options'=>[
-                         '7' => ['label' => 'JULY', 'selected'=>true],
-                     ]*/]);
-                ?>
-                <?php
                 $url = \yii\helpers\Url::to(['stock-entry/producers-list']);
                 $desc = '';//empty($model->city) ? '' : City::findOne($model->city)->description;
 
@@ -157,21 +149,6 @@ $this->title = Yii::t('app', 'frontend.client.integration.store_entry.conversion
                     ],
                 ]);
                 ?>
-
-                <h4>Сведения о бумажном ВСД: </h4>
-                <?= $form->field($model, 'vsd_issueSeries')->textInput(['maxlength' => true]); ?>
-                <?= $form->field($model, 'vsd_issueNumber')->textInput(['maxlength' => true]); ?>
-                <div class="form-group required">
-                    <?php echo '<label class="control-label"><b>Дата бумажного ВСД</b></label>';
-                    echo $form->field($inputDate, 'first_date')->widget(\kartik\widgets\DatePicker::classname(), [
-                        'options' => ['placeholder' => 'Дата бумажного ВСД'],
-                        'pluginOptions' => [
-                            'autoclose' => true,
-                            'format' => 'dd.mm.yyyy'
-                        ]
-                    ])->label(false);
-                    ?>
-                </div>
                 <div class="form-group">
                     <?php echo Html::submitButton(Yii::t('message', 'frontend.views.layouts.client.integration.create', ['ru' => 'Создать']), ['class' => 'btn btn-success', 'disabled' => 'disabled', 'id' => 'alSubmitVSDButton']) ?>
                 </div>

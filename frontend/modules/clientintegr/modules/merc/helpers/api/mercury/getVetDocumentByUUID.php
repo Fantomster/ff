623ed2ffd8414097027a5835e9731a689134339c
Sergey Frantsev <sims2planet@mail.ru>
2018-09-04 15:30:08 +0300

    fix load vsd and stock entry

--- frontend/modules/clientintegr/modules/merc/helpers/api/mercury/getVetDocumentByUUID.php
+++ frontend/modules/clientintegr/modules/merc/helpers/api/mercury/getVetDocumentByUUID.php
@@ -108,8 +108,8 @@ class getVetDocumentByUUID extends Model
         $this->type = $doc->vetDType;
         $this->status = $doc->vetDStatus;
 
-        $consignor_business = cerberApi::getInstance(Yii::$app->user->identity->organization_id)->getBusinessEntityByUuid($doc->certifiedConsignment->consignor->businessEntity->uuid);
-        $consignor_enterprise = cerberApi::getInstance(Yii::$app->user->identity->organization_id)->getEnterpriseByUuid($doc->certifiedConsignment->consignor->enterprise->uuid);
+        $consignor_business = cerberApi::getInstance(Yii::$app->user->identity->organization_id)->getBusinessEntityByGuid($doc->certifiedConsignment->consignor->businessEntity->guid);
+        $consignor_enterprise = cerberApi::getInstance(Yii::$app->user->identity->organization_id)->getEnterpriseByGuid($doc->certifiedConsignment->consignor->enterprise->guid);
 
         $enterprise = $consignor_enterprise;
         $businessEntity = $consignor_business;
@@ -166,7 +166,7 @@ class getVetDocumentByUUID extends Model
 
         $country_raw = ikarApi::getInstance(Yii::$app->user->identity->organization_id)->getCountryByGuid($doc->certifiedConsignment->batch->origin->country->guid);
 
-        $country = isset($country) ? $country_raw->country->name : null;
+        $country = isset($country_raw) ? $country_raw->country->name : null;
 
         $purpose = dictsApi::getInstance(Yii::$app->user->identity->organization_id)->getPurposeByGuid($doc->authentication->purpose->guid);
         $purpose = isset($purpose) ? $purpose->name : null;
@@ -246,7 +246,7 @@ class getVetDocumentByUUID extends Model
             'value' => $purpose,
         ];
 
-        $this->transportInfo = isset ($doc->certifiedConsignment->transportInfo) ? ([
+        $this->transportInfo = (isset ($doc->certifiedConsignment->transportInfo) && isset($doc->certifiedConsignment->transportInfo->transportType)) ? ([
             'type' => MercVsd::$transport_types[$doc->certifiedConsignment->transportInfo->transportType],
             'numbers' => [
                 [

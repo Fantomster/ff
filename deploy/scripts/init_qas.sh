81c9fe6a89df884f6db44d69af095296c9f84ba8
El Babuino <sharapudin.omarov@gmail.com>
2018-09-04 17:03:59 +0300

    oy vey

--- deploy/scripts/init_qas.sh
+++ deploy/scripts/init_qas.sh
@@ -17,9 +17,9 @@ fi
 
 php yii migrate --interactive=0
 
-sudo rm -rf /var/www/html/dev.mixcart.ru/frontend/web/assets/*
-sudo rm -rf /var/www/html/dev.mixcart.ru/backend/web/assets/*
-sudo rm -rf /var/www/html/dev.mixcart.ru/market/web/assets/*
-sudo rm -rf /var/www/html/dev.mixcart.ru/franchise/web/assets/*
+sudo rm -rf /var/www/html/qas.mixcart.ru/frontend/web/assets/*
+sudo rm -rf /var/www/html/qas.mixcart.ru/backend/web/assets/*
+sudo rm -rf /var/www/html/qas.mixcart.ru/market/web/assets/*
+sudo rm -rf /var/www/html/qas.mixcart.ru/franchise/web/assets/*
 
 echo "assets erased"

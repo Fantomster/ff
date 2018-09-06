e0eb740824b0dd7485f132e73624f33d749dc1f3
Sergey Frantsev <sims2planet@mail.ru>
2018-09-06 17:35:34 +0300

    fix migration

--- console/migrations/m180906_141924_add_settings_mercury.php
+++ console/migrations/m180906_141924_add_settings_mercury.php
@@ -18,8 +18,8 @@ class m180906_141924_add_settings_mercury extends Migration
      */
     public function safeUp()
     {
-        $this->insert('{{%merc_pconst}}', ['org' => 0, 'const_id' => 1, 'value' => 'onlinemarket-180612']);
-        $this->insert('{{%merc_pconst}}', ['org' => 0, 'const_id' => 2, 'value' => 'Jn3F6bK3k7']);
+        $this->insert('{{%merc_pconst}}', ['org' => 0, 'const_id' => 1, 'value' => '']);
+        $this->insert('{{%merc_pconst}}', ['org' => 0, 'const_id' => 2, 'value' => '']);
         $this->insert('{{%merc_pconst}}', ['org' => 0, 'const_id' => 4, 'value' => 'none']);
         $this->insert('{{%merc_pconst}}', ['org' => 0, 'const_id' => 5, 'value' => 'none']);
         $this->insert('{{%merc_pconst}}', ['org' => 0, 'const_id' => 6, 'value' => 'none']);

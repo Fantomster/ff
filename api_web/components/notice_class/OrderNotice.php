3fc865e7d02a50278861177a20e8842d1d47a9ed
Михаил Нарзяев <narzyaev@mixcart.ru>
2018-09-11 15:18:30 +0300

    Format DateTime Request

--- api_web/components/notice_class/OrderNotice.php
+++ api_web/components/notice_class/OrderNotice.php
@@ -297,7 +297,7 @@ class OrderNotice
                 'notifications' => uniqid(),
             ], [
                 'body' => $newMessage->message,
-                'date' => \Yii::$app->formatter->asDate(\gmmktime(), 'dd.MM.yyyy HH:mm:ss')
+                'date' => \Yii::$app->formatter->asDatetime(\gmmktime(), \DateTime::ISO8601 . 'z')
             ]);
         }
         foreach ($vendorUsers as $vendorUser) {

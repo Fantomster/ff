4c33144c7bbd0fb72d93b390f5910999e582464c
Basil A Konakov <konakov@mixcart.ru>
2018-09-12 16:12:39 +0000

    История заказов EDI #DEV-1806 Тестирование DEV Внес изменения согласно замечаниям Шарапудина

--- console/migrations/m180912_150538_tbl_order_status_upgrade.php
+++ console/migrations/m180912_150538_tbl_order_status_upgrade.php
@@ -17,48 +17,48 @@ class m180912_150538_tbl_order_status_upgrade extends Migration
 
         /** @var $os OrderStatus */
 
-        $os = OrderStatus::findOne(['id' => 1]);
+        $os = OrderStatus::findOne(1);
         $os->comment = $commentPrefix . 'status_awaiting_accept_from_vendor';
         $os->save();
         EchoRu::echo ($os->comment, 'Ожидает подтверждения от поставщика', 'app');
 
-        $os = OrderStatus::findOne(['id' => 2]);
+        $os = OrderStatus::findOne(2);
         $os->comment = $commentPrefix . 'status_awaiting_accept_from_client';
         $os->save();
         EchoRu::echo ($os->comment, 'Ожидает подтверждения от ресторана', 'app');
 
-        $os = OrderStatus::findOne(['id' => 3]);
+        $os = OrderStatus::findOne(3);
         $os->comment = $commentPrefix . 'status_processing';
         $os->save();
         EchoRu::echo ($os->comment, 'Выполняется. Поставщик подтвердил заказ', 'app');
 
-        $os = OrderStatus::findOne(['id' => 4]);
+        $os = OrderStatus::findOne(4);
         $os->comment = $commentPrefix . 'status_done';
         $os->save();
         EchoRu::echo ($os->comment, 'Завершен', 'app');
 
-        $os = OrderStatus::findOne(['id' => 5]);
+        $os = OrderStatus::findOne(5);
         $os->comment = $commentPrefix . 'status_rejected';
         $os->save();
         EchoRu::echo ($os->comment, 'Отклонен', 'app');
 
-        $os = OrderStatus::findOne(['id' => 6]);
+        $os = OrderStatus::findOne(6);
         $os->comment = $commentPrefix . 'status_cancelled';
         $os->save();
         $os->comment = $commentPrefix . EchoRu::echo ($os->comment, 'Отменен', 'app');
 
-        $os = OrderStatus::findOne(['id' => 7]);
+        $os = OrderStatus::findOne(7);
         $os->comment = $commentPrefix . 'status_forming';
         $os->save();
         EchoRu::echo ($os->comment, 'Формируется', 'app');
 
-        $os = OrderStatus::findOne(['id' => 8]);
+        $os = OrderStatus::findOne(8);
         $os->denom = 'STATUS_EDO_SENT_BY_VENDOR';
         $os->comment = $commentPrefix . 'status_edo_sent_by_vendor';
         $os->save();
         EchoRu::echo ($os->comment, 'Отправлен поставщиком', 'app');
 
-        $os = OrderStatus::findOne(['id' => 9]);
+        $os = OrderStatus::findOne(9);
         $os->denom = 'STATUS_EDO_RECADV_SENT';
         $os->comment = $commentPrefix . 'status_edo_recadv_sent';
         $os->save();

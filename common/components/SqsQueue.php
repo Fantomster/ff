07d1c6743935748f56fb22f60a8cc138aba96949
El Babuino <sharapudin.omarov@gmail.com>
2018-09-17 10:17:53 +0300

    tmp

---
+++ common/components/SqsQueue.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace common\components;
+
+use yii\base\Component;
+use Aws\Sqs;
+
+/**
+ * Description of SqsQueue
+ *
+ * @author El Babuino
+ */
+class SqsQueue extends Component
+{
+    //put your code here
+}

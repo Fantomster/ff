726f3910049fa833e16db9db79b0bc58da65cf8f
Sergey Frantsev <sims2planet@mail.ru>
2018-10-02 13:10:47 +0300

    fix #DEV-1964 Исправить работу фильтра по Фирме-отправителю в Меркурии.

--- api/common/models/merc/search/mercVSDSearch.php
+++ api/common/models/merc/search/mercVSDSearch.php
@@ -18,7 +18,7 @@ class mercVSDSearch extends MercVsd
         return [
             [['date_doc', 'production_date', 'guid', 'date_from', 'date_to'], 'safe'],
             [['amount','type'], 'number'],
-            [['uuid', 'number', 'status', 'product_name', 'unit', 'sender_name','type', 'sender_guid'], 'string', 'max' => 255],
+            [['uuid', 'number', 'status', 'product_name', 'unit', 'sender_name','type', 'sender_guid','recipient_guid'], 'string', 'max' => 255],
         ];
     }
 
@@ -58,10 +58,14 @@ class mercVSDSearch extends MercVsd
             'amount' => $this->amount,
         ]);
 
-        if($this->type == 2)
+        if($this->type == 2) {
             $query->andWhere("sender_guid = '$guid'");
-        else
+            $query->andFilterWhere(['recipient_guid' => $this->recipient_guid]);
+        }
+        else {
             $query->andWhere("recipient_guid = '$guid'");
+            $query->andFilterWhere(['sender_guid' => $this->sender_guid]);
+        }
 
         if ( !empty($this->date_from) && !empty($this->date_to)) {
             $start_date = date('Y-m-d 00:00:00',strtotime($this->date_from));
@@ -70,13 +74,6 @@ class mercVSDSearch extends MercVsd
         }
 
         $query->andFilterWhere(['like', 'product_name', $this->product_name]);
-
-        if($this->type == 2) {
-            $query->andFilterWhere(['recipient_guid' => $this->sender_guid]);
-        }
-        else {
-            $query->andFilterWhere(['sender_guid' => $this->recipient_guid]);
-        }
 
         return $dataProvider;
     }

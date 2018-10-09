70adfa13605d84976b8023e5bfd18d763bbfaa01
Sergey Frantsev <sims2planet@mail.ru>
2018-10-09 17:59:19 +0300

    fix #DEV-2026 Добавить кнопку-фильтр просроченная продукция

--- api/common/models/merc/search/mercStockEntrySearch.php
+++ api/common/models/merc/search/mercStockEntrySearch.php
@@ -13,12 +13,13 @@ class mercStockEntrySearch extends MercStockEntry
     public $date_to_production_date;
     public $date_from_expiry_date;
     public $date_to_expiry_date;
+    public $is_expiry;
 
     public function rules()
     {
         return [
             [['date_doc', 'production_date', 'date_from_production_date', 'date_to', 'date_to_production_date', 'date_from_expiry_date', 'date_to_expiry_date'], 'safe'],
-            [['amount'], 'number'],
+            [['amount','is_expiry'], 'number'],
             [['uuid', 'product_name', 'unit', 'producer_name'], 'string', 'max' => 255],
         ];
     }
@@ -41,11 +42,17 @@ class mercStockEntrySearch extends MercStockEntry
     public function search($params)
     {
         $guid = mercDicconst::getSetting('enterprise_guid');
-        $query = MercStockEntry::find()->where(['owner_guid' => $guid])->andWhere('amount > 0')->andWhere(['active'=>true, 'last' => true]);
+        $query = MercStockEntry::find()->where(['owner_guid' => $guid]);
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'pagination' => [
+                'page' => isset($params['page']) ? ($params['page'] - 1) : 0,
+                'pageSize' => isset($params['pageSize']) ? $params['pageSize'] : null,
+            ],
         ]);
+
+        $query->andWhere('amount > 0')->andWhere(['active'=>true, 'last' => true]);
 
         if (!($this->load($params) && $this->validate())) {
             return $dataProvider;
@@ -61,6 +68,11 @@ class mercStockEntrySearch extends MercStockEntry
             $start_date = date('Y-m-d 00:00:00',strtotime($this->date_from_expiry_date));
             $end_date = date('Y-m-d 23:59:59',strtotime($this->date_to_expiry_date));
             $query->andFilterWhere(['between', 'STR_TO_DATE(`expiry_date`, \'%Y-%c-%e %H:%i:%s\')', $start_date, $end_date]);
+        }
+
+        if($this->is_expiry == 1)
+        {
+            $query->andWhere('expiry_date < NOW()');
         }
 
         $query->andFilterWhere(['like', 'product_name', $this->product_name]);

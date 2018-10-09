e4d14f5d4a87b187b542d3eb1a919fb04f22d8dc
kosyanov <kosyanov@mixcart.ru>
2018-10-09 17:22:32 +0300

    Правильный редирект при выборе списка доступных товаров в IIKO #DEV-1980 Тестирование DEV

--- frontend/modules/clientintegr/modules/iiko/views/settings/_form.php
+++ frontend/modules/clientintegr/modules/iiko/views/settings/_form.php
@@ -12,67 +12,68 @@ use yii\widgets\Pjax;
 /* @var $form yii\bootstrap\ActiveForm */
 ?>
 
-<div class="dict-agent-form">
-    <?php $org = User::findOne(Yii::$app->user->id)->organization_id; ?>
-    <?php $form = ActiveForm::begin(); ?>
-    <?php echo $form->errorSummary($model); ?>
-    <?php
-    switch ($dicConst->type) {
-        case \api\common\models\iiko\iikoDicconst::TYPE_DROP :
-            if ($dicConst->denom === 'taxVat') {
-                echo $form->field($model, 'value')->dropDownList([
-                    '0' => '0',
-                    '1000' => '10',
-                    '1800' => '18'
-                ]);
-            } else if ($dicConst->denom === 'auto_unload_invoice') {
-                echo $form->field($model, 'value')->dropDownList([
-                    '0' => 'Выключено',
-                    '1' => 'Включено',
-                    '2' => 'Полуавтомат',
-                ]);
-            } else {
-                echo $form->field($model, 'value')->dropDownList([
-                    '0' => 'Выключено',
-                    '1' => 'Включено',
-                ]);
-            }
-            break;
-        case \api\common\models\iiko\iikoDicconst::TYPE_PASSWORD:
-            echo $form->field($model, 'value')->passwordInput(['maxlength' => true]);
-            break;
-        case \api\common\models\iiko\iikoDicconst::TYPE_CHECKBOX:
-            $arr = [];
-            $iikoSelectedStores = \api\common\models\iiko\iikoSelectedStore::findAll(['organization_id' => $org]);
-            if ($iikoSelectedStores) {
-                foreach ($iikoSelectedStores as $iikoStore) {
-                    $arr[] = $iikoStore->store_id;
+    <div class="dict-agent-form">
+        <?php $org = User::findOne(Yii::$app->user->id)->organization_id; ?>
+        <?php $form = ActiveForm::begin(); ?>
+        <?php echo $form->errorSummary($model); ?>
+        <?php
+        switch ($dicConst->type) {
+            case \api\common\models\iiko\iikoDicconst::TYPE_DROP :
+                if ($dicConst->denom === 'taxVat') {
+                    echo $form->field($model, 'value')->dropDownList([
+                        '0' => '0',
+                        '1000' => '10',
+                        '1800' => '18'
+                    ]);
+                } else if ($dicConst->denom === 'auto_unload_invoice') {
+                    echo $form->field($model, 'value')->dropDownList([
+                        '0' => 'Выключено',
+                        '1' => 'Включено',
+                        '2' => 'Полуавтомат',
+                    ]);
+                } else {
+                    echo $form->field($model, 'value')->dropDownList([
+                        '0' => 'Выключено',
+                        '1' => 'Включено',
+                    ]);
                 }
-            }
+                break;
+            case \api\common\models\iiko\iikoDicconst::TYPE_PASSWORD:
+                echo $form->field($model, 'value')->passwordInput(['maxlength' => true]);
+                break;
+            case \api\common\models\iiko\iikoDicconst::TYPE_CHECKBOX:
+                $arr = [];
+                $iikoSelectedStores = \api\common\models\iiko\iikoSelectedStore::findAll(['organization_id' => $org]);
+                if ($iikoSelectedStores) {
+                    foreach ($iikoSelectedStores as $iikoStore) {
+                        $arr[] = $iikoStore->store_id;
+                    }
+                }
 
-            $iikoStores = \api\common\models\iiko\iikoStore::findAll(['org_id' => $org, 'is_active' => 1]);
-            if ($iikoStores && is_iterable($iikoStores)) {
-                foreach ($iikoStores as $store) {
-                    echo $form->field($model, 'value')->checkbox(['label' => $store->denom, 'value' => (is_iterable($arr) && in_array($store->id, $arr)) ? true : false, 'name' => 'Stores[' . $store->id . ']', 'checked ' => (is_iterable($arr) && in_array($store->id, $arr)) ? true : false]);
+                $iikoStores = \api\common\models\iiko\iikoStore::findAll(['org_id' => $org, 'is_active' => 1]);
+                if ($iikoStores && is_iterable($iikoStores)) {
+                    foreach ($iikoStores as $store) {
+                        echo $form->field($model, 'value')->checkbox(['label' => $store->denom, 'value' => (is_iterable($arr) && in_array($store->id, $arr)) ? true : false, 'name' => 'Stores[' . $store->id . ']', 'checked ' => (is_iterable($arr) && in_array($store->id, $arr)) ? true : false]);
+                    }
                 }
-            }
-            break;
-        case \api\common\models\iiko\iikoDicconst::TYPE_LIST:
-            echo $this->render('_goods_list', ['org' => $org, 'id' => $id]);
-            break;
-        case \api\common\models\iiko\iikoDicconst::TYPE_INT:
-            echo $form->field($model, 'value')->input('number',['maxlength' => true]);
-            break;
-        default:
-            echo $form->field($model, 'value')->textInput(['maxlength' => true]);
-    }
-    ?>
-    <div class="form-group">
-        <?php echo Html::submitButton($model->isNewRecord ? 'Создать' : 'Сохранить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        <?= Html::a('Вернуться', ['index'], ['class' => 'btn btn-success btn-export']); ?>
+                break;
+            case \api\common\models\iiko\iikoDicconst::TYPE_LIST:
+                echo $this->render('_goods_list', ['org' => $org, 'id' => $id, 'productSearch' => $productSearch, 'cookingPlaceSearch' => $cookingPlaceSearch, 'unitSearch' => $unitSearch, 'sort' => $sort, 'page' => $page]);
+                break;
+            case \api\common\models\iiko\iikoDicconst::TYPE_INT:
+                echo $form->field($model, 'value')->input('number', ['maxlength' => true]);
+                break;
+            default:
+                echo $form->field($model, 'value')->textInput(['maxlength' => true]);
+        }
+        ?>
+        <div class="form-group">
+            <?php echo Html::/*submitButton*/
+            buttonInput($model->isNewRecord ? 'Создать' : 'Сохранить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+            <?= Html::a('Вернуться', ['index'], ['class' => 'btn btn-success btn-export']); ?>
+        </div>
+        <?php ActiveForm::end(); ?>
     </div>
-    <?php ActiveForm::end(); ?>
-</div>
 <?php
 $customJs = <<< JS
 

c833372f29864394d271d11b7a8c9239ae993ee2
El Babuino <sharapudin.omarov@gmail.com>
2018-10-09 15:14:55 +0300

    format

--- api/modules/v1/modules/mobile/Module.php
+++ api/modules/v1/modules/mobile/Module.php
@@ -13,8 +13,8 @@ use common\models\forms\LoginForm;
 
 class Module extends \yii\base\Module
 {
+
     public $controllerNamespace = 'api\modules\v1\modules\mobile\controllers';
-    
     public $controllerBehaviors;
 
     public function init()
@@ -22,37 +22,37 @@ class Module extends \yii\base\Module
         parent::init();
 
         $cookieValidationKey = Yii::$app->request->cookieValidationKey;
-        
+
         Yii::$app->set('user', [
-        'class' => 'api\modules\v1\modules\mobile\components\User',
-        'identityClass' => 'api\modules\v1\modules\mobile\models\User',
-        'loginUrl' => null,
-        'enableSession' => false,
+            'class' => 'api\modules\v1\modules\mobile\components\User',
+            'identityClass' => 'api\modules\v1\modules\mobile\models\User',
+            'loginUrl' => null,
+            'enableSession' => false,
         ]);
-        
+
         Yii::$app->set(
                 'request', [
-                        'class' => 'yii\web\Request',
-                        'parsers' => [
-                            'application/json' => 'yii\web\JsonParser',
-                        ],
-                'cookieValidationKey' => $cookieValidationKey,
-                    ]
-                );
-        
+            'class' => 'yii\web\Request',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
+            'cookieValidationKey' => $cookieValidationKey,
+                ]
+        );
+
         Yii::$app->set(
                 'urlManagerFrontEnd', [
-                        'class' => 'yii\web\urlManager',
-                        'baseUrl' => Yii::$app->params['maindUrl'],
-                        'enablePrettyUrl' => true,
-                        'showScriptName' => false,
-                    ]
-                );
-        
-         $this->controllerBehaviors['authenticator'] = [
+            'class' => 'yii\web\urlManager',
+            'baseUrl' => Yii::$app->params['maindUrl'],
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+                ]
+        );
+
+        $this->controllerBehaviors['authenticator'] = [
             'class' => CompositeAuth::className(),
-            'only' => ['index', 'view', 'options', 'auth','complete-registration', 
-                'refresh-fcm-token', 'send', 'create', 
+            'only' => ['index', 'view', 'options', 'auth', 'complete-registration',
+                'refresh-fcm-token', 'send', 'create',
                 'viewed', 'update', 'create', 'delete', 'new-order', 'favorites', 'send', 'cancel-order', 'confirm-order', 'remove-supply', 'buisiness-list', 'change-buisiness',
                 'index', 'add-to-cart', 'checkout', 'set-delivery', 'set-note', 'set-comment', 'remove-position', 'make-order', 'delete-order'
             ],
@@ -60,7 +60,7 @@ class Module extends \yii\base\Module
                 [
                     'class' => HttpBasicAuth::className(),
                     'auth' => function ($username, $password) {
-            
+
                         $model = new LoginForm();
                         $model->email = $username;
                         $model->password = $password;
@@ -68,16 +68,16 @@ class Module extends \yii\base\Module
                     }
                 ],
                 HttpBearerAuth::className(),
-                QueryParamAuth::className()
+                QueryParamAuth::className(),
             ]
         ];
-                
-        $this->controllerBehaviors['contentNegotiator'] = [
-        'class' => ContentNegotiator::className(),
-        'formats' => [
-            'application/json' => Response::FORMAT_JSON
-        ]
 
+        $this->controllerBehaviors['contentNegotiator'] = [
+            'class' => ContentNegotiator::className(),
+            'formats' => [
+                'application/json' => Response::FORMAT_JSON
+            ]
         ];
     }
+
 }

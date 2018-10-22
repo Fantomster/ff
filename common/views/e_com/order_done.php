cfc8cced01226d2d41e0f6a89d1891f811180eca
alexey.sergeev <sergeev@mixcart.ru>
2018-10-22 10:10:27 +0300

    Доработка транспортной подсистемы EDI для работы с методами SOAP КОРУС 22 #DEV-1876

--- common/views/e_com/order_done.php
+++ common/views/e_com/order_done.php
@@ -9,7 +9,7 @@
     <DELIVERYNOTEDATE><?= $order->ediOrder->invoice_date ?? $dateArray['requested_delivery_date'] ?></DELIVERYNOTEDATE>
     <WAYBILLNUMBER><?= $order->id ?></WAYBILLNUMBER>
     <WAYBILLDATE><?= $dateArray['requested_delivery_date'] ?></WAYBILLDATE>
-    <RECEPTIONDATE><?= $dateArray['requested_delivery_date'] ?></RECEPTIONDATE>
+    <RECEPTIONDATE><?= $dateArray['requested_delivery_date'] ?? '' ?></RECEPTIONDATE>
     <HEAD>
         <SUPPLIER><?= $vendor->ediOrganization->gln_code ?></SUPPLIER>
         <BUYER><?= $client->ediOrganization->gln_code ?></BUYER>

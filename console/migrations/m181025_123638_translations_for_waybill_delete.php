99fe61855a62c7130c6f5217a33ca974c77bcd3c
El Babuino <sharapudin.omarov@gmail.com>
2018-10-26 11:34:29 +0300

    fix translation

--- console/migrations/m181025_123638_translations_for_waybill_delete.php
+++ console/migrations/m181025_123638_translations_for_waybill_delete.php
@@ -9,7 +9,7 @@ class m181025_123638_translations_for_waybill_delete extends Migration
 {
     public $translations = [
         'waybill.waibill_not_found' => 'Накладная не найдена',
-        'waybill.waibill_not_relation_this_servic' => 'Накладная не связана с заданным сервисом',
+        'waybill.waibill_not_relation_this_service' => 'Накладная не связана с заданным сервисом',
         'waybill.waibill_is_unloading' => 'Накладная в статусе выгружена',
         'waybill.waibill_is_relation_order' => 'Накладная связана с заказом',
         'waybill.waibill_not_releated_current_user' => 'Накладная не пренадлежит организациям текущего пользователя',

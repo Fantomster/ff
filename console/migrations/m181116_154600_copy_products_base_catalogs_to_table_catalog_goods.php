bdbc5d800c50a3be61dfde7d37247f6d22a39921
kosyanov <kosyanov@mixcart.ru>
2018-11-16 20:22:40 +0300

    Вдогонку 3

--- console/migrations/m181116_154600_copy_products_base_catalogs_to_table_catalog_goods.php
+++ console/migrations/m181116_154600_copy_products_base_catalogs_to_table_catalog_goods.php
@@ -3,6 +3,7 @@
 use yii\db\Migration;
 use common\models\CatalogBaseGoods;
 use common\models\CatalogGoods;
+use yii\helpers\ArrayHelper;
 
 class m181116_154600_copy_products_base_catalogs_to_table_catalog_goods extends Migration
 {
@@ -10,19 +11,20 @@ class m181116_154600_copy_products_base_catalogs_to_table_catalog_goods extends 
     {
         $sql = 'SELECT DISTINCT `cat_id` FROM `relation_supp_rest` `rsr` LEFT JOIN `catalog` `c` ON `rsr`.`cat_id` = `c`.`id` WHERE `c`.`type` = 1';
         $array_catalogs = Yii::$app->db->createCommand($sql)->queryAll();
-        $string_catalogs = implode(',', array_map(function ($item) {
-            return array_pop($item);
-        }, $array_catalogs));
-        $products = CatalogBaseGoods::find()->where('id in (:string)',[':string' => $string_catalogs])->all();
+        $array_1 = ArrayHelper::getColumn($array_catalogs, 'cat_id');
+        $products = CatalogBaseGoods::find()->where(['cat_id' => $array_1])->andWhere(['deleted' => 0])->all();
         /** @var CatalogBaseGoods $product */
-        foreach($products as $product) {
-            $row = new CatalogGoods;
-            $row->cat_id = $product->cat_id;
-            $row->base_goods_id = $product->id;
-            $row->price = $product->price;
-            $row->vat = null;
-            if (!$row->save()) {
-                throw new \Exception('Не удалось сохранить для каталога ' . $product->cat_id . ' в таблице catalog_goods новую запись из catalog_base_goods ' . $product->id);
+        foreach ($products as $product) {
+            $result = CatalogGoods::find()->where(['base_goods_id' => $product->id])->exists();
+            if ($result === false) {
+                $row = new CatalogGoods;
+                $row->cat_id = $product->cat_id;
+                $row->base_goods_id = $product->id;
+                $row->price = $product->price;
+                $row->vat = null;
+                if (!$row->save()) {
+                    throw new \Exception('Не удалось сохранить для каталога ' . $product->cat_id . ' в таблице catalog_goods новую запись из catalog_base_goods ' . $product->id);
+                }
             }
         }
     }

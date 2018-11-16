98a3d0e10a0ea18b870ef8c06360be7a2cf6e9c7
kosyanov <kosyanov@mixcart.ru>
2018-11-16 19:22:34 +0300

    Копирование в catalog_goods недостающих позиций главных каталогов из catalog_base_goods #DEV-2255 Тестирование DEV

---
+++ console/migrations/m181116_154600_copy_products_base_catalogs_to_table_catalog_goods.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+use common\models\CatalogBaseGoods;
+use common\models\CatalogGoods;
+
+class m181116_154600_copy_products_base_catalogs_to_table_catalog_goods extends Migration
+{
+    public function safeUp()
+    {
+        $sql = 'SELECT DISTINCT `cat_id` FROM `relation_supp_rest` `rsr` LEFT JOIN `catalog` `c` ON `rsr`.`cat_id` = `c`.`id` WHERE `c`.`type` = 1';
+        $array_catalogs = Yii::$app->db->createCommand($sql)->queryAll();
+        $string_catalogs = implode(',', array_map(function ($item) {
+            return array_pop($item);
+        }, $array_catalogs));
+        $products = CatalogBaseGoods::find()->where('id in (:string)',[':string' => $string_catalogs])->all();
+        /** @var CatalogBaseGoods $product */
+        foreach($products as $product) {
+            $row = new CatalogGoods;
+            $row->cat_id = $product->cat_id;
+            $row->base_goods_id = $product->id;
+            $row->vat = null;
+            if (!$row->save()) {
+                throw new \Exception('Не удалось сохранить для каталога ' . $product->cat_id . ' в таблице catalog_goods новую запись из catalog_base_goods ' . $product->id);
+            }
+        }
+    }
+
+    public function safeDown()
+    {
+        echo "m181116_154600_copy_products_base_catalogs_to_table_catalog_goods cannot be reverted.\n";
+        return false;
+    }
+}

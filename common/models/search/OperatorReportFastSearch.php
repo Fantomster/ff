923b97353c1d0be981e069ce6002f7e0bdff2cc6
alexey.sergeev <sergeev@mixcart.ru>
2018-11-26 15:57:45 +0300

    Доработать отчет по операторам #dev-2344

--- common/models/search/OperatorReportFastSearch.php
+++ common/models/search/OperatorReportFastSearch.php
@@ -12,7 +12,7 @@ use common\models\User;
 use yii\data\ActiveDataProvider;
 use yii\db\Query;
 
-class OperatorReportSearch extends Order
+class OperatorReportFastSearch extends Order
 {
     /**
      * Description
@@ -40,36 +40,17 @@ class OperatorReportSearch extends Order
             $timeCondition = "a.created_at between now() - interval 7 day and now()";
         }
         $query = (new Query())->select(["
-            count(a.order_id) cnt_order, 
-            count(distinct d.order_id) cnt_order_changed,
             coalesce(c.username, c.email) operator_name,
-            a.created_at,
-            count(distinct b.vendor_id) cnt_vendor, 
-            case b.status
-                when 1 then 'Ожидает подтверждения поставщика'
-                when 3 then 'Выполняется'
-                when 4 then 'Завершен'
-                when 5 then 'Отклонен поставщиком'
-                when 6 then 'Отменен'
-                else b.status
-            end status,
-            status_call_id,
-            round(avg(TIME_TO_SEC(TIMEDIFF(a.closed_at, a.created_at))) / 60, 2) avg_resolve_mins"
+            count(a.order_id) cnt_order, 
+            count(distinct d.order_id) cnt_order_changed"
         ])
-            ->from("`order` b")
-            ->leftJoin(OperatorCall::tableName() . ' as a', 'a.order_id = b.id')
+            ->from(OperatorCall::tableName() . ' as a')
             ->leftJoin(User::tableName() . ' as c', 'c.id = a.operator_id')
-            ->leftJoin("order_chat as d", "d.order_id = b.id 
-                                and b.vendor_id = d.recipient_id 
-                                and d.is_system = 1
-                                and d.message like '%изменил детали заказа%'")
-            ->where("a.order_id = b.id
-                                and a.operator_id = c.id
-                                and $timeCondition
-                                and b.status in (3, 4)")
-            ->groupBy("c.email, a.created_at, status, status_call_id")
+            ->leftJoin("order_chat as d", "d.order_id = a.order_id")
+            ->where("a.operator_id = c.id
+                                and $timeCondition")
+            ->groupBy("c.email")
             ->orderBy("a.created_at, email, status");
-        //dd($query->createCommand()->rawSql);
 
         $dataProvider = new ActiveDataProvider([
             'query'      => $query,

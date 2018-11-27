2510f6c084869beabb20eae65327d15b5ce66259
Михаил Нарзяев <narzyaev@mixcart.ru>
2018-11-27 09:47:38 +0300

    Список сервисов с лицензиями

--- common/models/AllService.php
+++ common/models/AllService.php
@@ -11,16 +11,15 @@ use yii\helpers\ArrayHelper;
 /**
  * This is the model class for table "{{%all_service}}".
  *
- * @property int $id
- * @property int $type_id
- * @property int $is_active
- * @property string $denom
- * @property string $vendor
- * @property string $created_at
- * @property string $updated_at
- * @property string $log_table
- * @property string $log_field
- *
+ * @property int                   $id
+ * @property int                   $type_id
+ * @property int                   $is_active
+ * @property string                $denom
+ * @property string                $vendor
+ * @property string                $created_at
+ * @property string                $updated_at
+ * @property string                $log_table
+ * @property string                $log_field
  * @property AllServiceOperation[] $allServiceOperations
  */
 class AllService extends \yii\db\ActiveRecord
@@ -59,15 +58,15 @@ class AllService extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id' => Yii::t('app', 'ID'),
-            'type_id' => Yii::t('app', 'Type ID'),
-            'is_active' => Yii::t('app', 'Is Active'),
-            'denom' => Yii::t('app', 'Denom'),
-            'vendor' => Yii::t('app', 'Vendor'),
+            'id'         => Yii::t('app', 'ID'),
+            'type_id'    => Yii::t('app', 'Type ID'),
+            'is_active'  => Yii::t('app', 'Is Active'),
+            'denom'      => Yii::t('app', 'Denom'),
+            'vendor'     => Yii::t('app', 'Vendor'),
             'created_at' => Yii::t('app', 'Created At'),
             'updated_at' => Yii::t('app', 'Updated At'),
-            'log_table' => Yii::t('app', 'Log Table'),
-            'log_field' => Yii::t('app', 'Log Field'),
+            'log_table'  => Yii::t('app', 'Log Table'),
+            'log_field'  => Yii::t('app', 'Log Field'),
         ];
     }
 
@@ -80,9 +79,9 @@ class AllService extends \yii\db\ActiveRecord
     }
 
     /**
-     * @param $org_id
+     * @param       $org_id
      * @param array $service_ids
-     * @param null $is_active
+     * @param null  $is_active
      * @return array
      */
     public static function getAllServiceAndLicense($org_id, $service_ids = [], $is_active = null)
@@ -93,53 +92,17 @@ class AllService extends \yii\db\ActiveRecord
             $services->andWhere(['in', 'id', $service_ids]);
         }
 
-        return ArrayHelper::getColumn($services->all(\Yii::$app->db_api), function (self $service) use ($org_id, $is_active){
-            return $service->getLicense($org_id, $is_active);
-        });
-    }
+        $result = $services->asArray()->all(\Yii::$app->db_api);
+        $licenses = License::getAllLicense($org_id, $service_ids, $is_active);
 
-    /**
-     * @param $org_id
-     * @param $is_active
-     * @return array
-     */
-    private function getLicense($org_id, $is_active) {
-        $license = (new Query())
-            ->select([
-                'license.id',
-                'license.name',
-                '(CASE WHEN license.is_active = 1 AND lo.td > NOW() THEN 1 ELSE 0 END) as is_active',
-                'license.created_at',
-                'license.updated_at',
-                'license.login_allowed',
-                'max(lo.td) as to_date',
-            ])
-            ->from(License::tableName())
-            ->leftJoin(['lo' => LicenseOrganization::tableName()], 'lo.license_id=license.id')
-            ->where(['lo.org_id' => $org_id, 'license.service_id' => $this->id])
-            ->groupBy([
-                'license.id',
-                'license.name',
-                'license.is_active',
-                'license.created_at',
-                'license.updated_at',
-                'license.login_allowed',
-            ])
-            ->indexBy('id');
-
-        if (!is_null($is_active)) {
-            $license->andWhere(['=', 'is_active', (int)$is_active]);
+        foreach ($result as &$service) {
+            if (isset($licenses[$service['id']])) {
+                $service['license'] = $licenses[$service['id']];
+            } else {
+                $service['license'] = null;
+            }
         }
 
-        return [
-            "id" => $this->id,
-            "type_id" => $this->type_id,
-            "is_active" => $this->is_active,
-            "denom" => $this->denom,
-            "vendor" => $this->vendor,
-            "created_at" => $this->created_at,
-            "updated_at" => $this->updated_at,
-            "license" => $license->one(\Yii::$app->db_api) ?: null
-        ];
+        return $result;
     }
 }

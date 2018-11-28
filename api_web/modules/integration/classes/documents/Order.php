7bc7e3c998a7cada8c1c7bab1190da849f3a2499
Михаил Нарзяев <narzyaev@mixcart.ru>
2018-11-28 18:12:40 +0300

    fix

--- api_web/modules/integration/classes/documents/Order.php
+++ api_web/modules/integration/classes/documents/Order.php
@@ -22,6 +22,7 @@ class Order extends BaseOrder implements DocumentInterface
      *
      * @return array
      * @throws BadRequestHttpException
+     * @throws \yii\db\Exception
      */
     public function prepare()
     {

88c78895849f9301df6e014bfacee76fad1cedbc
El Babuino <sharapudin.omarov@gmail.com>
2018-11-28 12:04:26 +0300

    pdf update

--- api_web/controllers/OrderController.php
+++ api_web/controllers/OrderController.php
@@ -1591,6 +1591,7 @@ class OrderController extends WebApiController
             header('Access-Control-Allow-Origin:*');
             header('Access-Control-Allow-Methods:GET, POST, OPTIONS');
             header('Access-Control-Allow-Headers:Content-Type, Authorization');
+            header('Content-Disposition','attachment; filename=mixcart_order_' . $this->request['order_id'] . '.pdf');
             header("Content-type:application/pdf");
             exit($result);
         }

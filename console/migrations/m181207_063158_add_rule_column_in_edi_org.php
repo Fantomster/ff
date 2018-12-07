3b4a3828c55391ddd5baca24e3d153aa10e2f589
alexey.sergeev <sergeev@mixcart.ru>
2018-12-07 13:52:50 +0300

    Внести изменения в логику обработки атрибута ACTION при обработке PRICAT 3 #DEV-2448

--- console/migrations/m181207_063158_add_rule_column_in_edi_org.php
+++ console/migrations/m181207_063158_add_rule_column_in_edi_org.php
@@ -13,7 +13,7 @@ class m181207_063158_add_rule_column_in_edi_org extends Migration
     public function safeUp()
     {
         $this->addColumn('{{%edi_organization}}', 'pricat_action_attribute_rule', $this->tinyInteger()->defaultValue(2));
-        $this->addCommentOnColumn('{{%edi_organization}}', 'pricat_action_attribute_rule', 'Тип обработки документа pricat');
+        $this->addCommentOnColumn('{{%edi_organization}}', 'pricat_action_attribute_rule', 'Режим обработки поля ACTION в документах pricat. Таким образом, поставщики одной группы, могут использовать параметр ACTION для частичного обновления PRICAT, в то время как поставщики другой группы всегда должны отправлять PRICAT содержащий все актуальные позиции.');
     }
 
     /**

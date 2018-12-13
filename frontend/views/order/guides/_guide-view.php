82686b07fefb77bca264a4a21f81ce2f5d64ba4b
alexey.sergeev <sergeev@mixcart.ru>
2018-12-13 16:53:59 +0300

    Изменить функционал для роли Инициатор закупки #DEV-2472

--- frontend/views/order/guides/_guide-view.php
+++ frontend/views/order/guides/_guide-view.php
@@ -5,7 +5,8 @@ use yii\helpers\Url;
 
 ?>
 <div class="guid_block_title">
-    <p style="border-bottom: 1px dotted #ccc;" >
+    <p style="border-bottom: 1px dotted #ccc;">
+        <?php if (Yii::$app->user->identity->role_id != \common\models\Role::ROLE_RESTAURANT_ORDER_INITIATOR): ?>
         <span class="title">
             <?=
             Html::a($model->name, ['/order/edit-guide', 'id' => $model->id], [
@@ -13,43 +14,52 @@ use yii\helpers\Url;
             ])
             ?>
         </span>
-        <?=Html::tag(
+        <?= Html::tag(
             'i',
             '',
             [
-                'class' => 'fa fa-edit pull-right text-warning rename-template',
-                'style' => 'cursor:pointer',
-                'title' => Yii::t('app', 'frontend.views.order.guides.rename_five', ['ru'=>'Переименовать']),
-                'data-id' => $model->id,
+                'class'      => 'fa fa-edit pull-right text-warning rename-template',
+                'style'      => 'cursor:pointer',
+                'title'      => Yii::t('app', 'frontend.views.order.guides.rename_five', ['ru' => 'Переименовать']),
+                'data-id'    => $model->id,
                 'data-title' => $model->name
             ]
-        );?>
+        ); ?>
+        <?php else: ?>
+            <span class="title">
+            <?= $model->name ?>
+        </span>
+        <?php endif; ?>
     </p>
 </div>
 <div class="guid_block_counts">
-    <p><?= Yii::t('message', 'frontend.views.order.guides.quantity', ['ru'=>'Кол-во товаров:']) ?> <span><?= $model->productCount ?></span></p>
+    <p><?= Yii::t('message', 'frontend.views.order.guides.quantity', ['ru' => 'Кол-во товаров:']) ?>
+        <span><?= $model->productCount ?></span></p>
 </div>
 <div class="guid_block_updated">
-    <p><?= Yii::t('message', 'frontend.views.order.guides.changed', ['ru'=>'Изменен:']) ?> <span><?= Yii::$app->formatter->asDatetime($model->updated_at, "php:j M Y") ?></span></p>
+    <p><?= Yii::t('message', 'frontend.views.order.guides.changed', ['ru' => 'Изменен:']) ?>
+        <span><?= Yii::$app->formatter->asDatetime($model->updated_at, "php:j M Y") ?></span></p>
 </div>
 <div class="guid_block_buttons">
+    <?php if (Yii::$app->user->identity->role_id != \common\models\Role::ROLE_RESTAURANT_ORDER_INITIATOR): ?>
+        <?=
+        Html::button('<i class="fa fa-trash"></i> ' . Yii::t('message', 'frontend.views.order.guides.remove', ['ru' => 'Удалить']), [
+            'class'    => 'btn btn-sm btn-outline-danger delete-guide',
+            'data-url' => Url::to(['/order/ajax-delete-guide', 'id' => $model->id]),
+        ])
+        ?>
+        <?=
+        Html::a('<i class="fa fa-pencil"></i> ' . Yii::t('message', 'frontend.views.order.guides.edit', ['ru' => 'Редактировать']), ['/order/edit-guide', 'id' => $model->id], [
+            'class' => 'btn btn-sm btn-outline-default',
+        ])
+        ?>
+    <?php endif; ?>
     <?=
-    Html::button('<i class="fa fa-trash"></i> ' . Yii::t('message', 'frontend.views.order.guides.remove', ['ru'=>'Удалить']), [
-        'class' => 'btn btn-sm btn-outline-danger delete-guide',
-        'data-url' => Url::to(['/order/ajax-delete-guide', 'id' => $model->id]),
-    ])
-    ?>
-    <?=
-    Html::a('<i class="fa fa-pencil"></i> ' . Yii::t('message', 'frontend.views.order.guides.edit', ['ru'=>'Редактировать']), ['/order/edit-guide', 'id' => $model->id], [
-        'class' => 'btn btn-sm btn-outline-default',
-    ])
-    ?>
-    <?=
-    Html::a('<i class="fa fa-shopping-cart"></i> ' . Yii::t('message', 'frontend.views.order.guides.in_basket', ['ru'=>'В корзину']) . ' ', ['order/ajax-show-guide', 'id' => $model->id], [
+    Html::a('<i class="fa fa-shopping-cart"></i> ' . Yii::t('message', 'frontend.views.order.guides.in_basket', ['ru' => 'В корзину']) . ' ', ['order/ajax-show-guide', 'id' => $model->id], [
         'class' => 'btn btn-sm btn-success',
-        'data' => [
-            'target' => '#guideModal',
-            'toggle' => 'modal',
+        'data'  => [
+            'target'   => '#guideModal',
+            'toggle'   => 'modal',
             'backdrop' => 'static',
         ]
     ]);

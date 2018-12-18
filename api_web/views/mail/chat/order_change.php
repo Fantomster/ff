b02c89340245b4d779b9c4c92a821f172434a27a
Sergey Frantsev <sims2planet@mail.ru>
2018-12-18 16:36:30 +0300

    complete #DEV-2547 добавил необходимую разметку в сообщения с описанием действий по заказу

--- api_web/views/mail/chat/order_change.php
+++ api_web/views/mail/chat/order_change.php
@@ -5,8 +5,9 @@
  */
 
 $i = 0;
+$trClass = "";
 ?>
-<?php if (!empty($changed)): ?>
+<?php if (!empty($changed) || !empty($deleted)): ?>
     <table class="table">
         <thead>
         <tr>
@@ -19,48 +20,43 @@ $i = 0;
         </tr>
         </thead>
         <tbody>
-        <?php foreach ($changed as $model): $model->refresh(); ?>
-            <tr>
-                <td class="order"><?= ++$i ?></td>
-                <td class="name"><?= $model->product_name ?></td>
-                <td class="article"><?= $model->article ?></td>
-                <td class="quantity"><?= $model->quantity ?></td>
-                <td class="price">
-                    <?= \api_web\helpers\CurrencyHelper::asDecimal($model->price) ?>
-                    <?= $model->getCurrency()->symbol ?><?= $model->product->ed ? '/' . $model->product->ed : '' ?>
-                </td>
-                <td class="sum">
-                    <?= \api_web\helpers\CurrencyHelper::asDecimal($model->quantity * $model->price) ?>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
-<?php endif; ?>
-
-<?php if (!empty($deleted)): ?>
-    <?php
-    $i = 0;
-    ?>
-    <?= \Yii::t('api_web', 'order.delete.content') ?>
-    <table class="table">
-        <thead>
-        <tr>
-            <th class="order">#</th>
-            <th class="name">Товар</th>
-            <th class="article">Артикул</th>
-            <th class="quantity">Кол-во</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($deleted as $model): $model->refresh(); ?>
-            <tr>
-                <td class="order"><?= ++$i ?></td>
-                <td class="name"><?= $model->product_name ?></td>
-                <td class="article"><?= $model->article ?></td>
-                <td class="quantity"><?= $model->quantity ?></td>
-            </tr>
-        <?php endforeach; ?>
+        <?php if (!empty($changed)): ?>
+            <?php foreach ($changed as $model) :
+                $trClass = "";
+                if($model->isNewRecord)
+                    {
+                        $model->refresh();
+                        $trClass = "class=\"action-added\"";
+                    }
+                ?>
+                <tr <?= $trClass ?>>
+                    <td class="order"><?= ++$i ?></td>
+                    <td class="name"><?= $model->product_name ?></td>
+                    <td class="article"><?= $model->article ?></td>
+                    <td class="quantity<?= ($model->quantity != $model->oldAttributes['quantity']) ? (($model->quantity > $model->oldAttributes['quantity']) ? " action-raised" : " action-lowered" ) : ""?>"><?= $model->quantity ?></td>
+                    <td class="price<?= ($model->price != $model->oldAttributes['price']) ? (($model->price > $model->oldAttributes['price']) ? " action-raised" : " action-lowered" ) : ""?>">
+                        <?= \api_web\helpers\CurrencyHelper::asDecimal($model->price) ?>
+                        <?= $model->getCurrency()->symbol ?><?= $model->product->ed ? '/' . $model->product->ed : '' ?>
+                    </td>
+                    <td class="sum">
+                        <?= \api_web\helpers\CurrencyHelper::asDecimal($model->quantity * $model->price) ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
+        <?php if (!empty($deleted)): ?>
+            <?php
+            $i = 0;
+            ?>
+                <?php foreach ($deleted as $model): $model->refresh(); ?>
+                    <tr class = "action-removed">
+                        <td class="order"><?= ++$i ?></td>
+                        <td class="name"><?= $model->product_name ?></td>
+                        <td class="article"><?= $model->article ?></td>
+                        <td class="quantity"><?= $model->quantity ?></td>
+                    </tr>
+                <?php endforeach; ?>
+        <?php endif; ?>
         </tbody>
     </table>
 <?php endif; ?>

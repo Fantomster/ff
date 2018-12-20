a89ef44cb4bc220bf7892516c8cb60ebf161914a
Михаил Нарзяев <narzyaev@mixcart.ru>
2018-12-20 10:35:38 +0300

    fix #DEV-2547 Тестирование DEV

--- api_web/views/mail/chat/order_change.php
+++ api_web/views/mail/chat/order_change.php
@@ -1,62 +1,81 @@
 <?php
+
+use \api_web\helpers\CurrencyHelper;
+
 /**
  * @var \api_web\modules\integration\classes\documents\OrderContent[] $changed
- * @var string[]                                                      $deleted
+ * @var \api_web\modules\integration\classes\documents\OrderContent[] $deleted
  */
 
-$i = 0;
-$trClass = "";
+/**
+ * @param \yii\db\ActiveRecord $model
+ * @param                      $attribute
+ * @return string
+ */
+function getActionClass($model, $attribute)
+{
+    $class = '';
+    if ($model->getAttribute($attribute) != $model->getOldAttribute($attribute)) {
+        if ($model->getAttribute($attribute) > $model->getOldAttribute($attribute)) {
+            $class = 'action-raised';
+        } else {
+            $class = 'action-lowered';
+        }
+    }
+    return $class;
+}
+
 ?>
 <?php if (!empty($changed) || !empty($deleted)): ?>
     <table class="table">
         <thead>
         <tr>
             <th class="order">#</th>
-            <th class="name">Товар</th>
-            <th class="article">Артикул</th>
-            <th class="quantity">Кол-во</th>
-            <th class="price">Цена</th>
-            <th class="sum">Сумма</th>
+            <th class="name"><?= \Yii::t('api_web', 'mail.chat.order_changed.name') ?></th>
+            <th class="article"><?= \Yii::t('api_web', 'mail.chat.order_changed.article') ?></th>
+            <th class="quantity"><?= \Yii::t('api_web', 'mail.chat.order_changed.quantity') ?></th>
+            <th class="price"><?= \Yii::t('api_web', 'mail.chat.order_changed.price') ?></th>
+            <th class="sum"><?= \Yii::t('api_web', 'mail.chat.order_changed.sum') ?></th>
         </tr>
         </thead>
         <tbody>
-        <?php if (!empty($changed)): ?>
-            <?php foreach ($changed as $model) :
-                $trClass = "";
-                if($model->isNewRecord)
-                    {
-                        $model->refresh();
-                        $trClass = "class=\"action-added\"";
-                    }
-                ?>
-                <tr <?= $trClass ?>>
+
+        <?php if (!empty($changed)): $i = 0; ?>
+            <?php foreach ($changed as $model) : ?>
+                <?php if ($model->isNewRecord) {
+                    $model->refresh();
+                    $add = true;
+                } else {
+                    $add = false;
+                } ?>
+                <tr class="<?= $add ? 'action-added' : 'action-changed' ?>">
                     <td class="order"><?= ++$i ?></td>
                     <td class="name"><?= $model->product_name ?></td>
                     <td class="article"><?= $model->article ?></td>
-                    <td class="quantity<?= ($model->quantity != $model->oldAttributes['quantity']) ? (($model->quantity > $model->oldAttributes['quantity']) ? " action-raised" : " action-lowered" ) : ""?>"><?= $model->quantity ?></td>
-                    <td class="price<?= ($model->price != $model->oldAttributes['price']) ? (($model->price > $model->oldAttributes['price']) ? " action-raised" : " action-lowered" ) : ""?>">
-                        <?= \api_web\helpers\CurrencyHelper::asDecimal($model->price) ?>
-                        <?= $model->getCurrency()->symbol ?><?= $model->product->ed ? '/' . $model->product->ed : '' ?>
+                    <td class="quantity <?= getActionClass($model, 'quantity') ?>"><?= $model->quantity ?></td>
+                    <td class="price <?= getActionClass($model, 'price') ?>">
+                        <?= CurrencyHelper::asDecimal($model->price) ?>
+                        <?= $model->getCurrency()->symbol ?>
+                        <?= $model->product->ed ? '/' . $model->product->ed : '' ?>
                     </td>
                     <td class="sum">
-                        <?= \api_web\helpers\CurrencyHelper::asDecimal($model->quantity * $model->price) ?>
+                        <?= CurrencyHelper::asDecimal($model->quantity * $model->price) ?>
                     </td>
                 </tr>
             <?php endforeach; ?>
         <?php endif; ?>
-        <?php if (!empty($deleted)): ?>
-            <?php
-            $i = 0;
-            ?>
-                <?php foreach ($deleted as $model): $model->refresh(); ?>
-                    <tr class = "action-removed">
-                        <td class="order"><?= ++$i ?></td>
-                        <td class="name"><?= $model->product_name ?></td>
-                        <td class="article"><?= $model->article ?></td>
-                        <td class="quantity"><?= $model->quantity ?></td>
-                    </tr>
-                <?php endforeach; ?>
+
+        <?php if (!empty($deleted)): $i = 0; ?>
+            <?php foreach ($deleted as $model): ?>
+                <tr class="action-removed">
+                    <td class="order"><?= ++$i ?></td>
+                    <td class="name"><?= $model->product_name ?></td>
+                    <td class="article"><?= $model->article ?></td>
+                    <td class="quantity"><?= $model->quantity ?></td>
+                </tr>
+            <?php endforeach; ?>
         <?php endif; ?>
+
         </tbody>
     </table>
 <?php endif; ?>

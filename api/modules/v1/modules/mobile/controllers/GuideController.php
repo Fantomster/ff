517c36267fdbc37ce40275f784ba77aec8917d34
Arsen <arsen-web@yandex.ru>
2019-01-23 12:14:33 +0300

    task ready DEV-2715 Тестирование DEV Исправил запросы

--- api/modules/v1/modules/mobile/controllers/GuideController.php
+++ api/modules/v1/modules/mobile/controllers/GuideController.php
@@ -2,19 +2,21 @@
 
 namespace api\modules\v1\modules\mobile\controllers;
 
+use api\modules\v1\modules\mobile\resources\CatalogBaseGoods;
+use api\modules\v1\modules\mobile\resources\GuideProduct;
+use common\models\User;
 use Yii;
+use yii\data\SqlDataProvider;
+use yii\db\Query;
 use yii\rest\ActiveController;
 use yii\web\NotFoundHttpException;
-use api\modules\v1\modules\mobile\resources\Order;
-use yii\data\ActiveDataProvider;
 use api\modules\v1\modules\mobile\resources\Guide;
-use yii\web\ServerErrorHttpException;
-
 
 /**
  * @author Eugene Terentev <eugene@terentev.net>
  */
-class GuideController extends ActiveController {
+class GuideController extends ActiveController
+{
 
     /**
      * @var string
@@ -24,7 +26,8 @@ class GuideController extends ActiveController {
     /**
      * @return array
      */
-    public function behaviors() {
+    public function behaviors()
+    {
         $behaviors = parent::behaviors();
 
         $behaviors = array_merge($behaviors, $this->module->controllerBehaviors);
@@ -35,20 +38,21 @@ class GuideController extends ActiveController {
     /**
      * @inheritdoc
      */
-    public function actions() {
+    public function actions()
+    {
         return [
-            'index' => [
-                'class' => 'yii\rest\IndexAction',
-                'modelClass' => $this->modelClass,
+            'index'  => [
+                'class'               => 'yii\rest\IndexAction',
+                'modelClass'          => $this->modelClass,
                 'prepareDataProvider' => [$this, 'prepareDataProvider']
             ],
             'create' => [
-                'class' => 'yii\rest\CreateAction',
+                'class'      => 'yii\rest\CreateAction',
                 'modelClass' => 'common\models\guides\Guide',
             ],
             'update' => [
-                'class' => 'yii\rest\UpdateAction',
-                'modelClass' => 'common\models\guides\Guide',
+                'class'       => 'yii\rest\UpdateAction',
+                'modelClass'  => 'common\models\guides\Guide',
                 'checkAccess' => [$this, 'checkAccess'],
             ],
         ];
@@ -56,91 +60,117 @@ class GuideController extends ActiveController {
 
     /**
      * @param $id
-     * @return null|static
+     * @return Guide
      * @throws NotFoundHttpException
      */
-    public function findModel($id) {
+    public function findModel($id)
+    {
         $model = Guide::findOne($id);
         if (!$model) {
             throw new NotFoundHttpException;
         }
+
         return $model;
     }
-    
+
     /**
-     * @return ActiveDataProvider
+     * @return SqlDataProvider
+     * @throws \Throwable
      */
     public function prepareDataProvider()
     {
         $params = new Guide();
-        $query = Guide::find();
-        
-        $dataProvider =  new ActiveDataProvider(array(
-            'query' => $query,
-            'pagination' => false,
-        ));
-        $filters = [];
+        $params->setAttributes(Yii::$app->request->queryParams);
+        /** @var User $user */
         $user = Yii::$app->user->getIdentity();
-        
-        $filters['client_id'] = $user->organization_id;
+        $org = $user->organization;
 
-        $query->select([
-            'guide.*',
-            '(select catalog_base_goods.product from guide_product left join catalog_base_goods on catalog_base_goods.id = guide_product.cbg_id where guide_product.guide_id = guide.id and catalog_base_goods.status = 1 and catalog_base_goods.deleted = 0 limit 1 ) as product1',
-            '(select catalog_base_goods.product from guide_product left join catalog_base_goods on catalog_base_goods.id = guide_product.cbg_id where guide_product.guide_id = guide.id and catalog_base_goods.status = 1 and catalog_base_goods.deleted = 0 limit 1 offset 1) as product2',
-            '(select catalog_base_goods.product from guide_product left join catalog_base_goods on catalog_base_goods.id = guide_product.cbg_id where guide_product.guide_id = guide.id and catalog_base_goods.status = 1 and catalog_base_goods.deleted = 0 limit 1 offset 2) as product3']);
-        $query->leftJoin('(select count(id) as count, guide_id, cbg_id from guide_product group by guide_id) as gp', 'gp.guide_id = guide.id');
-        $query->andWhere('gp.count is not null');
+        $product = (new Query())
+            ->select(['cbg.product'])
+            ->from(['gp' => GuideProduct::tableName()])
+            ->leftJoin(['cbg' => CatalogBaseGoods::tableName()], 'cbg.id = gp.cbg_id')
+            ->innerJoin(['guide' => Guide::tableName()], 'gp.guide_id = guide.id')
+            ->where([
+                'cbg.status'  => 1,
+                'cbg.deleted' => 0
+            ])
+            ->limit(1);
 
-        if (!($params->load(Yii::$app->request->queryParams) && $params->validate())) {
-            $query->andFilterWhere($filters);
-            return $dataProvider;
-        }
-        
-        if(isset($params->count))
-        {
-            $query->limit($params->count);
-                if(isset($params->page))
-                {
-                    $offset = ($params->page * $params->count) - $params->count;
-                    $query->offset($offset);
-                }
-        }
-  
-       
-            $filters['id'] = $params->id; 
-            $filters['type'] = $params->type; 
-            $filters['name'] = $params->name; 
-            $filters['deleted'] = $params->deleted; 
-            $filters['created_at'] = $params->created_at; 
-            $filters['updated_at'] = $params->updated_at; 
-  
-            $query->andFilterWhere($filters);
-  
-        return $dataProvider;
+        $product1 = $product->createCommand()
+            ->getRawSql();
+        $product2 = $product->offset(1)
+            ->createCommand()
+            ->getRawSql();
+        $product3 = $product->offset(2)
+            ->createCommand()
+            ->getRawSql();
+
+        $count = (new Query())
+            ->select([
+                "count(id) as count",
+                "guide_id",
+                "cbg_id"
+            ])
+            ->from(['gp' => GuideProduct::tableName()])
+            ->groupBy('guide_id')
+            ->createCommand()
+            ->getRawSql();
+
+        $query = Guide::find()
+            ->select([
+                'guide.*',
+                "({$product1}) AS " . 'product1',
+                "({$product2}) AS " . 'product2',
+                "({$product3}) AS " . 'product3',
+            ])
+            ->leftJoin("({$count}) AS gp", 'gp.guide_id = guide.id')
+            ->andWhere('gp.count is not null')
+            ->andFilterWhere([
+                'client_id'  => $org->id,
+                'id'         => $params->id,
+                'type'       => $params->type,
+                'name'       => $params->name,
+                'deleted'    => $params->deleted,
+                'created_at' => $params->created_at,
+                'updated_at' => $params->updated_at,
+            ]);
+
+        return new SqlDataProvider([
+            'sql'        => $query->createCommand()->getRawSql(),
+            'pagination' => [
+                'page'     => isset($params->page) ? ($params->page - 1) : 0,
+                'pageSize' => isset($params->count) ? $params->count : null,
+            ],
+        ]);
     }
 
-    public function actionDelete ($id)
+    /**
+     * @param $id
+     * @throws \Throwable
+     * @throws \yii\db\StaleObjectException
+     * @throws \yii\web\ForbiddenHttpException
+     */
+    public function actionDelete($id)
     {
         $guide = Guide::findOne(['id' => $id]);
-        $this->checkAccess('delete',$guide);
+        $this->checkAccess('delete', $guide);
         $guide->delete();
         Yii::$app->getResponse()->setStatusCode(204);
     }
 
     /**
      * Checks the privilege of the current user.
-     *
      * This method should be overridden to check whether the current user has the privilege
      * to run the specified action against the specified data model.
      * If the user does not have access, a [[ForbiddenHttpException]] should be thrown.
      *
-     * @param string $action the ID of the action to be executed
-     * @param \yii\base\Model $model the model to be accessed. If `null`, it means no specific model is being accessed.
-     * @param array $params additional parameters
-     * @throws ForbiddenHttpException if the user does not have access
+     * @param string          $action the ID of the action to be executed
+     * @param \yii\base\Model $model  the model to be accessed. If `null`, it means no specific model is being accessed.
+     * @param array           $params additional parameters
+     * @throws \yii\web\ForbiddenHttpException
      */
-    public function checkAccess($action, $model = null, $params = []) {
+    public function checkAccess($action, $model = null, $params = [])
+    {
         // check if the user can access $action and $model
         // throw ForbiddenHttpException if access should be denied
         if ($action === 'update' || $action === 'delete') {

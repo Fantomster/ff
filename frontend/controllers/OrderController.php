8d1b22afc5b3600f94715130310c1ca47c12e0cf
Konstantin Silukov <silukov@mixcart.ru>
2019-01-31 11:10:21 +0300

    fx report

--- frontend/controllers/OrderController.php
+++ frontend/controllers/OrderController.php
@@ -2581,21 +2581,18 @@ class OrderController extends DefaultController
 
         foreach ($dbResult as $item) {
             $arExcelHeader[$item['client_name'] . $item['id']] = $item['client_name'];
-            if (!empty($item['product_id'])) {
-                if (!isset($report[$item['product_id']])) {
-                    $report[$item['product_id']] =
+            if (!empty($item['product'])) {
+                if (!isset($report[$item['product']])) {
+                    $report[$item['product']] =
                         [
                             'product' => $item['product'],
                             'unit'    => $item['unit'],
                         ];
                     if (count($arExcelHeader) >= 3) {
-                        $report[$item['product_id']] = array_merge($report[$item['product_id']], array_fill(count($arExcelHeader), $item['count_org'], '0'));
+                        $report[$item['product']] = array_merge($report[$item['product']], array_fill(count($arExcelHeader), $item['count_org'], '0'));
                     }
-                    $report[$item['product_id']][count($arExcelHeader) - 3] = $item['sum_quantity'];
-                } else {
-                    $index = count($arExcelHeader) - 3;
-                    $report[$item['product_id']][$index] = $item['sum_quantity'];
                 }
+                $report[$item['product']][count($arExcelHeader) - 3] = $item['sum_quantity'];
             }
         }
         $objPHPExcel = new \PHPExcel();

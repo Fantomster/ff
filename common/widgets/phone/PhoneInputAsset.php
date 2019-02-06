b16d8696adbbc44cc3932abb4ddd7b0d065e17f4
El Babuino <sharapudin.omarov@gmail.com>
2019-02-06 17:30:51 +0300

    phone

---
+++ common/widgets/phone/PhoneInputAsset.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace common\widgets\phone;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset Bundle of the phone input widget. Registers required CSS and JS files.
+ * @package borales\extensions\phoneInput
+ */
+class PhoneInputAsset extends AssetBundle
+{
+    /** @var string */
+    public $sourcePath = '@common/widgets/set-info/assets';
+    /** @var array */
+    public $css = ['css/intlTelInput.css'];
+    /** @var array */
+    public $js = [
+        'js/utils.js',
+        'js/intlTelInput.min.js',
+    ];
+    /** @var array */
+    public $depends = ['yii\web\JqueryAsset'];
+}

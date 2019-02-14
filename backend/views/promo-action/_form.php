aa407d6f52fa67d2ff97c79e464a9d8b5dde421f
Sergey Frantsev <sims2planet@mail.ru>
2019-02-14 14:07:33 +0300

    DEV-2973 добавил список допустимых макросов

--- backend/views/promo-action/_form.php
+++ backend/views/promo-action/_form.php
@@ -22,17 +22,24 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'message')->widget(
         \yii\imperavi\Widget::class,
         [
-            'plugins' => ['fontsize','fullscreen', 'fontcolor','fontfamily','limiter','table'],
+            'plugins' => ['fontsize', 'fullscreen', 'fontcolor', 'fontfamily', 'limiter', 'table'],
             'options' => [
-                'minHeight' => 400,
-                'maxHeight' => 400,
-                'buttonSource' => true,
-                'convertDivs' => false,
+                'minHeight'       => 400,
+                'maxHeight'       => 400,
+                'buttonSource'    => true,
+                'convertDivs'     => false,
                 'removeEmptyTags' => true,
-                'limiter' => 1000,
+                'limiter'         => 1000,
             ],
         ]
     ) ?>
+    <p>
+        Допустимые макросы:
+    <ul>
+        <li>:lead_name|"текст замены если пусто"</li>
+        <li>:lead_city|"текст замены если пусто"</li>
+    </ul>
+    </p>
 
 
     <div class="form-group">

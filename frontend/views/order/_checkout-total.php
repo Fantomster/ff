056edb3c9cd55ecc717b06ee1c272bc04c688274
kosyanov <kosyanov@mixcart.ru>
2019-02-14 17:55:55 +0300

    Вдогонку 2

--- frontend/views/order/_checkout-total.php
+++ frontend/views/order/_checkout-total.php
@@ -1,17 +1,30 @@
 <?php
+
 use yii\helpers\Url;
 use yii\helpers\Html;
+use common\models\User;
+use common\models\Role;
+
 ?>
 
 <div class="block_right_wrap">
-    <p><?= Yii::t('app', 'Итого:') ?> <span id="orderTotal<?= $cart['id'] ?>"><?= $cart['total_price'] ?></span> <?= $cart['currency'] ?></p>
+    <p><?= Yii::t('app', 'Итого:') ?> <span
+                id="orderTotal<?= $cart['id'] ?>"><?= $cart['total_price'] ?></span> <?= $cart['currency'] ?></p>
 </div>
 <div class="block_right_wrap_1">
     <?php if ($cart['for_min_cart_price']) { ?>
-        <button type="button" class="btn btn-default alButton" disabled="disabled"><?= Yii::t('message', 'frontend.views.order.make_order_two', ['ru' => 'Оформить заказ']) ?></button>
-        <p class="alP"><?= Yii::t('message', 'frontend.views.order.until_min', ['ru' => 'до минимального заказа']) ?></p><p><?= $cart['for_min_cart_price'] ?> <?= $cart['currency'] ?></p>
-        <?php } elseif ($cart['for_free_delivery'] > 0) { ?>
-        <p><?= Yii::t('message', 'frontend.views.order.until_free', ['ru' => 'до бесплатной доставки']) ?> </p><p><?= $cart['for_free_delivery']?> <?= $cart['currency'] ?></p>
+        <?php
+        $user_id = Yii::$app->user->id;
+        $role_id = User::find()->select('role_id')->where(['id' => $user_id])->column();
+        if ($role_id != Role::ROLE_RESTAURANT_ORDER_INITIATOR) { ?>
+            <button type="button" class="btn btn-default alButton"
+                    disabled="disabled"><?= Yii::t('message', 'frontend.views.order.make_order_two', ['ru' => 'Оформить заказ']) ?></button>
+        <?php } ?>
+        <p class="alP"><?= Yii::t('message', 'frontend.views.order.until_min', ['ru' => 'до минимального заказа']) ?></p>
+        <p><?= $cart['for_min_cart_price'] ?> <?= $cart['currency'] ?></p>
+    <?php } elseif ($cart['for_free_delivery'] > 0) { ?>
+        <p><?= Yii::t('message', 'frontend.views.order.until_free', ['ru' => 'до бесплатной доставки']) ?> </p>
+        <p><?= $cart['for_free_delivery'] ?> <?= $cart['currency'] ?></p>
     <?php } elseif ($cart['for_free_delivery'] == 0) { ?>
         <p><?= Yii::t('message', 'frontend.views.order.free_delivery', ['ru' => 'бесплатная доставка!']) ?></p>
     <?php } else { ?>
@@ -19,12 +32,12 @@ use yii\helpers\Html;
     <?php } ?>
     <?=
     (!$cart['for_min_cart_price']) ? Html::button(Yii::t('message', 'frontend.views.order.make_order_two', ['ru' => 'Оформить заказ']), [
-                'class' => 'create',
-                'data' => [
-                    'url' => Url::to(['/order/ajax-make-order']),
-                    'id' => $cart['id'],
-                    'all' => false
-                ],
-            ]) : '';
+        'class' => 'create',
+        'data'  => [
+            'url' => Url::to(['/order/ajax-make-order']),
+            'id'  => $cart['id'],
+            'all' => false
+        ],
+    ]) : '';
     ?>
 </div>

cf9ade6a3cc651f45bdbef72b9229b00900e5cab
Semikov Maxim <semikov@mixcart.ru>
2019-02-18 15:25:59 +0300

    Удаление инд.кат.лен.пост. #DEV-2989 Тестирование DEV

--- api_web/classes/LazyVendorPriceWebApi.php
+++ api_web/classes/LazyVendorPriceWebApi.php
@@ -7,11 +7,15 @@
 
 namespace api_web\classes;
 
+use api_web\components\definitions\Organization;
 use api_web\helpers\CurrencyHelper;
 use api_web\helpers\WebApiHelper;
-use common\models\Catalog;
-use common\models\CatalogGoods;
-use common\models\RelationSuppRest;
+use common\models\
+{
+    Catalog,
+    CatalogGoods,
+    RelationSuppRest
+};
 use yii\data\ArrayDataProvider;
 use yii\data\Pagination;
 use yii\web\BadRequestHttpException;
@@ -131,5 +135,54 @@ class LazyVendorPriceWebApi extends LazyVendorWebApi
         }
 
         return $result;
+    }
+
+    /**
+     * Удаление индивидуального каталога ленивого поставщика
+     *
+     * @param $post
+     * @return array
+     * @throws BadRequestHttpException
+     */
+    public function deletePriceList($post)
+    {
+        $this->validateRequest($post, ['vendor_id']);
+        if (!is_int($post['vendor_id'])) {
+            throw new BadRequestHttpException('catalog.wrong_value');
+        }
+        $org = \common\models\Organization::find()
+            ->where([
+                'id'      => $post['vendor_id'],
+                'type_id' => \common\models\Organization::TYPE_LAZY_VENDOR
+            ])->one();
+        if (!empty($org)) {
+            throw new BadRequestHttpException('catalog.not_lazy_vendor');
+        }
+        $catId = $this->user->organization->getCatalogs($post['vendor_id']);
+        if ($catId < 0) {
+            throw new BadRequestHttpException('catalog.no_such_vendor');
+        }
+        /**@var $catalog Catalog */
+        $catalog = Catalog::find()
+            ->where([
+                'id'          => $catId,
+                'type'        => Catalog::CATALOG,
+                'supp_org_id' => $post['vendor_id'],
+                'status'      => Catalog::STATUS_ON
+            ])->one();
+        if (empty($catalog)) {
+            throw new BadRequestHttpException('catalog.not_exist');
+        }
+        $transaction = \Yii::$app->db->beginTransaction();
+        try {
+            CatalogGoods::deleteAll("cat_id = $catId");
+            $catalog->status = Catalog::STATUS_OFF;
+            $catalog->save();
+            $transaction->commit();
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            throw $e;
+        }
+        return ['result' => true];
     }
 }
